d284642c44df9b35f5f80594285f937af0a3b60a
Prabeen04 <prabeen.strange@gmail.com>
2018-03-31 15:03:03 +0530

    NavMenu Component implemented

--- src/components/layouts/navbar.js
+++ src/components/layouts/navbar.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { Layout, Menu, Icon, Button } from 'antd';
-import { NavMenu } from './navMenu';
+import { Layout, Menu, Icon } from 'antd';
 import './navbar.css';
+import NavMenu from './navMenu';
 const { Header, Sider, Content } = Layout;
 
 class Navbar extends React.Component {
-    state = {
+    constructor(props){
+        super(props)
+      this.toggle = this.toggle.bind(this);
+      this.state = {
         collapsed: false,
     };
+    }
+    
     toggle = () => {
         this.setState({
             collapsed: !this.state.collapsed,
@@ -20,10 +25,26 @@ class Navbar extends React.Component {
                     trigger={null}
                     collapsible
                     collapsed={this.state.collapsed}
-                    style={{ height: '100vh' }}
-                >
+                    style={{height: '100vh'}}
+                 >
                     <div className="logo" />
-                    <NavMenu inlineCollapsed={this.state.collapsed} />
+                    <NavMenu 
+                        collapsed={this.state.collapsed}
+                        toggleCollapsed={this.toggle}/>
+                    {/* <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+                        <Menu.Item key="1">
+                            <Icon type="user" />
+                            <span>nav 1</span>
+                        </Menu.Item>
+                        <Menu.Item key="2">
+                            <Icon type="video-camera" />
+                            <span>nav 2</span>
+                        </Menu.Item>
+                        <Menu.Item key="3">
+                            <Icon type="upload" />
+                            <span>nav 3</span>
+                        </Menu.Item>
+                    </Menu> */}
                 </Sider>
                 <Layout>
                     <Header style={{ background: '#fff', padding: 0 }}>
