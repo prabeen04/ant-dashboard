d3e59cdb373a4ee48a28004840ecc1f19b69c3f2
Prabeen04 <prabeen.strange@gmail.com>
2018-04-01 19:19:27 +0530

    login form addde

--- src/container/forms/forms.js
+++ src/container/forms/forms.js
@@ -1,13 +1,46 @@
 import React, { Component } from 'react'
+import { Card, Form, Icon, Input, Button, Checkbox } from 'antd';
+const FormItem = Form.Item;
 
 class Forms extends Component {
   render() {
+    const { getFieldDecorator } = this.props.form;
     return (
       <div>
-        
+        <Card>
+        <Form onSubmit={this.handleSubmit} className="login-form">
+        <FormItem>
+          {getFieldDecorator('userName', {
+            rules: [{ required: true, message: 'Please input your username!' }],
+          })(
+            <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+          )}
+        </FormItem>
+        <FormItem>
+          {getFieldDecorator('password', {
+            rules: [{ required: true, message: 'Please input your Password!' }],
+          })(
+            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+          )}
+        </FormItem>
+        <FormItem>
+          {getFieldDecorator('remember', {
+            valuePropName: 'checked',
+            initialValue: true,
+          })(
+            <Checkbox>Remember me</Checkbox>
+          )}
+          <a className="login-form-forgot" href="">Forgot password</a>
+          <Button type="primary" htmlType="submit" className="login-form-button">
+            Log in
+          </Button>
+          Or <a href="">register now!</a>
+        </FormItem>
+      </Form>
+        </Card>
       </div>
     )
   }
 }
-
-export default Forms;
+// const WrappedNormalLoginForm = Form.create()(NormalLoginForm);
+export default Form.create()(Forms);
