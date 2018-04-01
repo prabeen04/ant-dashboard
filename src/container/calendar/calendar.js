6240360941eb224a4bad56040ef3542b9e41dff5
Prabeen04 <prabeen.strange@gmail.com>
2018-04-01 19:36:00 +0530

    calendar configuration adde

--- src/container/calendar/calendar.js
+++ src/container/calendar/calendar.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
 import BigCalendar from 'react-big-calendar';
 import moment from 'moment';
+import eventList from './eventList';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
 BigCalendar.setLocalizer(BigCalendar.momentLocalizer(moment))
 
 class Calendar extends Component {
   render() {
     return (
-      <div>
-        
+      <div style={{height: '520px'}}>
+        <BigCalendar
+        events={eventList}
+        startAccessor='startDate'
+        endAccessor='endDate'
+        selectable={true}
+        onSelecting={(e)=>console.log(e)}/>
       </div>
     )
   }
