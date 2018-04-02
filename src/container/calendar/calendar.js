76894cfad9af0d093959aede5ae1489200c40431
Prabeen04 <prabeen.strange@gmail.com>
2018-04-02 21:14:40 +0530

    modal import andstate added

--- src/container/calendar/calendar.js
+++ src/container/calendar/calendar.js
@@ -3,10 +3,19 @@ import BigCalendar from 'react-big-calendar';
 import moment from 'moment';
 import eventList from './eventList';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { Modal, Button } from 'antd';
 
 BigCalendar.setLocalizer(BigCalendar.momentLocalizer(moment))
 
 class Calendar extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      ModalText: '',
+      visible: false,
+      confirmLoading: false,
+    }
+  }
   render() {
     return (
       <div style={{height: '520px'}}>
@@ -16,6 +25,7 @@ class Calendar extends Component {
           startAccessor='startDate'
           endAccessor='endDate'
           selectable={true}
+          onSelectSlot={(slot)=>console.log(slot)}
           onSelecting={(range)=>console.log(range)}
         />
       </div>
