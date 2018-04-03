5e956acc3e67d0b90ee0fe3a0b9331bff10b2e64
Prabeen04 <prabeen.strange@gmail.com>
2018-04-03 09:12:04 +0530

    getEvents action type created

--- src/actions/calendar_actions.js
+++ src/actions/calendar_actions.js
@@ -1,1 +1,21 @@
-import { GET_EVENTS, ADD_EVENT, ADD_EVENT_SUCCESS, ADD_EVENT_FAILURE } from '../types/calendar_actiontypes';
+import { GET_EVENTS, GET_EVENTS_SUCCESS, GET_EVENTS_FAILURE, ADD_EVENT, ADD_EVENT_SUCCESS, ADD_EVENT_FAILURE } from '../types/calendar_actiontypes';
+import axios from 'axios';
+
+export const getEvents = () => {
+    return (dispatch) => {
+        dispatch({
+            type: GET_EVENTS
+        })
+        return axios.get(`${baseURL}`)
+            .then(response => {
+                dispatch({
+                    type: GET_EVENTS_SUCCESS,
+                    payload: response.data
+                })
+            })
+            .catch(error => {
+                dispatch({
+                    type: GET_EVENTS_FAILURE
+                })
+            });
+    }
