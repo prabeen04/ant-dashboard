c1adfa404961e26b4235ee1948e9910e81716e4e
Prabeen04 <prabeen.strange@gmail.com>
2018-04-05 20:10:45 +0530

    form submit handler added

--- src/container/calendar/eventForm.js
+++ src/container/calendar/eventForm.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react'
 import { Field, reduxForm } from 'redux-form';
 import { Input, DatePicker } from 'antd';
+import { addEvent } from '../../actions/calendar_actions';
 
 class EventForm extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
-    
+
         this.renderInput = this.renderInput.bind(this);
         this.onDateChange = this.onDateChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this)
     }
+    onSubmit = (values) => {
+        console.log('values')
+        this.props.addEvent(values);
+    }
+
     onDateChange = (date, dateString) => {
         console.log(date, dateString);
-      }
+    }
     renderInput = ({ input, label, type, meta: { touched, error }, ...custom }) => (
         <div>
             <Input
@@ -24,47 +31,47 @@ class EventForm extends Component {
     )
     renderDatepicker = ({ input, label, type, meta: { touched, error }, ...custom }) => (
         <div>
-            <DatePicker 
+            <DatePicker
                 placeholder={label}
                 onChange={this.onDateChange}
-                value = { Date.now() }
+                value={Date.now()}
                 {...input}
                 {...custom}
             />
             {touched && error && <span>{error}</span>}
         </div>
     )
-  render() {
-    const { handleSubmit, pristine, reset, submitting } = this.props;
-    return (
-      <div>
-    <form onSubmit={handleSubmit(this.onSubmit)}>
-        <Field 
-            name="startDate"
-            label="Start Date"
-            format={(value, name) => { 
-                return value === '' ? null : value
-            }}
-            component={this.renderDatepicker}/>
-        <Field 
-            name="endDate"
-            label="End Date"
-            format={(value, name) => { 
-                return value === '' ? null : value
-            }}
-            component={this.renderDatepicker}/>
-        <Field 
-            name="desc"
-            label="Description"
-            component={this.renderInput}/>
-        <Field 
-            name="desc"
-            label="Description"
-            component={this.renderInput}/>
-          </form>  
-      </div>
-    )
-  }
+    render() {
+        const { handleSubmit, pristine, reset, submitting } = this.props;
+        return (
+            <div>
+                <form onSubmit={handleSubmit(this.onSubmit)}>
+                    <Field
+                        name="startDate"
+                        label="Start Date"
+                        format={(value, name) => {
+                            return value === '' ? null : value
+                        }}
+                        component={this.renderDatepicker} />
+                    <Field
+                        name="endDate"
+                        label="End Date"
+                        format={(value, name) => {
+                            return value === '' ? null : value
+                        }}
+                        component={this.renderDatepicker} />
+                    <Field
+                        name="desc"
+                        label="Description"
+                        component={this.renderInput} />
+                    <Field
+                        name="desc"
+                        label="Description"
+                        component={this.renderInput} />
+                </form>
+            </div>
+        )
+    }
 }
 EventForm = reduxForm({
     form: 'eventForm'
