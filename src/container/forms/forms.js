124e76e81f4145dc861bbc22078dd28e350a2082
Prabeen04 <prabeen.strange@gmail.com>
2018-04-06 21:20:26 +0530

    total form removed

--- src/container/forms/forms.js
+++ src/container/forms/forms.js
@@ -1,34 +1,15 @@
 import React, { Component } from 'react'
-import { Card, Form, Icon, Input, Button, Checkbox } from 'antd';
-const FormItem = Form.Item;
-
+\
 class Forms extends Component {
   render() {
-    const { getFieldDecorator } = this.props.form;
     return (
       <div>
         <Card>
-        <Form onSubmit={this.handleSubmit} className="login-form">
-        <FormItem>
-          {getFieldDecorator('userName', {
-            rules: [{ required: true, message: 'Please input your username!' }],
-          })(
-            <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
-          )}
-        </FormItem>
-        <FormItem>
-          {getFieldDecorator('password', {
-            rules: [{ required: true, message: 'Please input your Password!' }],
-          })(
-            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
-          )}
-        </FormItem>
-     
-      </Form>
+          <h3>Form component</h3>
         </Card>
       </div>
     )
   }
 }
 // const WrappedNormalLoginForm = Form.create()(NormalLoginForm);
-export default Form.create()(Forms);
+export default Forms;
