05727c42d1de21e83cfa66ed0d7da9bdf6888beb
Prabeen04 <prabeen.strange@gmail.com>
2018-04-07 16:06:10 +0530

    addd edit and profile view created

---
+++ src/container/profile/addProfile.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class AddProfile extends Component {
+  render() {
+    return (
+      <div>
+       AddProfile
+      </div>
+    )
+  }
+}
+export default AddProfile;
---
+++ src/container/profile/profileVIew.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class ProfileView extends Component {
+  render() {
+    return (
+      <div>
+       ProfileView
+      </div>
+    )
+  }
+}
+export default ProfileView;
