2dd20abad686c1c30956d5e53775db5bf85e6261
Prabeen04 <prabeen.strange@gmail.com>
2018-04-07 16:34:46 +0530

    profiles actions created

---
+++ src/actions/profile_actions.js
@@ -0,0 +1,42 @@
+import { GET_PROFILES, GET_PROFILES_SUCCESS, GET_PROFILES_FAILURE, ADD_PROFILE, ADD_PROFILE_SUCCESS, ADD_PROFILE_FAILURE } from '../types/calendar_actiontypes';
+import axios from 'axios';
+
+const baseURL = 'https://prabeen-restapi.herokuapp.com/api/users';
+
+export const getProfiles = () => {
+    return (dispatch) => {
+        dispatch({
+            type: GET_PROFILES
+        })
+        return axios.get(`${baseURL}`)
+            .then(response => {
+                console.log(response)
+                dispatch({
+                    type: GET_PROFILES_SUCCESS,
+                    payload: response.data
+                })
+            })
+            .catch(error => {
+                dispatch({
+                    type: GET_PROFILES_FAILURE
+                })
+            });
+    }
+}
+export const addProfile = (profile) => {
+    return (dispatch) => {
+        dispatch({
+            type: ADD_PROFILE
+        })
+        return axios.post(`${baseURL}`,{profile})
+            .then(response => {
+                console.log(response)
+                dispatch(getProfiles())
+            })
+            .catch(error => {
+                dispatch({
+                    type: ADD_PROFILE_FAILURE
+                })
+            });
+    }
+}
--- src/container/profile/profileList.js
+++ src/container/profile/profileList.js
@@ -21,7 +21,7 @@ const mapStateToProps = (state) =>{
 
 const mapDispatchToProps = (dispatch) =>{
     return bindActionCreators({
-        getProfile
+        getProfiles
     }, dispatch)
 }
 export default connect(mapStateToProps, mapDispatchToProps)(ProfileList);
