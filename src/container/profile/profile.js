b889eceb2be26f0548f695fe8029e869660e7c8d
Prabeen04 <prabeen.strange@gmail.com>
2018-04-07 15:56:28 +0530

    profile component createf

---
+++ src/container/profile/profile.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Profile extends Component {
+  render() {
+    return (
+      <div>
+        Profile Component
+      </div>
+    )
+  }
+}
+export default Profile;
