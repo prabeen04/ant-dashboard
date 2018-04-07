0375ba18ced7a3a26fc9ffff5c5ae35e78a8632e
Prabeen04 <prabeen.strange@gmail.com>
2018-04-07 16:11:54 +0530

    profile sub component implemtmted

--- src/container/profile/profile.js
+++ src/container/profile/profile.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
-
+import ProfileList from './profileList'
+import AddProfile from './addProfile'
+import EditProfile from './editProfile'
+import ProfileView from './profileView'
 class Profile extends Component {
   render() {
     return (
-      <div>
-       
+      <div className="profile-wrapper">
+        <ProfileList/>
+        <AddProfile/>
+        <EditProfile/>
+        <ProfileView/>
       </div>
     )
   }
