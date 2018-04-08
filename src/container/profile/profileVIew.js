0cd7c9acd6a8237b4833d943ba961465b80186f3
Prabeen04 <prabeen.strange@gmail.com>
2018-04-08 17:49:07 +0530

    card in profile view added

--- src/container/profile/profileVIew.js
+++ src/container/profile/profileVIew.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
+import { Card, Icon, Avatar } from 'antd';
+const { Meta } = Card;
 
 class ProfileView extends Component {
   render() {
     return (
       <div className="profile-view">
-      profile view
-      {this.props.profile.name && <h1>{this.props.profile.name}</h1>}
+        profile view
+      {this.props.profile.name &&
+          <Card
+            style={{ width: 300 }}
+            cover={<img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />}
+            actions={[<Icon type="setting" />, <Icon type="edit" />, <Icon type="ellipsis" />]}
+          >
+            <Meta
+              avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+              title={this.props.profile.name}
+              description={this.props.profile.email}
+            />
+          </Card>
+
+        }
       </div>
     )
   }
@@ -20,4 +35,4 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
   return bindActionCreators({}, dispatch)
 }
-export default connect( mapStateToProps, mapDispatchToProps )(ProfileView);
+export default connect(mapStateToProps, mapDispatchToProps)(ProfileView);
