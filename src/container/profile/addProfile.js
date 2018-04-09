f947e31458fba017675b18c4e07c0189b5182e16
Prabeen04 <prabeen.strange@gmail.com>
2018-04-09 08:54:28 +0530

    submit functionality added

--- src/container/profile/addProfile.js
+++ src/container/profile/addProfile.js
@@ -4,41 +4,52 @@ import { bindActionCreators } from 'redux'
 import { Field, reduxForm } from 'redux-form'
 import { Input } from 'antd'
 import './profile.css'
+import { addProfile } from '../../actions/profile_actions';
 
 class AddProfile extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
+
+    this.renderInput = this.renderInput.bind(this);
+    this.submitProfile = this.submitProfile.bind(this);
   }
 
   renderInput = ({ input, label, type, meta: { touched, error }, ...custom }) => {
-    return <div className="text-input"> 
-              <Input 
-                placeholder={label}
-                {...input}
-                {...custom}
-                />
-            </div>    
+    return <div className="text-input">
+      <Input
+        placeholder={label}
+        {...input}
+        {...custom}
+      />
+    </div>
+  }
+  submitProfile = (values) =>{
+    console.log(values)
   }
   render() {
+    const { handleSubmit, pristine, reset, submitting } = this.props
     return (
       <div className="add-profile">
-       <Field component={this.renderInput} label={'Name'} name="name" />
-       <Field component={this.renderInput} label={'Description'} name="description" />
-       <Field component={this.renderInput} label={'Location'} name="location" />
+        <form onSubmit={handleSubmit(this.submitProfile)}>
+          <Field component={this.renderInput} label={'Name'} name="name" />
+          <Field component={this.renderInput} label={'Description'} name="description" />
+          <Field component={this.renderInput} label={'Location'} name="location" />
+          <button type="submit">Submit</button>
+        </form>
       </div>
     )
   }
 }
 
 const mapStateToProps = (state) => {
-  return{
+  return {
 
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return bindActionCreators({
-    // addProfile
+    addProfile
   }, dispatch)
 }
 AddProfile = connect(mapStateToProps, mapDispatchToProps)(AddProfile)
