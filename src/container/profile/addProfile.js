484d66e2050668615ad1acba2b57e88e9a0d7148
Prabeen04 <prabeen.strange@gmail.com>
2018-04-09 08:46:06 +0530

    more InputFields addded

--- src/container/profile/addProfile.js
+++ src/container/profile/addProfile.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { Field, reduxForm } from 'redux-form'
 import { Input } from 'antd'
+import './profile.css'
 
 class AddProfile extends Component {
   constructor(props){
@@ -10,18 +11,20 @@ class AddProfile extends Component {
   }
 
   renderInput = ({ input, label, type, meta: { touched, error }, ...custom }) => {
-    return <Input 
+    return <div className="text-input"> 
+              <Input 
                 placeholder={label}
                 {...input}
                 {...custom}
-
                 />
+            </div>    
   }
   render() {
     return (
       <div className="add-profile">
-       AddProfile
-       <Field component={this.renderInput} label={'Your Name'} name="name" />
+       <Field component={this.renderInput} label={'Name'} name="name" />
+       <Field component={this.renderInput} label={'Description'} name="description" />
+       <Field component={this.renderInput} label={'Location'} name="location" />
       </div>
     )
   }
