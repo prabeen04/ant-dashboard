197dc3bf1b89b574a683c648882e147294e6c733
Prabeen04 <prabeen.strange@gmail.com>
2018-04-09 23:10:26 +0530

    redux form field added in editForm

--- src/container/profile/editProfile.js
+++ src/container/profile/editProfile.js
@@ -1,10 +1,41 @@
 import React, { Component } from 'react'
 
 class EditProfile extends Component {
+  constructor(props) {
+    super(props)
+
+    this.renderInput = this.renderInput.bind(this);
+    this.submitProfile = this.submitProfile.bind(this);
+  }
+
+  renderInput = ({ input, label, type, meta: { touched, error }, ...custom }) => {
+    return <div className="text-input">
+      <Input
+        placeholder={label}
+        {...input}
+        {...custom}
+      />
+    </div>
+  }
+  submitProfile = (values) =>{
+    console.log(values)
+    this.props.addProfile(values)
+  }
+
   render() {
+    const { handleSubmit, pristine, reset, submitting } = this.props
+    console.log(submitting)
     return (
-      <div className="edit-profile">
-       EditProfile
+      <div className="add-profile">
+        <form onSubmit={handleSubmit(this.submitProfile)}>
+          <Field component={this.renderInput} label={'Name'} name="name" />
+          <Field component={this.renderInput} label={'Email'} name="email" />
+          <Field component={this.renderInput} label={'Location'} name="location" />
+          <Button htmlType="submit" type="primary" loading={submitting}>
+          Click me!
+        </Button>
+       { console.log(submitting)}
+        </form>
       </div>
     )
   }
