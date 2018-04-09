06486c33b67e7c2c36e743dea96c1f626ccf6edf
Prabeen04 <prabeen.strange@gmail.com>
2018-04-09 08:41:44 +0530

    renderInput field type added

--- src/container/profile/addProfile.js
+++ src/container/profile/addProfile.js
@@ -2,12 +2,26 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { Field, reduxForm } from 'redux-form'
+import { Input } from 'antd'
 
 class AddProfile extends Component {
+  constructor(props){
+    super(props)
+  }
+
+  renderInput = ({ input, label, type, meta: { touched, error }, ...custom }) => {
+    return <Input 
+                placeholder={label}
+                {...input}
+                {...custom}
+
+                />
+  }
   render() {
     return (
       <div className="add-profile">
        AddProfile
+       <Field component={this.renderInput} label={'Your Name'} name="name" />
       </div>
     )
   }
