a40b11b9e4f56ff9c7ba82f1f6f89e327c21e7f1
Prabeen04 <prabeen.strange@gmail.com>
2018-04-10 22:05:19 +0530

    visibility local state from profile  removed

--- src/container/profile/profile.js
+++ src/container/profile/profile.js
@@ -4,22 +4,11 @@ import AddProfile from './addProfile'
 import EditProfile from './editProfile'
 import ProfileView from './profileView'
 class Profile extends Component {
-  constructor(props){
-    super(props)
-    this.state={
-      isAddProfile: false,
-      isEditProfile: false,
-      isViewProfile: false,
-
-    }
-  }
   render() {
     return (
       <div className="profile-wrapper">
         <ProfileList/>
-        {this.state.isAddProfile &&
-           <AddProfile
-           isAddProfile={this.state.isAddProfile}/>}
+        <AddProfile/>
         <EditProfile/>
         <ProfileView/>
       </div>
