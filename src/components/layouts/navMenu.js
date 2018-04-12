aa012eecdd69ad961d42032449e27468d20cc63a
Prabeen04 <prabeen.strange@gmail.com>
2018-04-12 21:37:09 +0530

    post component added to routes

--- src/components/layouts/navMenu.js
+++ src/components/layouts/navMenu.js
@@ -36,22 +36,22 @@ class NavMenu extends React.Component {
                     </Menu.Item>
                     <Menu.Item key="3">
                         <Link to='/profile'>
-                            <Icon type="inbox" />
+                            <Icon type="profile" />
                             <span>Profile</span>
                         </Link>
                     </Menu.Item>
-                    <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
-                        <Menu.Item key="5">Option 5</Menu.Item>
-                        <Menu.Item key="6">Option 6</Menu.Item>
-                        <Menu.Item key="7">Option 7</Menu.Item>
-                        <Menu.Item key="8">Option 8</Menu.Item>
-                    </SubMenu>
+                    <Menu.Item key="4">
+                        <Link to='/post'>
+                            <Icon type="medium" />
+                            <span>Post</span>
+                        </Link>
+                    </Menu.Item>
                     <SubMenu key="sub2" title={<span><Icon type="pie-chart" /><span>Navigation Two</span></span>}>
-                        <Menu.Item key="9">Option 9</Menu.Item>
-                        <Menu.Item key="10">Option 10</Menu.Item>
+                        <Menu.Item key="5">Option 9</Menu.Item>
+                        <Menu.Item key="6">Option 10</Menu.Item>
                         <SubMenu key="sub3" title="Submenu">
-                            <Menu.Item key="11">Option 11</Menu.Item>
-                            <Menu.Item key="12">Option 12</Menu.Item>
+                            <Menu.Item key="7">Option 11</Menu.Item>
+                            <Menu.Item key="8">Option 12</Menu.Item>
                         </SubMenu>
                     </SubMenu>
                 </Menu>
