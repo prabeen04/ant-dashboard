0eae2192c1ef31fa3bac851e1539d357f9d22202
Prabeen04 <prabeen.strange@gmail.com>
2018-04-12 23:52:08 +0530

    AddPost Component created

---
+++ src/container/post/addPost.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class AddPost extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Add Post Component</h1>
+      </div>
+    )
+  }
+}
+
+export default AddPost;
