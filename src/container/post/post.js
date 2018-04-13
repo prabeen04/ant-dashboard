32f3eb2bbfc29b38525dd952894370b6f6bd8251
Prabeen04 <prabeen.strange@gmail.com>
2018-04-13 09:27:20 +0530

    modal configured

--- src/container/post/post.js
+++ src/container/post/post.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
 import AddPost from "./addPost";
 import PostList from './postList';
-import { Button, Icon } from 'antd';
+import { Button, Icon, Modal } from 'antd';
 import './post.css'
 
 class Post extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      isModalOpen: false
+    }
+    this.setIsModalOpen = this.setIsModalOpen.bind(this);
+  }
+  setIsModalOpen = (bool) =>{
+    console.log(bool)
+  }
   render() {
     return (
       <div className="post-wrapper">
         <div className="post-actions">
+        <Button type="primary" icon="filter" >Filter</Button>
         <Button type="primary" icon="plus" >Add Post</Button>
         </div>
+        <Modal
+        title="Vertically centered modal dialog"
+          wrapClassName="vertical-center-modal"
+          visible={this.state.isModalOpen}
+          onOk={() => this.setIsModalOpen(false)}
+          >
         <AddPost/>
+        </Modal>
         <PostList/>
       </div>
     )
