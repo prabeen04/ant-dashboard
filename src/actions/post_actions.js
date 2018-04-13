69413560ee077ebccbf0b5d97c00d63c646e102f
Prabeen04 <prabeen.strange@gmail.com>
2018-04-13 22:36:12 +0530

    getPost action created

--- src/actions/post_actions.js
+++ src/actions/post_actions.js
@@ -2,4 +2,17 @@ import {
 GET_POSTS, GET_POSTS_SUCCESS, GET_POSTS_FAILURE, ADD_POST, ADD_POST_SUCCESS, ADD_POST_FAILURE,
  VIEW_POST, VIEW_POST_SUCCESS, VIEW_POST_FAILURE
 } from '../types/post_actiontypes';
+import axios from 'axios';
 
+const baseURL = 'https://prabeen-restapi.herokuapp.com/api/posts';
+
+export const getPosts = (dispatch) => {
+
+    return (dispatch) => {
+        dispatch({
+            type: GET_POSTS
+        })
+
+    return axios.get(`${baseURL}`)    
+    }
+}
