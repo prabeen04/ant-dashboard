a59a392107267ac0b50bf4686cee8ced05b58432
Prabeen04 <prabeen.strange@gmail.com>
2018-04-13 22:48:16 +0530

    postReducer created

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import { reducer as FormReducer } from 'redux-form';
 import { calendarReducer } from '../reducers/calendar_reducer';
 import { profileReducer } from '../reducers/profile_reducer';
+import { postReducer } from '../reducers/post_reducer';
 
 export const rootReducer = combineReducers({
     form: FormReducer,
     calendarReducer,
-    profileReducer
+    profileReducer,
+    postReducer
 })
 
 export default rootReducer;
--- src/reducers/post_reducer.js
+++ src/reducers/post_reducer.js
@@ -2,3 +2,17 @@ import {
     GET_POSTS, GET_POSTS_SUCCESS, GET_POSTS_FAILURE, ADD_POST, ADD_POST_SUCCESS, ADD_POST_FAILURE,
     VIEW_POST, VIEW_POST_SUCCESS, VIEW_POST_FAILURE
 } from '../types/post_actiontypes';
+
+const initialState = {
+    isLoading: false,
+    isError: false,
+    posts: [],
+    singlePost: {}
+}
+
+export const postReducer = (state=initialState, action){
+    switch(action.type ){
+        case GET_POSTS:
+            return 
+    }
+}
