9b26931d6b381bc8a6c2fdd1c8b7268ffc322a7b
Prabeen04 <prabeen.strange@gmail.com>
2018-04-14 10:31:33 +0530

    loading error state rendered in post list

--- src/container/post/postList.js
+++ src/container/post/postList.js
@@ -6,21 +6,35 @@ import { getPosts } from '../../actions/post_actions'
 class PostList extends Component {
   constructor(props){
     super(props)
+
+    this.renderPosts = this.renderPosts.bind(this)
   }
   componentDidMount(){
     console.log('hghgjh')
     this.props.getPosts();
   }
+  renderPosts =  this.props.posts.map(post => {
+        return <h1> my posts</h1>
+  })
   render() {
+    if(this.props.isLoading){
+      return <h4>Loading...</h4>
+    }
+    if(this.props.isError){
+      return <h4>Some Error Occoured...</h4>
+    }
     return (
       <div>
         <h1>PostList Component Created</h1>
+        {console.log(this.renderPosts)}
       </div>
     )
   }
 }
 const mapStateToProps = (state) =>{
   return{
+    isLoading: state.postReducer.isLoading,
+    isError: state.postReducer.isError,
     posts: state.postReducer.posts
   }
 }
