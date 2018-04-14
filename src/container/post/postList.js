23caed1ef2333d0ae6a5ab5874cbdd8c122ffccb
Prabeen04 <prabeen.strange@gmail.com>
2018-04-14 10:36:30 +0530

    posts rendered on component

--- src/container/post/postList.js
+++ src/container/post/postList.js
@@ -4,43 +4,43 @@ import { bindActionCreators } from 'redux'
 import { getPosts } from '../../actions/post_actions'
 
 class PostList extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
-
-    this.renderPosts = this.renderPosts.bind(this)
   }
-  componentDidMount(){
+  componentDidMount() {
     console.log('hghgjh')
     this.props.getPosts();
   }
-  renderPosts =  this.props.posts.map(post => {
-        return <h1> my posts</h1>
-  })
+   
+
   render() {
-    if(this.props.isLoading){
+ let renderPost = this.props.posts.map(post => {
+      return <h1> my posts</h1>
+    })
+    if (this.props.isLoading) {
       return <h4>Loading...</h4>
     }
-    if(this.props.isError){
+    if (this.props.isError) {
       return <h4>Some Error Occoured...</h4>
     }
     return (
       <div>
         <h1>PostList Component Created</h1>
-        {console.log(this.renderPosts)}
+        {renderPost}
       </div>
     )
   }
 }
-const mapStateToProps = (state) =>{
-  return{
+const mapStateToProps = (state) => {
+  return {
     isLoading: state.postReducer.isLoading,
     isError: state.postReducer.isError,
     posts: state.postReducer.posts
   }
 }
-const mapDispatchToProps = (dispatch) =>{
-    return bindActionCreators({
-      getPosts
-    }, dispatch)
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    getPosts
+  }, dispatch)
 }
 export default connect(mapStateToProps, mapDispatchToProps)(PostList);
