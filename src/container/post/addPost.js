020d1a07841896b99c26e3d3d9a8c8c912dc7aa8
Prabeen04 <prabeen.strange@gmail.com>
2018-04-15 19:47:03 +0530

    input and textarea generator function created

--- src/container/post/addPost.js
+++ src/container/post/addPost.js
@@ -1,19 +1,45 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux' 
+import { bindActionCreators } from 'redux'
 import { Field, withForm } from 'redux-form';
 import { addPost } from '../../actions/post_actions'
-import { Icon, Card, Button } from 'antd'
+import { Input, Icon, Card, Button } from 'antd'
 
 class AddPost extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
   }
+
+  renderInput = ({ input, label, type, meta: { touched, error }, ...custom }) => (
+    <div>
+      <Input
+        placeholder={label}
+        className="semantic-input"
+        fluid={true}
+        {...input}
+        {...custom}
+      />
+      {touched && error && <span>{error}</span>}
+    </div>
+  )
+  renderTextarea = ({ input, label, type, meta: { touched, error }, ...custom }) => (
+    <div>
+      <Form.Field
+        placeholder={label}
+        className="semantic-input"
+        control={TextArea}
+        siz="large"
+        {...input}
+        {...custom}
+      />
+      {touched && error && <span>{error}</span>}
+    </div>
+  )
   render() {
-    if(this.props.isLoading){
+    if (this.props.isLoading) {
       return <h4>Loading...</h4>
     }
-    if(this.props.isError){
+    if (this.props.isError) {
       return <h4>Oops... Something went wrong!!!</h4>
     }
     return (
