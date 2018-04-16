d6b458a5522eaf8e513a243587dd89c380ad04e9
Prabeen04 <prabeen.strange@gmail.com>
2018-04-16 21:23:30 +0530

    redirection code removed

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
-import { Redirect } from 'react-router-dom'
 import Navbar from './components/layouts/navbar';
 import './App.css';
 
 class App extends Component {
-  
   render() {
-    console.log(this.props.isAuthenticated)
-    if(!this.props.isAuthenticated){ 
-      console.log('navigate here');
-      <Redirect to="/login"/>
-    }
     return (
       <div className="wrapper">
-        <Navbar />       
+        <Navbar />
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => {
-  console.log(state)
-  return{
-    isAuthenticated: state.authReducer.isAuthenticated
-  }
-}
-export default connect(mapStateToProps)(App);
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,17 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import Login from './container/login/login'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
     <Provider store={store}>
         <Router basename={process.env.PUBLIC_URL}>
             <App />
-            <Route exact path="/login" component={Login}/>
         </Router>
     </Provider>, document.getElementById('root'));
 registerServiceWorker();
