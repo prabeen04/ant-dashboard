e1a993110b22353f0079aeeabfc9bac12a54d368
Prabeen04 <prabeen.strange@gmail.com>
2018-04-26 23:57:00 +0530

    react datepicker removed

--- src/container/calendar/calendar.js
+++ src/container/calendar/calendar.js
@@ -9,8 +9,9 @@ import eventList from './eventList';
 import EventForm from './eventForm';
 import './calendar.css';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-import { Button, Icon, message } from 'antd';
-import DatePicker from 'react-datepicker';
+import { Button, Icon, message, DatePicker } from 'antd';
+const dateFormat = 'YYYY-MM-DD';
+
 BigCalendar.setLocalizer(BigCalendar.momentLocalizer(moment))
 
 class Calendar extends Component {
@@ -38,7 +39,6 @@ class Calendar extends Component {
     message.success('Event Added', 10);
   };
   handleOk = (slot) => {
-    console.log(moment.utc(slot.start))
     this.setState({
       startDate: JSON.stringify(slot.start),
       endDate: JSON.stringify(slot.end),
@@ -65,29 +65,23 @@ class Calendar extends Component {
     if (this.props.isError) {
       return (<p>Some Error occoured...</p>)
     }
-
+    console.log(moment(this.state.startDate))
     return (
       <div className="flex-container" style={{ height: '520px', backgroundColor: '#fff', margin: '1rem' }}>
         <BigCalendar
           style={{ flexBasis: '70%' }}
           events={this.props.events}
           defaultDate={new Date()}
-          startAccessor={(event) => Date + 1}
-          endAccessor={(event) => Date + 1}
+          startAccessor={(event) => startDate}
+          endAccessor={(event) =>endDate}
           selectable={true}
+          dateFormat={dateFormat}
           onSelectSlot={(slot) => this.handleOk(slot)}
           onSelecting={(range) => console.log(range)}
         />
         <div className="event-form" >
           <form onSubmit={handleSubmit(this.onSubmit)}>
-            <DatePicker selected={moment(this.state.startDate) && null}
-            onChange={this.handleChange}
-            showTimeSelect
-            timeFormat="HH:mm"
-            timeIntervals={15}
-            dateFormat="LLL"
-            timeCaption="time"
-        />
+            <DatePicker defaultValue={moment(this.state.startDate)} />
             {/* <EventForm
               start={startDate}
               end={endDate} /> */}
