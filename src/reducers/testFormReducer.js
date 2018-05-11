b9a775695454d65480ca854e102df349ea96f76b
Prabeen04 <prabeen.strange@gmail.com>
2018-05-11 21:25:57 +0530

    datea state and reducer case created

--- src/reducers/testFormReducer.js
+++ src/reducers/testFormReducer.js
@@ -1,4 +1,4 @@
-import { SET_SELECT_VALUE } from '../types/testFormActionTypes';
+import { SET_SELECT_VALUE, SET_DATE } from '../types/testFormActionTypes';
 
 const initialState = {
     firstName: 'Paul',
@@ -6,14 +6,17 @@ const initialState = {
     location: 'Bhubaneswar',
     street: '',
     city: '',
+    date: ''
 }
 
-export const formReducer = (state=initialState, action) => {
-  switch(action.type){
-    case SET_SELECT_VALUE:
-        return Object.assign({}, state, {city: action.payload});
-    default:
-        return state;
-  }
+export const formReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case SET_SELECT_VALUE:
+            return Object.assign({}, state, { city: action.payload });
+        case SET_DATE:
+            return Object.assign({}, state, { date: action.payload });
+        default:
+            return state;
+    }
     return state
 }
