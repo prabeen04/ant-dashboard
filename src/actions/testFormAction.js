bfd92d91ca3dec4225077656d7866b69f56324af
Prabeen04 <prabeen.strange@gmail.com>
2018-05-11 21:24:23 +0530

    setDate action and constant types created

--- src/actions/testFormAction.js
+++ src/actions/testFormAction.js
@@ -1,7 +1,13 @@
-import { SET_SELECT_VALUE } from '../types/testFormActionTypes';
+import { SET_SELECT_VALUE, SET_DATE } from '../types/testFormActionTypes';
 export const setSelectValue = value => dispatch => {
     return dispatch({
         type: SET_SELECT_VALUE,
         payload: value
+    })
+}
+export const setDate = date => dispatch => {
+    return dispatch({
+        type: SET_DATE,
+        payload: date
     })
 }
--- src/types/testFormActionTypes.js
+++ src/types/testFormActionTypes.js
@@ -1,1 +1,2 @@
 export const SET_SELECT_VALUE = "SET_SELECT_VALUE";
+export const SET_DATE = "SET_DATE";
