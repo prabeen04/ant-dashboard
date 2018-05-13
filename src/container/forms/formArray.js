8beec78f2ff49fcb3f5f46e370dea682046a40fe
Prabeen04 <prabeen.strange@gmail.com>
2018-05-13 11:24:23 +0530

    add remove functions added

--- src/container/forms/formArray.js
+++ src/container/forms/formArray.js
@@ -1,16 +1,51 @@
 import React, { Component } from 'react';
 import { Form, Input, Icon, Button  } from 'antd';
 const FormItem = Form.Item;
-
+let uuid = 0;
 class FormArray extends Component {
     constructor(props){
         super(props)
-        this.handleSubmit = this.handleSubmit.bind(this);    
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.add = this.add.bind(this);
+        this.remove = this.remove.bind(this);       
     }
   
-  handleSubmit = ()=>{
-    console.log('handlesubmit called')
-  }  
+    remove = (k) => {
+        const { form } = this.props;
+        // can use data-binding to get
+        const keys = form.getFieldValue('keys');
+        // We need at least one passenger
+        if (keys.length === 1) {
+          return;
+        }
+    
+        // can use data-binding to set
+        form.setFieldsValue({
+          keys: keys.filter(key => key !== k),
+        });
+      }
+    
+      add = () => {
+        const { form } = this.props;
+        // can use data-binding to get
+        const keys = form.getFieldValue('keys');
+        const nextKeys = keys.concat(uuid);
+        uuid++;
+        // can use data-binding to set
+        // important! notify form to detect changes
+        form.setFieldsValue({
+          keys: nextKeys,
+        });
+      }
+    
+      handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+          if (!err) {
+            console.log('Received values of form: ', values);
+          }
+        });
+      } 
   render() {
     const { getFieldDecorator } = this.props.form ;  
     return (
