556a503f6bf510c402be4ec5ee0f4a6b320102b6
Prabeen04 <prabeen.strange@gmail.com>
2018-05-13 10:47:56 +0530

    FormArray component created

---
+++ src/container/forms/formArray.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Form } from 'antd';
+const FortItem = Form.Item;
+
+class FormArray extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+FormArray = Form.create()(FormArray)
+export default FormArray;
