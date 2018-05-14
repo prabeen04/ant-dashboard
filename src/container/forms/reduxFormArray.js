28548e92f9625b1b09eaf2b8f9204a08084768ff
Prabeen04 <prabeen.strange@gmail.com>
2018-05-14 23:49:04 +0530

    previous error messages removed

--- src/container/forms/reduxFormArray.js
+++ src/container/forms/reduxFormArray.js
@@ -167,32 +167,6 @@ const validate = values => {
           errors.members = membersArrayErrors;
         }
       }
-
-
-    if(!values.select1){
-        console.log('please select select1')
-        errors.select1 = "select1 is required"
-    }
-    if(!values.date){
-        console.log('please select date')
-        errors.date = "date is required"
-    }
-    if(!values.date){
-        console.log('please select date')
-        errors.date = "date is required"
-    }
-    if(!values.description){
-        console.log('please select description')
-        errors.description = "description is required"
-    }
-    if(!values.field1){
-        console.log('please select field1')
-        errors.field1 = "field1 is required"
-    }
-    if(!values.field2){
-        console.log('please select field2')
-        errors.field2 = "field2 is required"
-    }
     return errors;
 }
 ReduxFormArray = reduxForm({
