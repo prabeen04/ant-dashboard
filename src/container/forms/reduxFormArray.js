6365dcb322e4d4de7d0386e14c9c812d960fec29
Prabeen04 <prabeen.strange@gmail.com>
2018-05-14 19:48:32 +0530

    input field creator function created

--- src/container/forms/reduxFormArray.js
+++ src/container/forms/reduxFormArray.js
@@ -2,30 +2,31 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { Field, reduxForm } from 'redux-form';
+import { Field, FieldArray, reduxForm } from 'redux-form';
 
- class ReduxFormArray extends Component {
+class ReduxFormArray extends Component {
 
     render() {
-    return (
-      <div>
-        <h3>ReduxFormArray Component</h3>
-      </div>
-    )
-  }
+        return (
+            <div>
+                <h3>ReduxFormArray Component</h3>
+            </div>
+        )
+    }
 }
 
 ReduxFormArray = reduxForm({
     form: 'formArray'
 })(ReduxFormArray)
+
 const mapStateToProps = (state) => ({
-  
+
 })
 
-const mapDispatchToProps = dispatch =>{
-  return bindActionCreators({
+const mapDispatchToProps = dispatch => {
+    return bindActionCreators({
 
-  },dispatch)
+    }, dispatch)
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(ReduxFormArray)
