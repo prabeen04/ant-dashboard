f27e7957f2a6e9383bf4adb72d1c1c4b288ec299
Prabeen04 <prabeen.strange@gmail.com>
2018-05-20 23:52:36 +0530

    button and state removed

--- src/container/forms/exampleForm.js
+++ src/container/forms/exampleForm.js
@@ -3,18 +3,12 @@ import React, { Component } from 'react'
 class ExampleForm extends Component {
     constructor(props){
         super(props)
-       this.state = {
-           fields: [1, 2],
-
-       } 
     }
   
   render() {
-    console.log(this.state)
     return (
       <div>
         <h2>ExampleForm Component</h2>
-        <button onClick={(e)=>this.handleClick(e)}>Add</button>
       </div>
     )
   }
