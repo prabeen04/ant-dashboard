f0f520f5ebd2823c600dbf51a90bcab0cc3d3d6d
Prabeen04 <prabeen.strange@gmail.com>
2018-05-23 09:30:59 +0530

    more input fields and setState created

--- src/container/forms/exampleForm.js
+++ src/container/forms/exampleForm.js
@@ -1,31 +1,45 @@
 import React from 'react'
 
 class ExampleForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      team: '',
+      noOfPlayers: '',
+      captain: '',
+      goalkeeper: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange = (e) => {
+    console.log(e.target.name)
+    this.setState({
+      [e.target.name] : e.target.value
+    })
+  }
   render() {
-    constructor(props){
-      super(props)
-      this.state = {
-        team: '',
-        noOfPlayers: '',
-        captain: '',
-        goalkeeper: ''
-      }
-
-      this.handleChange = this.handleChange.bind(this);
-    }
-
-    handleChange = (e) =>{
-      console.log(e)
-    }
     return (
       <div>
         <h3>Premier League Form</h3>
         <form action="">
-        <div>
-          <label htmlFor="">team</label>
-          <input type="text" name="team" value={this.state.team} onChange={this.handleChange}/>
-        </div>
-        </form>  
+          <div>
+            <label htmlFor="">team</label>
+            <input type="text" name="team" value={this.state.team} onChange={this.handleChange} />
+          </div>
+          <div>
+            <label htmlFor="">No Of Players</label>
+            <input type="number" name="noOfPlayers" value={this.state.noOfPlayers} onChange={this.handleChange} />
+          </div>
+          <div>
+            <label htmlFor="">captain</label>
+            <input type="number" name="captain" value={this.state.captain} onChange={this.handleChange} />
+          </div>
+          <div>
+            <label htmlFor="">Goalkeeper</label>
+            <input type="number" name="goalkeeper" value={this.state.goalkeeper} onChange={this.handleChange} />
+          </div>
+        </form>
 
       </div>
     )
