86875517a0144dcca33ae0385299946968db33dd
Prabeen04 <prabeen.strange@gmail.com>
2018-05-23 09:23:59 +0530

    input field and change handler created

--- src/container/forms/exampleForm.js
+++ src/container/forms/exampleForm.js
@@ -1,14 +1,35 @@
 import React from 'react'
 
 class ExampleForm extends React.Component {
-  render(){
+  render() {
+    constructor(props){
+      super(props)
+      this.state = {
+        team: '',
+        noOfPlayers: '',
+        captain: '',
+        goalkeeper: ''
+      }
+
+      this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange = (e) =>{
+      console.log(e)
+    }
     return (
-    <div>
-      <h3>ExampleComponent</h3>
-     
-    </div>
-  )
+      <div>
+        <h3>Premier League Form</h3>
+        <form action="">
+        <div>
+          <label htmlFor="">team</label>
+          <input type="text" name="team" value={this.state.team} onChange={this.handleChange}/>
+        </div>
+        </form>  
+
+      </div>
+    )
   }
-  
+
 }
 export default ExampleForm;
