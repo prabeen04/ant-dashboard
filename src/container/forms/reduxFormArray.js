df356311eb7ab14dadccae004b4a66776043cff4
Prabeen04 <prabeen.strange@gmail.com>
2018-05-24 23:58:47 +0530

    CWRP removed

--- src/container/forms/reduxFormArray.js
+++ src/container/forms/reduxFormArray.js
@@ -317,10 +317,7 @@ class ReduxFormArray extends Component {
             </div>
         </div>
     );
-    componentWillReceiveProps(nextProps, other){
-        console.log(nextProps)
-        console.log(other)
-    }
+
     render() {
         const { handleSubmit, pristine, reset, submitting, change } = this.props;
         return (
