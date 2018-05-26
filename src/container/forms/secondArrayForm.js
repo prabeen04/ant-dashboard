e86444a03ef7ad632af85a1afe011930829b9049
Prabeen04 <prabeen.strange@gmail.com>
2018-05-26 11:04:08 +0530

    renderInput method created

--- src/container/forms/secondArrayForm.js
+++ src/container/forms/secondArrayForm.js
@@ -2,9 +2,27 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Field, reduxForm, FieldArray } from 'redux-form';
+import { Select, Input, Button, Icon, } from 'antd';
+const Option = Select.Option;
 
 class SecondArrayForm extends Component {
+    constructor(props){
+        super(props)
+      
+        this.renderInput = this.renderInput.bind(this);
+        this.renderSelect = this.renderSelect.bind(this);
+    }
 
+    renderInput = ({input, label, type, meta: {touched, error}, ...custom}) => {
+        return <div>
+            <label><label>
+            <Input 
+            {...input} 
+            {...custom} 
+            />   
+            {touched && error && <span>{error}</span>} 
+        </div>
+    }
   render() {
     return (
       <div>
