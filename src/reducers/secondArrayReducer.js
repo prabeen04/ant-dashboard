1052385441c29a733090849ab1f1ccce20f5eaa7
Prabeen04 <prabeen.strange@gmail.com>
2018-05-28 23:30:58 +0530

    deafult select value added in reducer

--- src/reducers/secondArrayReducer.js
+++ src/reducers/secondArrayReducer.js
@@ -1,21 +1,27 @@
 import { GET_PRODUCTS } from '../types/secondArrayActionTypes';
 const initialState = {
     calculatedValue: 0.0,
-   teams: [{
-        team: 'manutd',
-        captain: 'valencia',
-        trophy: 50
-    },
-    {
-        team: 'arsenal',
-        captain: 'wilshere',
-        trophy: 15
-    },
-    {
-        team: 'chelsea',
-        captain: 'cahil',
-        trophy: 10
-    }]}
+    teams: [
+        {
+            team: 'select',
+            captain: '',
+            trophy: 0
+        }, {
+            team: 'manutd',
+            captain: 'valencia',
+            trophy: 50
+        },
+        {
+            team: 'arsenal',
+            captain: 'wilshere',
+            trophy: 15
+        },
+        {
+            team: 'chelsea',
+            captain: 'cahil',
+            trophy: 10
+        }]
+}
 
 export const secondArrayReducer = (state = initialState, action) => {
     switch (action.types) {
