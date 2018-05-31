6b5c3163e1d6ca415e0226de3ecb540026dfa366
Prabeen04 <prabeen.strange@gmail.com>
2018-05-31 21:34:20 +0530

    tooltip added in profileHeader icons

--- src/container/profile/profileHeader.js
+++ src/container/profile/profileHeader.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import './profile.css';
-import { Icon } from 'antd';
+import { Icon, Tooltip } from 'antd';
 
 class ProfileHeader extends Component {
   render() {
     return (
       <div className="profile-header">
         <div className="view-icons">
+        <Tooltip title="Grid View">
             <Icon type="appstore-o" style={{ fontSize: 25}} />
+            </Tooltip>
+            <Tooltip title="List View">
             <Icon type="profile" style={{ fontSize: 25, marginLeft: '0.5rem'}} />
+            </Tooltip>
         </div>
         <div className="setting-icons">
+        <Tooltip title="Settings">
             <Icon type="setting" style={{ fontSize: 25}} />
+            </Tooltip>
         </div>
       </div>
     )
