59cb64256936e2dab1686966d43cf6483bcfec22
Prabeen04 <prabeen.strange@gmail.com>
2018-05-31 21:09:50 +0530

    ProfileHeader component creatd

--- src/container/profile/profile.js
+++ src/container/profile/profile.js
@@ -1,24 +1,28 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import ProfileList from './profileList'
-import AddProfile from './addProfile'
-import EditProfile from './editProfile'
-import ProfileView from './profileView'
-import './profile.css'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import ProfileList from './profileList';
+import AddProfile from './addProfile';
+import EditProfile from './editProfile';
+import ProfileView from './profileView';
+import ProfileHeader from './profileHeader';
+import './profile.css';
 
 class Profile extends Component {
   render() {
-    
-    return (
-      <div className="profile-wrapper">
 
-        <ProfileList />
-        <div className="profile-action">
-          {this.props.showAddProfile && <AddProfile />}
-          {this.props.showEditProfile && <EditProfile />}
-          {this.props.showViewProfile && <ProfileView />}
+    return (
+      <div>
+        <ProfileHeader />
+        <div className="profile-wrapper">
+          <ProfileList />
+          <div className="profile-action">
+            {this.props.showAddProfile && <AddProfile />}
+            {this.props.showEditProfile && <EditProfile />}
+            {this.props.showViewProfile && <ProfileView />}
+          </div>
         </div>
       </div>
+
     )
   }
 }
---
+++ src/container/profile/profileHeader.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './profile.css';
+
+class ProfileHeader extends Component {
+  render() {
+    return (
+      <div>
+        <h2>ProfileHeader Component</h2>
+      </div>
+    )
+  }
+}
+export default ProfileHeader;
