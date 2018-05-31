c579638cc9962b451f802c011701ea65d5b7a6ae
Prabeen04 <prabeen.strange@gmail.com>
2018-06-01 00:14:39 +0530

    checkbox added in list

--- src/components/tabs/settingTab.js
+++ src/components/tabs/settingTab.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
-import { Tabs, Icon, Button, List } from 'antd';
+import { Tabs, Icon, Button, List, Checkbox  } from 'antd';
 const TabPane = Tabs.TabPane;
 const data = [
-    'Racing car sprays burning fuel into crowd.',
-    'Japanese princess to wed commoner.',
-    'Australian walks 100km after outback crash.',
-    'Man charged over missing wedding girl.',
-    'Los Angeles battles huge wildfires.',
-  ];
+    <Checkbox>Manchester United</Checkbox>,
+    <Checkbox>Arsenal</Checkbox>,
+    <Checkbox>Chelsea</Checkbox>,
+    <Checkbox>Manchester City</Checkbox>,
+    <Checkbox>Liverpool</Checkbox>,
+    <Checkbox>Tottenham</Checkbox>
+];
 class SettingTab extends Component {
     constructor(props) {
         super(props)
@@ -18,13 +19,15 @@ class SettingTab extends Component {
                 <Tabs defaultActiveKey="1">
                     <TabPane tab={<span><Icon type="apple" />Tab 1</span>} key="1">
                         <List
-                        bordered
-                        dataSource={data}
-                        renderItem={item => (<List.Item>{item}</List.Item>)}
+                            size="small"
+                            header={<div>Header</div>}
+                            footer={<div style={{textAlign: 'right'}}> <Button
+                                type="primary"
+                                onClick={this.props.hide}>Close</Button></div>}
+                            dataSource={data}
+                            renderItem={item => (<List.Item>{item}</List.Item>)}
                         />
-                        <Button
-                            type="primary"
-                            onClick={this.props.hide}>Close</Button>
+
                     </TabPane>
                     <TabPane tab={<span><Icon type="android" />Tab 2</span>} key="2">
                         Tab 2
