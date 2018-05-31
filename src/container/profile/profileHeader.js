dbe5bdc7d461bfe80fc4b40dba66b98919e1d130
Prabeen04 <prabeen.strange@gmail.com>
2018-05-31 21:42:43 +0530

    onClick method added to In=cons

--- src/container/profile/profileHeader.js
+++ src/container/profile/profileHeader.js
@@ -3,24 +3,36 @@ import './profile.css';
 import { Icon, Tooltip } from 'antd';
 
 class ProfileHeader extends Component {
-  render() {
-    return (
-      <div className="profile-header">
-        <div className="view-icons">
-        <Tooltip title="Grid View">
-            <Icon type="appstore-o" style={{ fontSize: 25}} />
-            </Tooltip>
-            <Tooltip title="List View">
-            <Icon type="profile" style={{ fontSize: 25, marginLeft: '0.5rem'}} />
-            </Tooltip>
-        </div>
-        <div className="setting-icons">
-        <Tooltip title="Settings">
-            <Icon type="setting" style={{ fontSize: 25}} />
-            </Tooltip>
-        </div>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className="profile-header">
+                <div className="view-icons">
+                    <Tooltip title="Grid View">
+                        <Icon
+                            style={{ fontSize: 25, cursor: 'pointer' }}
+                            type="appstore-o"
+                            onClick={()=>console.log('grid view clicked')}
+                        />
+                    </Tooltip>
+                    <Tooltip title="List View">
+                        <Icon
+                            style={{ fontSize: 25, marginLeft: '0.5rem', cursor: 'pointer' }}
+                            type="profile"
+                            onClick={()=>console.log('list view clicked')}
+                        />
+                    </Tooltip>
+                </div>
+                <div className="setting-icons">
+                    <Tooltip title="Settings">
+                        <Icon
+                            style={{ fontSize: 25, cursor: 'pointer' }}
+                            type="setting"
+                            onClick={()=>console.log('settings view clicked')}
+                        />
+                    </Tooltip>
+                </div>
+            </div>
+        )
+    }
 }
 export default ProfileHeader;
