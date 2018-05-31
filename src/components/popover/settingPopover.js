fab84c632b143d63d8a998e2abde5ac38ec47154
Prabeen04 <prabeen.strange@gmail.com>
2018-05-31 23:11:08 +0530

    settingPopover component created

---
+++ src/components/popover/settingPopover.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Icon, Popover Card, Button } from 'antd';
+
+class SettingPopover extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+export default SettingPopover
