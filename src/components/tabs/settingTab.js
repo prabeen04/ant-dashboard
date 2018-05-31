9a5d14fa7491293725a1ca4dffc0a0546fca8b4f
Prabeen04 <prabeen.strange@gmail.com>
2018-06-01 00:07:59 +0530

    list added in settings tab

--- src/components/tabs/settingTab.js
+++ src/components/tabs/settingTab.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react'
-import { Tabs, Icon, Button } from 'antd';
+import { Tabs, Icon, Button, List } from 'antd';
 const TabPane = Tabs.TabPane;
-
+const data = [
+    'Racing car sprays burning fuel into crowd.',
+    'Japanese princess to wed commoner.',
+    'Australian walks 100km after outback crash.',
+    'Man charged over missing wedding girl.',
+    'Los Angeles battles huge wildfires.',
+  ];
 class SettingTab extends Component {
     constructor(props) {
         super(props)
@@ -11,7 +17,11 @@ class SettingTab extends Component {
             <div>
                 <Tabs defaultActiveKey="1">
                     <TabPane tab={<span><Icon type="apple" />Tab 1</span>} key="1">
-  
+                        <List
+                        bordered
+                        dataSource={data}
+                        renderItem={item => (<List.Item>{item}</List.Item>)}
+                        />
                         <Button
                             type="primary"
                             onClick={this.props.hide}>Close</Button>
