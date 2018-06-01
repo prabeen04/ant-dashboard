ba59350c02981d84c782c1be5d067ab9504e35d7
Prabeen04 <prabeen.strange@gmail.com>
2018-06-01 21:34:59 +0530

    demoselector stuff

--- src/selectors/demoSelector.js
+++ src/selectors/demoSelector.js
@@ -3,14 +3,7 @@ import { createSelector } from 'reselect'
 const getVisibilityFilter = (state) => state.visibilityFilter
 const getTodos = (state) => state.todos
 
-export const getVisibleTodos = createSelector(
-  [ getVisibilityFilter, getTodos ],
-  (visibilityFilter, todos) => {
-    switch (visibilityFilter) {
-      case 'SHOW_ALL':
-        return todos
-      case 'SHOW_COMPLETED':
-        return todos.filter(t => t.title = 'martial')
-    }
-  }
+export const getVisibleProfiles = createSelector(
+  [profiles ],
+  (profile) => profile
 )
