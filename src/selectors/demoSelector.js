43075ac585a1ee31cee263883cd994f5c947b277
Prabeen04 <prabeen.strange@gmail.com>
2018-06-01 21:54:35 +0530

    filter function added in demoSelector

--- src/selectors/demoSelector.js
+++ src/selectors/demoSelector.js
@@ -3,6 +3,9 @@ import { createSelector } from 'reselect'
 const getProfiles = (state) => state.profileReducer.profiles
 
 export const getProfilesState = createSelector(
-  [getProfiles ],
-  (profile) => profile
+    [getProfiles],
+    (profile) => {
+        console.log('inside demoSelector')
+        return profile.filter((profile) => profile.name.startsWith('m'))
+    }
 )
