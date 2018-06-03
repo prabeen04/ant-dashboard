53ef4cc5a0d31a8fca81f947c5a4deb0167f99f4
Prabeen04 <prabeen.strange@gmail.com>
2018-06-03 11:43:25 +0530

    BigCalendar imported and added in CalendarInstance

--- src/container/calendar/calendarInstace.js
+++ src/container/calendar/calendarInstace.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react'
+import BigCalendar from 'react-big-calendar';
+import moment from 'moment';
 
 class CalendarInstance extends Component {
   render() {
     return (
       <div>
-        <h4>Calendar Instance Component</h4>
+        <BigCalendar
+            // className="red-background"
+            style={{ flexBasis: '70%' }}
+            // events={this.props.events ?this.props.events :[] }
+            defaultDate={new Date()}
+            startAccessor={(event) => new Date(event.start)}
+            endAccessor={(event) => new Date(event.end)}
+            selectable={true}
+            popup={true}
+            // formats={formats}
+            // onSelectSlot={(slot) => this.handleOk(slot)}
+            onSelecting={(range) => console.log(range)}
+            onSelectEvent={(event, e) =>{
+              alert(event.description)
+              console.log(e)
+            } }
+            // eventPropGetter={event => ({className: 'e-'+event.user.toLowerCase()})}
+            slotPropGetter={date =>({className: 'red-background'})}
+          />
       </div>
     )
   }
