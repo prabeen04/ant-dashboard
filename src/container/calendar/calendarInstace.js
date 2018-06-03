ba2684b69a96efca60c5ff0249a4d90c630714bd
Prabeen04 <prabeen.strange@gmail.com>
2018-06-03 11:41:24 +0530

    CalendarInstance Component Created

---
+++ src/container/calendar/calendarInstace.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class CalendarInstance extends Component {
+  render() {
+    return (
+      <div>
+        <h4>Calendar Instance Component</h4>
+      </div>
+    )
+  }
+}
+export default CalendarInstance;
