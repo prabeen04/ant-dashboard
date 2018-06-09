79a7277b71a84c5bf16111701fcf46b9f937b621
Prabeen04 <prabeen.strange@gmail.com>
2018-06-09 11:04:36 +0530

    tabs added in settings

--- src/container/settings/settings.js
+++ src/container/settings/settings.js
@@ -1,27 +1,33 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import { Tabs, Icon } from 'antd';
+const TabPane = Tabs.TabPane;
 
 class Settings extends Component {
-  static propTypes = {
-    prop: PropTypes
-  }
 
-  render() {
-    return (
-      <div>
-        <h1>Settings Component</h1>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <Tabs defaultActiveKey="2">
+                    <TabPane tab={<span><Icon type="apple" />Tab 1</span>} key="1">
+                        Tab 1
+                    </TabPane>
+                    <TabPane tab={<span><Icon type="android" />Tab 2</span>} key="2">
+                        Tab 2
+                    </TabPane>
+                </Tabs>
+            </div>
+        )
+    }
 }
 
 const mapStateToProps = (state) => ({
-  
+
 })
 
 const mapDispatchToProps = {
-  
+
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Settings)
