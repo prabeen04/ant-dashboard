499a4691baaa2df908d081072e38f1e189ff02ce
Prabeen04 <prabeen.strange@gmail.com>
2018-06-11 21:03:35 +0530

    dashboard action created

---
+++ src/actions/dashboardAction.js
@@ -0,0 +1,7 @@
+import { GET_JUMPSTART_LIST } from '../types/dashboardActionTypes';
+
+export const getjumpstartList = dispatch => {
+    return dispatch({
+        type: GET_JUMPSTART_LIST
+    })
+}
