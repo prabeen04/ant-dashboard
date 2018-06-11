9f0c2dffd4d3fcbb2bb7ef22dc8e957655ce273f
Prabeen04 <prabeen.strange@gmail.com>
2018-06-11 23:02:12 +0530

    dashboard jumpstart rendered from reducer

--- src/container/dashboard/jumpStart.js
+++ src/container/dashboard/jumpStart.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Bar, Line } from 'react-chartjs-2';
 import { Card } from 'antd';
-import { jumpStartData } from './jumpStartData';
 class JumpStartBox extends Component {
     constructor(props) {
         super(props)
@@ -10,19 +10,24 @@ class JumpStartBox extends Component {
     componentDidMount() {
     }
     render() {
-        let renderJumpStartBoxes = jumpStartData.filter((data) => data.visible)
+        let renderJumpStartBoxes = this.props.jumpStartData.filter((data) => data.visible)
             .map((data) => <Card key={data.id} style={{ margin: '1rem', justifyContent: 'space-evenly', minWidth: 200 }}>
-            <div>
-                <h3>{data.title}</h3>
-                <h2>{data.value}</h2>
-            </div>
-        </Card>)
+                <div>
+                    <h3>{data.title}</h3>
+                    <h2>{data.value}</h2>
+                </div>
+            </Card>)
 
         return (
-            <div className="flex-container" style={{ flexGrow: 1}}>
+            <div className="flex-container" style={{ flexGrow: 1 }}>
                 {renderJumpStartBoxes}
             </div>
         )
     }
 }
-export default JumpStartBox;
+const mapStateToProps = (state) => {
+    return {
+        jumpStartData: state.dashboardReducer.jumpStartData
+    }
+}
+export default connect(mapStateToProps)(JumpStartBox);
