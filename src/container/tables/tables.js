826edcd10ecf2abd223b6b7b722ab75a91a52de6
Prabeen04 <prabeen.strange@gmail.com>
2018-06-12 20:10:34 +0530

    two rows removed

--- src/container/tables/tables.js
+++ src/container/tables/tables.js
@@ -12,14 +12,6 @@ class Tables extends Component {
         }, {
             Header: 'Age',
             accessor: 'age',
-           Cell: props => <span className='number'>{props.value}</span> // Custom cell components!
-        }, {
-            id: 'friendName', // Required because our accessor is not a string
-            Header: 'Friend Name',
-           // accessor: d => d.friend.name // Custom value accessors!
-        }, {
-            Header: props => <span>Friend Age</span>, // Custom header components!
-            accessor: 'friend.age'
         }]
         return (
             <div className="table-container">
