4009f44710271f7ee96c89f6d3950ecad0bbc8bf
Prabeen04 <prabeen.strange@gmail.com>
2018-06-12 20:09:15 +0530

    table column stuff

--- src/container/tables/tables.js
+++ src/container/tables/tables.js
@@ -12,11 +12,11 @@ class Tables extends Component {
         }, {
             Header: 'Age',
             accessor: 'age',
-            Cell: props => <span className='number'>{props.value}</span> // Custom cell components!
+           Cell: props => <span className='number'>{props.value}</span> // Custom cell components!
         }, {
             id: 'friendName', // Required because our accessor is not a string
             Header: 'Friend Name',
-            accessor: d => d.friend.name // Custom value accessors!
+           // accessor: d => d.friend.name // Custom value accessors!
         }, {
             Header: props => <span>Friend Age</span>, // Custom header components!
             accessor: 'friend.age'
