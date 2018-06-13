90d198e1c7c31be23edf5bc8af7d7b81d69d6c87
Prabeen04 <prabeen.strange@gmail.com>
2018-06-13 21:54:56 +0530

    more data added to tableData

--- src/container/tables/tableData.js
+++ src/container/tables/tableData.js
@@ -3,28 +3,42 @@ import React from 'react';
 export const data = [{
     name: 'Tanner Linsley',
     age: 26,
+    pofession: 'React Developer',
+    location: 'Miami'
+},
+{
+    name: 'asfasf iu',
+    age: 36,
+    pofession: 'React Developer',
+    location: 'Miami'
+},
+{
+    name: 'Tanner Linsley',
+    age: 36,
+    pofession: 'React Developer',
+    location: 'Miami'
 },
 {
     name: 'asfasf Linsley',
     age: 36,
+    pofession: 'React Developer',
+    location: 'Miami'
 },
 {
     name: 'Tanner Linsley',
     age: 26,
+    pofession: 'React Developer',
+    location: 'Miami'
 },
 {
     name: 'asfasf Linsley',
     age: 36,
-},
-{
-    name: 'Tanner Linsley',
-    age: 26,
-},
-{
-    name: 'asfasf Linsley',
-    age: 36,
+    pofession: 'React Developer',
+    location: 'Miami'
 },
 {
     name: 'Tannetyrtuer Linsley',
     age: 16,
+    pofession: 'React Developer',
+    location: 'Miami'
 }]
