053a050af694b2431321e2998c5d399b9e5fe903
Prabeen04 <prabeen.strange@gmail.com>
2018-06-13 21:49:18 +0530

    tableData cleaned

--- src/container/tables/tableData.js
+++ src/container/tables/tableData.js
@@ -3,56 +3,28 @@ import React from 'react';
 export const data = [{
     name: 'Tanner Linsley',
     age: 26,
-    friend: {
-        name: 'Jason Maurer',
-        age: 23,
-    }
 },
 {
     name: 'asfasf Linsley',
     age: 36,
-    friend: {
-        name: 'Jason Maurer',
-        age: 23,
-    }
 },
 {
     name: 'Tanner Linsley',
     age: 26,
-    friend: {
-        name: 'Jason Maurer',
-        age: 23,
-    }
 },
 {
     name: 'asfasf Linsley',
     age: 36,
-    friend: {
-        name: 'Jason Maurer',
-        age: 23,
-    }
 },
 {
     name: 'Tanner Linsley',
     age: 26,
-    friend: {
-        name: 'Jason Maurer',
-        age: 23,
-    }
 },
 {
     name: 'asfasf Linsley',
     age: 36,
-    friend: {
-        name: 'Jason Maurer',
-        age: 23,
-    }
 },
 {
     name: 'Tannetyrtuer Linsley',
     age: 16,
-    friend: {
-        name: 'Jason Maurer',
-        age: 23,
-    }
 }]
