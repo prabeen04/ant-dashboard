44d51fead1e8bca4a7bdc29d4871000eb94311dd
Prabeen04 <prabeen.strange@gmail.com>
2018-06-16 13:46:17 +0530

    profile component made stateless

--- src/container/profile/profile.js
+++ src/container/profile/profile.js
@@ -5,30 +5,27 @@ import AddProfile from './addProfile';
 import EditProfile from './editProfile';
 import ProfileView from './profileView';
 import ProfileHeader from './profileHeader';
-import PaginationComponent  from '../../components/common/pagination/pagination';
+import PaginationComponent from '../../components/common/pagination/pagination';
 import './profile.css';
 
-class Profile extends Component {
-  render() {
-
-    return (
-      <div className="pr">
-        <ProfileHeader />
-        <div className="profile-wrapper">
-          <div className="profile-view">
-            <ProfileList />          
-          <PaginationComponent/>
-          </div>
-          <div className="profile-action">
-            {this.props.showAddProfile && <AddProfile />}
-            {this.props.showEditProfile && <EditProfile />}
-            {this.props.showViewProfile && <ProfileView />}
-          </div>
+const Profile = (props) => {
+  return (
+    <div className="pr">
+      <ProfileHeader />
+      <div className="profile-wrapper">
+        <div className="profile-view">
+          <ProfileList />
+          <PaginationComponent />
+        </div>
+        <div className="profile-action">
+          {props.showAddProfile && <AddProfile />}
+          {props.showEditProfile && <EditProfile />}
+          {props.showViewProfile && <ProfileView />}
         </div>
       </div>
+    </div>
 
-    )
-  }
+  )
 }
 const mapStateToProps = (state) => {
   return {
