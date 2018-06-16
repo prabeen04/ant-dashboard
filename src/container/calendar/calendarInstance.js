9e736a443cb863c92a8452f0929903e1f8a8a620
Prabeen04 <prabeen.strange@gmail.com>
2018-06-16 13:49:18 +0530

    calendar instance made stateless component

--- src/container/calendar/calendarInstance.js
+++ src/container/calendar/calendarInstance.js
@@ -5,39 +5,34 @@ import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 BigCalendar.setLocalizer(BigCalendar.momentLocalizer(moment))
 
-class CalendarInstance extends Component {
-  constructor(props){
-    super(props)
-  }
-  render() {
-    return (
-      <div style={{ flexBasis: '70%', height: 500 }}>
-        <BigCalendar
-            style={{ flexBasis: '70%' }}
-            events={this.props.events ?this.props.events :[] }
-            defaultDate={this.props.defaultDate || new Date()}
-            startAccessor={(event) => new Date(event.start)}
-            endAccessor={(event) => new Date(event.end)}
-            selectable={this.props.selectable}
-            popup={this.props.popup}
-            onSelecting={(range) => console.log(range)}
-            onSelectSlot={(range) =>{
-              this.props.onSelectSlot(range)
-            }}
-            onSelectEvent={this.props.onSelectEvent()}
-            slotPropGetter={(date) =>{
-              console.log(date)
-            }}
-            // eventPropGetter={(a,b,c,d)=>{
-            //   console.log(a)
-            //   console.log(b)
-            //   console.log(c)
-            //   console.log(d)
-            //   // ({className: 'red-background'})
-            // }}
-          />
-      </div>
-    )
-  }
+const CalendarInstance = (props) => {
+  return (
+    <div style={{ flexBasis: '70%', height: 500 }}>
+      <BigCalendar
+        style={{ flexBasis: '70%' }}
+        events={props.events ? props.events : []}
+        defaultDate={props.defaultDate || new Date()}
+        startAccessor={(event) => new Date(event.start)}
+        endAccessor={(event) => new Date(event.end)}
+        selectable={props.selectable}
+        popup={props.popup}
+        onSelecting={(range) => console.log(range)}
+        onSelectSlot={(range) => {
+          props.onSelectSlot(range)
+        }}
+        onSelectEvent={props.onSelectEvent()}
+        slotPropGetter={(date) => {
+          console.log(date)
+        }}
+      // eventPropGetter={(a,b,c,d)=>{
+      //   console.log(a)
+      //   console.log(b)
+      //   console.log(c)
+      //   console.log(d)
+      //   // ({className: 'red-background'})
+      // }}
+      />
+    </div>
+  )
 }
 export default CalendarInstance;
--- src/container/profile/profile.js
+++ src/container/profile/profile.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import ProfileList from './profileList';
 import AddProfile from './addProfile';
