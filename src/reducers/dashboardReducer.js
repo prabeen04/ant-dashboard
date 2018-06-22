f8ede09b91bfc7e3171b429e86ab31638ddcc35f
Prabeen04 <prabeen.strange@gmail.com>
2018-06-22 12:51:57 +0530

    nested spread operator in dashboard reducer

--- src/reducers/dashboardReducer.js
+++ src/reducers/dashboardReducer.js
@@ -47,8 +47,8 @@ export const dashboardReducer = (state = initialState, action) => {
                 }]
             };
         case SET_JUMPSTART_LIST:
-        console.log(state.jumpStartData.map(data => data.id === action.payload.id ? action.payload : data))
-         return {...state}
+        console.log(...state,{ jumpstartData: state.jumpStartData.map(data => data.id === action.payload.id ? action.payload : data)})
+         return ...state,{ jumpstartData: state.jumpStartData.map(data => data.id === action.payload.id ? action.payload : data)}
   
         default:
             return state;
