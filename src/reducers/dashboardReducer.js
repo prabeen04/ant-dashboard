a94b8323cd55b75c2e29d9c620db6a3b657dd5d5
Prabeen04 <prabeen.strange@gmail.com>
2018-06-25 20:51:38 +0530

    google-places-autocomplete addef

--- src/reducers/dashboardReducer.js
+++ src/reducers/dashboardReducer.js
@@ -1,36 +1,36 @@
 import { GET_JUMPSTART_LIST, SET_JUMPSTART_LIST } from '../types/dashboardActionTypes';
-const initialState =  [
-        {
-            visible: false,
-            id: 1,
-            title: 'Title 1',
-            value: 100
-        },
-        {
-            visible: true,
-            id: 2,
-            title: 'Title 2',
-            value: 4500
-        },
-        {
-            visible: true,
-            id: 3,
-            title: 'Title 3',
-            value: 90
-        },
-        {
-            visible: true,
-            id: 4,
-            title: 'Title 4',
-            value: 45
-        },
-        {
-            visible: true,
-            id: 5,
-            title: ' Title 5',
-            value: 1100
-        },
-    ]
+const initialState = [
+    {
+        visible: false,
+        id: 1,
+        title: 'Title 1',
+        value: 100
+    },
+    {
+        visible: true,
+        id: 2,
+        title: 'Title 2',
+        value: 4500
+    },
+    {
+        visible: true,
+        id: 3,
+        title: 'Title 3',
+        value: 90
+    },
+    {
+        visible: true,
+        id: 4,
+        title: 'Title 4',
+        value: 45
+    },
+    {
+        visible: true,
+        id: 5,
+        title: ' Title 5',
+        value: 1100
+    },
+]
 export const dashboardReducer = (state = initialState, action) => {
     switch (action.type) {
         case GET_JUMPSTART_LIST:
@@ -43,11 +43,8 @@ export const dashboardReducer = (state = initialState, action) => {
                 }]
             };
         case SET_JUMPSTART_LIST:
-        console.log(
-            state.map(data => data.id === action.payload.id ? action.payload : data)
-           )
-         return [...state.map(data => data.id === action.payload.id ? action.payload : data)]
-  
+            return [...state.map(data => data.id === action.payload.id ? action.payload : data)];
+
         default:
             return state;
     }
