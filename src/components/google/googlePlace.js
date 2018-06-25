6e844e827ae63cde38c795f7cb53cb7e64465df6
Prabeen04 <prabeen.strange@gmail.com>
2018-06-25 21:01:31 +0530

    google aputocomplete stuff added

--- src/components/google/googlePlace.js
+++ src/components/google/googlePlace.js
@@ -5,6 +5,36 @@ class GooglePlace extends Component {
     return (
       <div>
           <h1>GooglePlace Component</h1>
+          <PlacesAutocomplete
+        value={this.state.address}
+        onChange={this.handleChange}
+        onSelect={this.handleSelect}
+      >
+        {({ getInputProps, suggestions, getSuggestionItemProps }) => (
+          <div>
+            <input
+              {...getInputProps({
+                placeholder: 'Search Places ...',
+                className: 'location-search-input'
+              })}
+            />
+            <div className="autocomplete-dropdown-container">
+              {suggestions.map(suggestion => {
+                const className = suggestion.active ? 'suggestion-item--active' : 'suggestion-item';
+                // inline style for demonstration purpose
+                const style = suggestion.active
+                            ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                            : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                return (
+                  <div {...getSuggestionItemProps(suggestion, { className, style })}>
+                    <span>{suggestion.description}</span>
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+        )}
+      </PlacesAutocomplete>
       </div>
     )
   }
