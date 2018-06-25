af6d43768d470458b3725ea3434dc7cfa51e7146
Prabeen04 <prabeen.strange@gmail.com>
2018-06-25 20:02:16 +0530

    bug fixes

--- src/reducers/dashboardReducer.js
+++ src/reducers/dashboardReducer.js
@@ -43,12 +43,10 @@ export const dashboardReducer = (state = initialState, action) => {
                 }]
             };
         case SET_JUMPSTART_LIST:
-        console.log({ 
-            ...state, jumpstartData: state.jumpStartData.map(data => data.id === action.payload.id ? action.payload : data)
-           })
-         return { 
-             ...state, jumpstartData: state.jumpStartData.map(data => data.id === action.payload.id ? action.payload : data)
-            };
+        console.log(
+            ...state, state.map(data => data.id === action.payload.id ? action.payload : data)
+           )
+         return [...state, state.map(data => data.id === action.payload.id ? action.payload : data)]
   
         default:
             return state;
