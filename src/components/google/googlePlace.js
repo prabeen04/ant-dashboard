2a0baebb4eddc26e9898ce1b5f929b87033a8061
Prabeen04 <prabeen.strange@gmail.com>
2018-06-25 21:02:35 +0530

    constructor and eventHandler added

--- src/components/google/googlePlace.js
+++ src/components/google/googlePlace.js
@@ -1,43 +1,59 @@
 import React, { Component } from 'react'
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete'
 
 class GooglePlace extends Component {
-  render() {
-    return (
-      <div>
-          <h1>GooglePlace Component</h1>
-          <PlacesAutocomplete
-        value={this.state.address}
-        onChange={this.handleChange}
-        onSelect={this.handleSelect}
-      >
-        {({ getInputProps, suggestions, getSuggestionItemProps }) => (
-          <div>
-            <input
-              {...getInputProps({
-                placeholder: 'Search Places ...',
-                className: 'location-search-input'
-              })}
-            />
-            <div className="autocomplete-dropdown-container">
-              {suggestions.map(suggestion => {
-                const className = suggestion.active ? 'suggestion-item--active' : 'suggestion-item';
-                // inline style for demonstration purpose
-                const style = suggestion.active
-                            ? { backgroundColor: '#fafafa', cursor: 'pointer' }
-                            : { backgroundColor: '#ffffff', cursor: 'pointer' };
-                return (
-                  <div {...getSuggestionItemProps(suggestion, { className, style })}>
-                    <span>{suggestion.description}</span>
-                  </div>
-                )
-              })}
+    constructor(props) {
+        super(props);
+        this.state = { address: '' }
+    }
+
+    handleChange = (address) => {
+        this.setState({ address })
+    }
+
+    handleSelect = (address) => {
+        geocodeByAddress(address)
+            .then(results => getLatLng(results[0]))
+            .then(latLng => console.log('Success', latLng))
+            .catch(error => console.error('Error', error))
+    }
+    render() {
+        return (
+            <div>
+                <h1>GooglePlace Component</h1>
+                <PlacesAutocomplete
+                    value={this.state.address}
+                    onChange={this.handleChange}
+                    onSelect={this.handleSelect}
+                >
+                    {({ getInputProps, suggestions, getSuggestionItemProps }) => (
+                        <div>
+                            <input
+                                {...getInputProps({
+                                    placeholder: 'Search Places ...',
+                                    className: 'location-search-input'
+                                })}
+                            />
+                            <div className="autocomplete-dropdown-container">
+                                {suggestions.map(suggestion => {
+                                    const className = suggestion.active ? 'suggestion-item--active' : 'suggestion-item';
+                                    // inline style for demonstration purpose
+                                    const style = suggestion.active
+                                        ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                                        : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                                    return (
+                                        <div {...getSuggestionItemProps(suggestion, { className, style })}>
+                                            <span>{suggestion.description}</span>
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                        </div>
+                    )}
+                </PlacesAutocomplete>
             </div>
-          </div>
-        )}
-      </PlacesAutocomplete>
-      </div>
-    )
-  }
+        )
+    }
 }
 
 export default GooglePlace;
