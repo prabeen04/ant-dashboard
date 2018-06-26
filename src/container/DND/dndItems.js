fcd6d9a817db724e4968fedb59bf1bf3c77ec0af
Prabeen04 <prabeen.strange@gmail.com>
2018-06-26 22:18:57 +0530

    DndItem component created

---
+++ src/container/DND/dndItems.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class DndItem extends Component {
+  render() {
+    return (
+      <div>
+        <h2>DndItem Component</h2>
+      </div>
+    )
+  }
+}
+export default DndItem
