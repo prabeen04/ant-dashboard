ac5fbd6ada3bdff984264ae1931520d8b075af9b
Prabeen04 <prabeen.strange@gmail.com>
2018-06-26 22:18:11 +0530

    DndTarget component created

---
+++ src/container/DND/dndTarget.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class DndTarget extends Component {
+  render() {
+    return (
+      <div>
+        <h2>DndTarget Component</h2>
+      </div>
+    )
+  }
+}
+export default DndTarget
