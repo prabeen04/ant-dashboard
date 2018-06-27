a9625775294c628a0b89f4a169c3ed2321b3cf60
Prabeen04 <prabeen.strange@gmail.com>
2018-06-27 19:44:39 +0530

    state and data added

--- src/container/DND/DND.js
+++ src/container/DND/DND.js
@@ -7,11 +7,25 @@ import './dnd.css';
 
 
 class DND extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      items: [
+        { id: 1, value: 'manutd' },
+        { id: 2, value: 'chelsea' },
+        { id: 3, value: 'mancity' },
+        { id: 4, value: 'arsenel' },
+        { id: 5, value: 'liverpool' },
+        { id: 6, value: 'tottenham' }
+      ]
+    }
+  }
   render() {
     return (
       <div className="dnd-wrapper">
-        <DndItems/>
-        <DndTarget/>
+        <DndItems />
+        <DndTarget />
       </div>
     )
   }
