58bfb12ed776ab836835e9625b19f95b0f5898d2
Prabeen04 <prabeen.strange@gmail.com>
2018-06-28 19:51:54 +0530

    itemSource values added

--- src/container/DND/item.js
+++ src/container/DND/item.js
@@ -4,14 +4,44 @@ const Types = {
     ITEM: 'item'
 }
 const itemSource = {
-    beginDrag(props) {
-        // const item = { id: props.id };
-        return props.item;
-    },
-    endDrag(props) {
-        /* code here */
-        return
-    }
+    canDrag(props) {
+        // You can disallow drag based on props
+        return props.isReady;
+      },
+    
+      isDragging(props, monitor) {
+        // If your component gets unmounted while dragged
+        // (like a card in Kanban board dragged between lists)
+        // you can implement something like this to keep its
+        // appearance dragged:
+        return monitor.getItem().id === props.id;
+      },
+    
+      beginDrag(props, monitor, component) {
+        // Return the data describing the dragged item
+        const item = { id: props.id };
+        return item;
+      },
+    
+      endDrag(props, monitor, component) {
+        if (!monitor.didDrop()) {
+          // You can check whether the drop was successful
+          // or if the drag ended but nobody handled the drop
+          return;
+        }
+    
+        // When dropped on a compatible target, do something.
+        // Read the original dragged item from getItem():
+        const item = monitor.getItem();
+    
+        // You may also read the drop result from the drop target
+        // that handled the drop, if it returned an object from
+        // its drop() method.
+        const dropResult = monitor.getDropResult();
+    
+        // This is a good place to call some Flux action
+        CardActions.moveCardToList(item.id, dropResult.listId);
+      }
 }
 function collect(connect, monitor) {
     return {
