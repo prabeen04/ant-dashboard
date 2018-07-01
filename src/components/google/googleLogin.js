2462aa88788558f86d21ace3b529dad9a0af0358
Prabeen04 <prabeen.strange@gmail.com>
2018-07-01 12:57:02 +0530

    isLoggedIn state and stuff

--- src/components/google/googleLogin.js
+++ src/components/google/googleLogin.js
@@ -4,22 +4,35 @@ import { GoogleLogin, GoogleLogout } from 'react-google-login';
 class LoginWithGoogle extends Component {
     constructor(props) {
         super(props)
-        
+        this.state = {
+            isLoggedIn: false
+        }
         this.responseGoogle = this.responseGoogle.bind(this);
     }
-    responseGoogle = (res) => console.log(res)
+    responseGoogle = (res) =>{ 
+        console.log(res)
+        if(res.accessToken){
+            this.setState({
+                isLoggedIn: true
+            })
+        }
+    }
     render() {
         return (
             <div>
-               {!isLoggedIn && <GoogleLogin
+               {!this.state.isLoggedIn && <GoogleLogin
                     clientId="553440503285-rgciog1jtcen74j5hq1ojul6o4b93erh.apps.googleusercontent.com"
                     buttonText=" Login with Google "
                     onSuccess={this.responseGoogle}
                     onFailure={this.responseGoogle}
                 />}
-              {isLoggedIn &&  <GoogleLogout
+              {this.state.isLoggedIn &&  <GoogleLogout
                     buttonText="Logout"
-                    onLogoutSuccess={(res)=>console.log(res))}
+                    onLogoutSuccess={(res)=>{
+                        this.setState({
+                            isLoggedIn: false
+                        })
+                    }}
                 >
                 </GoogleLogout>}
             </div>
