83361be5eae72f3fc6e735166867eb2b024bfa38
Prabeen04 <prabeen.strange@gmail.com>
2018-07-01 12:20:31 +0530

    GoogleLogin component added

--- src/container/dashboard/dashboard.js
+++ src/container/dashboard/dashboard.js
@@ -28,6 +28,12 @@ class Dashboard extends Component {
       <div className='flex-container'>
         <JumpStartBox/>
       </div>
+      <GoogleLogin
+        clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={this.responseGoogle}
+        onFailure={this.responseGoogle}
+      />
       {/* <GooglePlace/> */}
        <Card loading={this.state.isLoading} style={{height: '400px'}}>
        <div className="flex-container" style={{ justifyContent: 'space-evenly'}}>
