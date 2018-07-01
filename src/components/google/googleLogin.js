687786c0f939e322773b7b83f97d65c07bc6c6c1
Prabeen04 <prabeen.strange@gmail.com>
2018-07-01 12:51:23 +0530

    GoogleLogut added

--- src/components/google/googleLogin.js
+++ src/components/google/googleLogin.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react'
-import { GoogleLogin } from 'react-google-login';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
 
 class LoginWithGoogle extends Component {
     constructor(props) {
         super(props)
+        
         this.responseGoogle = this.responseGoogle.bind(this);
     }
     responseGoogle = (res) => console.log(res)
     render() {
         return (
             <div>
-                <GoogleLogin
+               {!isLoggedIn && <GoogleLogin
                     clientId="553440503285-rgciog1jtcen74j5hq1ojul6o4b93erh.apps.googleusercontent.com"
                     buttonText=" Login with Google "
                     onSuccess={this.responseGoogle}
                     onFailure={this.responseGoogle}
-                />
+                />}
+              {isLoggedIn &&  <GoogleLogout
+                    buttonText="Logout"
+                    onLogoutSuccess={(res)=>console.log(res))}
+                >
+                </GoogleLogout>}
             </div>
         )
     }
