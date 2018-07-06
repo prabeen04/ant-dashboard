30b0d5dee287c5c87608e8034f8ce45a289904f4
Prabeen04 <prabeen.strange@gmail.com>
2018-07-06 23:31:17 +0530

    default style and transition style removed

--- src/components/animation/transition.js
+++ src/components/animation/transition.js
@@ -3,16 +3,6 @@ import Transition from 'react-transition-group/Transition';
 
 const duration = 300;
 
-const defaultStyle = {
-  transition: `opacity ${duration}ms ease-in-out`,
-  opacity: 0,
-}
-
-const transitionStyles = {
-  entering: { opacity: 0 },
-  entered:  { opacity: 1 },
-};
-
 class TransitionComponent extends Component{
     constructor(props){
         super(props)
