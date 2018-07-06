c79fee8fa9dbbfe3fccbe65548e90256e02d8c5f
Prabeen04 <prabeen.strange@gmail.com>
2018-07-06 21:32:39 +0530

    transition component created

---
+++ src/components/animation/transition.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import Transition from 'react-transition-group/Transition';
+
+const duration = 300;
+
+const defaultStyle = {
+  transition: `opacity ${duration}ms ease-in-out`,
+  opacity: 0,
+}
+
+const transitionStyles = {
+  entering: { opacity: 0 },
+  entered:  { opacity: 1 },
+};
+
+const Fade = ({ in: inProp }) => (
+  <Transition in={inProp} timeout={duration}>
+    {(state) => (
+      <div style={{
+        ...defaultStyle,
+        ...transitionStyles[state]
+      }}>
+        I'm a fade Transition!
+      </div>
+    )}
+  </Transition>
+);
