5a18097178380fb5165eab875591b70d3a67cf18
Prabeen04 <prabeen.strange@gmail.com>
2018-07-06 20:24:48 +0530

    transition group imported and

--- src/container/dashboard/dashboard.js
+++ src/container/dashboard/dashboard.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react'
 import { Card, Spin, Icon } from 'antd';
+import Transition from 'react-transition-group/Transition'
 import JumpStartBox from './jumpStart';
 import GooglePlace from '../../components/google/googlePlace';
 import LoginWithGoogle from '../../components/google/googleLogin';
 import FunnelChart from '../../components/charts/funnelChart';
+
+const duration = 300;
+
+const defaultStyle = {
+  transition: `opacity ${duration}ms ease-in-out`,
+  opacity: 0,
+}
+
+const transitionStyles = {
+  entering: { opacity: 0 },
+  entered:  { opacity: 1 },
+};
 
 class Dashboard extends Component {
   constructor(props){
