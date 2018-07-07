ebd1b64125ae9443300807f0dfb0e967e156aba6
Prabeen04 <prabeen.strange@gmail.com>
2018-07-07 20:28:02 +0530

    LineChart component created

---
+++ src/container/charts/lineChart.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class LineChart extends Component {
+  render() {
+    return (
+      <div>
+        <h1>LineChart Component</h1>
+      </div>
+    )
+  }
+}
+
+export default LineChart;
