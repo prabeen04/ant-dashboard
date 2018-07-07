2e59c2c7ae613a322c735a25b0f515fe4753f029
Prabeen04 <prabeen.strange@gmail.com>
2018-07-07 20:35:42 +0530

    new data added

--- src/components/charts/funnelChart.js
+++ src/components/charts/funnelChart.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react'
 import D3Funnel from 'd3-funnel';
-
+const data = [
+    { name: 'Page A', uv: 1000, pv: 2400, amt: 2400, uvError: [75, 20] },
+    { name: 'Page B', uv: 300, pv: 4567, amt: 2400, uvError: [90, 40] },
+    { name: 'Page C', uv: 280, pv: 1398, amt: 2400, uvError: 40 },
+    { name: 'Page D', uv: 200, pv: 9800, amt: 2400, uvError: 20 },
+    { name: 'Page E', uv: 278, pv: null, amt: 2400, uvError: 28 },
+    { name: 'Page F', uv: 189, pv: 4800, amt: 2400, uvError: [90, 20] },
+    { name: 'Page G', uv: 189, pv: 4800, amt: 2400, uvError: [28, 40] },
+    { name: 'Page H', uv: 189, pv: 4800, amt: 2400, uvError: 28 },
+    { name: 'Page I', uv: 189, pv: 4800, amt: 2400, uvError: 28 },
+    { name: 'Page J', uv: 189, pv: 4800, amt: 2400, uvError: [15, 60] },
+  ];
+  
 class FunnelChart extends Component {
     constructor(props) {
         super(props)
         this.myRef = React.createRef();
-        this.state = {
-            data: [
-                { label: 'Inquiries', value: 5000 },
-                { label: 'Applicants', value: 2500 },
-                { label: 'Admits', value: 500 },
-                { label: 'Deposits', value: 200 },
-            ]
-        }
+        this.state = {data}
     }
     componentDidMount(){
         const options = {
