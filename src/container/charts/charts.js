db35bc3112f129b66fe99cb08f63d74e9aaa91c7
Prabeen04 <prabeen.strange@gmail.com>
2018-07-07 18:43:35 +0530

    charts container created

---
+++ src/container/charts/charts.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Charts extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Charts Component</h1>
+      </div>
+    )
+  }
+}
+export default Charts;
