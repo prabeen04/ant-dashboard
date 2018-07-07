86ea225fe36e41b00f3db80310483e0fbbeb0c44
Prabeen04 <prabeen.strange@gmail.com>
2018-07-07 20:38:58 +0530

    data added in linechart

--- src/container/charts/lineChart.js
+++ src/container/charts/lineChart.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import { LineChart, Line } from 'recharts';
+const data = [
+    { name: 'Page A', uv: 1000, pv: 2400, amt: 2400, uvError: [75, 20] },
+    { name: 'Page B', uv: 300, pv: 4567, amt: 2400, uvError: [90, 40] },
+    { name: 'Page C', uv: 280, pv: 1398, amt: 2400, uvError: 40 },
+    { name: 'Page D', uv: 200, pv: 9800, amt: 2400, uvError: 20 },
+    { name: 'Page E', uv: 278, pv: null, amt: 2400, uvError: 28 },
+    { name: 'Page F', uv: 189, pv: 4800, amt: 2400, uvError: [90, 20] },
+    { name: 'Page G', uv: 189, pv: 4800, amt: 2400, uvError: [28, 40] },
+    { name: 'Page H', uv: 189, pv: 4800, amt: 2400, uvError: 28 },
+    { name: 'Page I', uv: 189, pv: 4800, amt: 2400, uvError: 28 },
+    { name: 'Page J', uv: 189, pv: 4800, amt: 2400, uvError: [15, 60] },
+];
 
 class LineChart1 extends Component {
     render() {
         return (
             <div>
-                <h1>LineChart Component</h1>
-                <LineChart width={400} height={400} data={[
-                { label: 'Inquiries', value: 5000 },
-                { label: 'Applicants', value: 2500 },
-                { label: 'Admits', value: 500 },
-                { label: 'Deposits', value: 200 },
-            ]}>
+                <LineChart width={400} height={400} data={data}>
                     <Line type="monotone" dataKey="uv" stroke="#8884d8" />
                 </LineChart>
             </div>
