fa095698ecbf4c402a19bc930b7bd80ecc85af28
Prabeen04 <prabeen.strange@gmail.com>
2018-07-07 11:56:01 +0530

    Input commom form component created

---
+++ src/components/common/forms/Input.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Input extends Component {
+  render() {
+    return (
+      <div>
+        <input type="text" placeholder="my Placeholder"/>
+      </div>
+    )
+  }
+}
