bff869cdc29807941aed8ca10fcfb9f603ad5f27
Prabeen04 <prabeen.strange@gmail.com>
2018-07-08 08:44:46 +0530

    legend and label added

--- src/container/charts/pieChart.js
+++ src/container/charts/pieChart.js
@@ -25,6 +25,8 @@ class PieChart1 extends Component {
         <PieChart width={730} height={250}>
           <Pie data={data01} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={50} fill="#8884d8" />
           <Pie data={data02} dataKey="value" nameKey="name" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#82ca9d" label />
+          <Tooltip/>
+          <Legend/>
         </PieChart>
       </div>
     )
