e3b86780eaadcad27db2a45d8bd2f3918f0b4b7e
Prabeen04 <prabeen.strange@gmail.com>
2018-07-08 22:31:24 +0530

    area chart data and imports added

--- src/container/charts/areaChart.js
+++ src/container/charts/areaChart.js
@@ -1,5 +1,17 @@
 import React, { Component } from 'react'
-
+import { AreaChart, Area, XAxis, YAxis, Tooltip, CartesianGrid, Brush, Legend,
+    ReferenceArea, ReferenceLine, ReferenceDot, ResponsiveContainer,
+    LabelList, Label } from 'recharts';
+  
+  const data = [
+    { name: 'Page A', uv: 4000, pv: 2400, amt: 2400, time: 1 },
+    { name: 'Page B', uv: 3000, pv: 1398, amt: 2210, time: 3 },
+    { name: 'Page C', uv: 2000, pv: 9800, amt: 2290, time: 9 },
+    { name: 'Page D', uv: 2780, pv: 3908, amt: 2000, time: 10 },
+    { name: 'Page E', uv: 2500, pv: 4800, amt: 2181, time: 12 },
+    { name: 'Page F', uv: 1220, pv: 3800, amt: 2500, time: 16 },
+    { name: 'Page G', uv: 2300, pv: 4300, amt: 2100, time: 18 },
+  ];
 class AreaChart1 extends Component {
   render() {
     return (
