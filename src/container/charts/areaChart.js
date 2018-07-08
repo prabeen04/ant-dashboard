be51a1f850787ca0a5342f50d63d9c3dd241baeb
Prabeen04 <prabeen.strange@gmail.com>
2018-07-08 23:01:48 +0530

    small stuff

--- src/container/charts/areaChart.js
+++ src/container/charts/areaChart.js
@@ -22,7 +22,7 @@ class AreaChart1 extends Component {
                     margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
                     <defs>
                         <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-                            <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                            <stop offset="5%" stopColor="tomato" stopOpacity={0.8} />
                             <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
                         </linearGradient>
                         <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
