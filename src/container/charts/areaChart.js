2542736d8d2e4652e753e2898312fe450dacddd1
Prabeen04 <prabeen.strange@gmail.com>
2018-07-08 22:32:33 +0530

    area chart component added

--- src/container/charts/areaChart.js
+++ src/container/charts/areaChart.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
-import { AreaChart, Area, XAxis, YAxis, Tooltip, CartesianGrid, Brush, Legend,
+import {
+    AreaChart, Area, XAxis, YAxis, Tooltip, CartesianGrid, Brush, Legend,
     ReferenceArea, ReferenceLine, ReferenceDot, ResponsiveContainer,
-    LabelList, Label } from 'recharts';
-  
-  const data = [
+    LabelList, Label
+} from 'recharts';
+
+const data = [
     { name: 'Page A', uv: 4000, pv: 2400, amt: 2400, time: 1 },
     { name: 'Page B', uv: 3000, pv: 1398, amt: 2210, time: 3 },
     { name: 'Page C', uv: 2000, pv: 9800, amt: 2290, time: 9 },
@@ -11,15 +13,24 @@ import { AreaChart, Area, XAxis, YAxis, Tooltip, CartesianGrid, Brush, Legend,
     { name: 'Page E', uv: 2500, pv: 4800, amt: 2181, time: 12 },
     { name: 'Page F', uv: 1220, pv: 3800, amt: 2500, time: 16 },
     { name: 'Page G', uv: 2300, pv: 4300, amt: 2100, time: 18 },
-  ];
+];
 class AreaChart1 extends Component {
-  render() {
-    return (
-      <div>
-        <h3>AreaChart Component</h3>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <AreaChart width={730} height={250} data={data}
+                    margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <Tooltip />
+                    <Area type="monotone" dataKey="uv" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+                    <Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+                </AreaChart>
+            </div>
+        )
+    }
 }
 
 export default AreaChart1;
