af3961f0d97c18d112917d70b601a9c527fc748e
Prabeen04 <prabeen.strange@gmail.com>
2018-07-08 08:41:12 +0530

    piechart component added

--- src/container/charts/pieChart.js
+++ src/container/charts/pieChart.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react'
-import { PieChart, Pie, Legend, Cell, Tooltip, ResponsiveContainer, Sector,  Label, LabelList } from 'recharts';
+import { PieChart, Pie, Legend, Cell, Tooltip, ResponsiveContainer, Sector, Label, LabelList } from 'recharts';
 const data = [
-    { name: 'Page A', uv: 1000, pv: 2400, amt: 2400, uvError: [75, 20] },
-    { name: 'Page B', uv: 300, pv: 4567, amt: 2400, uvError: [90, 40] },
-    { name: 'Page C', uv: 280, pv: 1398, amt: 2400, uvError: 40 },
-    { name: 'Page D', uv: 200, pv: 9800, amt: 2400, uvError: 20 },
-    { name: 'Page E', uv: 278, pv: null, amt: 2400, uvError: 28 },
-    { name: 'Page F', uv: 189, pv: 4800, amt: 2400, uvError: [90, 20] },
-    { name: 'Page G', uv: 189, pv: 4800, amt: 2400, uvError: [28, 40] },
-    { name: 'Page H', uv: 189, pv: 4800, amt: 2400, uvError: 28 },
-    { name: 'Page I', uv: 189, pv: 4800, amt: 2400, uvError: 28 },
-    { name: 'Page J', uv: 189, pv: 4800, amt: 2400, uvError: [15, 60] },
+  { name: 'Page A', uv: 1000, pv: 2400, amt: 2400, uvError: [75, 20] },
+  { name: 'Page B', uv: 300, pv: 4567, amt: 2400, uvError: [90, 40] },
+  { name: 'Page C', uv: 280, pv: 1398, amt: 2400, uvError: 40 },
+  { name: 'Page D', uv: 200, pv: 9800, amt: 2400, uvError: 20 },
+  { name: 'Page E', uv: 278, pv: null, amt: 2400, uvError: 28 },
+  { name: 'Page F', uv: 189, pv: 4800, amt: 2400, uvError: [90, 20] },
+  { name: 'Page G', uv: 189, pv: 4800, amt: 2400, uvError: [28, 40] },
+  { name: 'Page H', uv: 189, pv: 4800, amt: 2400, uvError: 28 },
+  { name: 'Page I', uv: 189, pv: 4800, amt: 2400, uvError: 28 },
+  { name: 'Page J', uv: 189, pv: 4800, amt: 2400, uvError: [15, 60] },
 ];
 
 class PieChart1 extends Component {
@@ -18,6 +18,10 @@ class PieChart1 extends Component {
     return (
       <div>
         <h2>PieChart1 Component</h2>
+        <PieChart width={730} height={250}>
+          <Pie data={data01} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={50} fill="#8884d8" />
+          <Pie data={data02} dataKey="value" nameKey="name" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#82ca9d" label />
+        </PieChart>
       </div>
     )
   }
