3972d291304d31bedc36d38e99f7d54361ea6401
Prabeen04 <prabeen.strange@gmail.com>
2018-07-08 22:28:15 +0530

    area chart component craeted

---
+++ src/container/charts/areaChart.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class AreaChart1 extends Component {
+  render() {
+    return (
+      <div>
+        <h3>AreaChart Component</h3>
+      </div>
+    )
+  }
+}
+
+export default AreaChart1;
