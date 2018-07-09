46c94c38693d045b877c0a289206a21bc9bf1496
Prabeen04 <prabeen.strange@gmail.com>
2018-07-09 20:42:49 +0530

    dashboard Header component created

---
+++ src/container/dashboard/dashboardHeader.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class DashboardHeader extends Component {
+  render() {
+    return (
+      <div>
+        <h1>DashboardHeader Component</h1>
+      </div>
+    )
+  }
+}
+
+export default DashboardHeader;
