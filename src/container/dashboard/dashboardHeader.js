7cbb42485d698c498cd77165278088ef3e38267e
Prabeen04 <prabeen.strange@gmail.com>
2018-07-09 20:48:41 +0530

    button group added

--- src/container/dashboard/dashboardHeader.js
+++ src/container/dashboard/dashboardHeader.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
+import { Button, Icon } from 'antd';
 import './dashboard.css';
+const ButtonGroup = Button.Group;
 
 class DashboardHeader extends Component {
-  render() {
-    return (
-      <div className="dashboard-header">
-        <h1>DashboardHeader Component</h1>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className="dashboard-header">
+                <ButtonGroup>
+                    <Button>L</Button>
+                    <Button>M</Button>
+                    <Button>R</Button>
+                </ButtonGroup>
+            </div>
+        )
+    }
 }
 
 export default DashboardHeader;
