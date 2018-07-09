f09dd2334b3e4fa44dc816d708987c418fec22d7
Prabeen04 <prabeen.strange@gmail.com>
2018-07-09 16:12:53 +0530

    Mychart component created

---
+++ src/container/charts/myChart.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class MyChart extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Mychart component created</h1>
+      </div>
+    )
+  }
+}
+
+export default MyChart;
