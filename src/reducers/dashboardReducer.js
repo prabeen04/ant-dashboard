d2c74d242bd494b2538a31544974ed1b33828b6e
Prabeen04 <prabeen.strange@gmail.com>
2018-07-10 20:29:36 +0530

    dateRangelist initital state added in dashboard reducer

--- src/reducers/dashboardReducer.js
+++ src/reducers/dashboardReducer.js
@@ -1,49 +1,60 @@
 import { GET_JUMPSTART_LIST, SET_JUMPSTART_LIST } from '../types/dashboardActionTypes';
-const initialState = {jumpStartList: [
-    {
-        visible: true,
-        id: 1,
-        title: 'Title 1',
-        value: 100
-    },
-    {
-        visible: true,
-        id: 2,
-        title: 'Title 2',
-        value: 4500
-    },
-    {
-        visible: true,
-        id: 3,
-        title: 'Title 3',
-        value: 90
-    },
-    {
-        visible: true,
-        id: 4,
-        title: 'Title 4',
-        value: 45
-    },
-    {
-        visible: true,
-        id: 5,
-        title: ' Title 5',
-        value: 1100
-    },
-    {
-        visible: true,
-        id: 6,
-        title: ' Title 6',
-        value: 7700
-    }
-]}
+const initialState = {
+    dateRangeList: [
+        { id: 1,value: '1W'},
+        { id: 1,value: '4W'},
+        { id: 1,value: '1Y'},
+        { id: 1,value: 'MTD'},
+        { id: 1,value: 'QTD'},
+        { id: 1,value: 'YTD'},
+        { id: 1,value: 'ALL'},
+    ]
+    jumpStartList: [
+        {
+            visible: true,
+            id: 1,
+            title: 'Title 1',
+            value: 100
+        },
+        {
+            visible: true,
+            id: 2,
+            title: 'Title 2',
+            value: 4500
+        },
+        {
+            visible: true,
+            id: 3,
+            title: 'Title 3',
+            value: 90
+        },
+        {
+            visible: true,
+            id: 4,
+            title: 'Title 4',
+            value: 45
+        },
+        {
+            visible: true,
+            id: 5,
+            title: ' Title 5',
+            value: 1100
+        },
+        {
+            visible: true,
+            id: 6,
+            title: ' Title 6',
+            value: 7700
+        }
+    ]
+}
 export const dashboardReducer = (state = initialState, action) => {
     switch (action.type) {
         case GET_JUMPSTART_LIST:
-        return state;
+            return state;
 
         case SET_JUMPSTART_LIST:
-            return {...state, jumpStartList: [...state.jumpStartList.map(data => data.id === action.payload.id ? action.payload : data)]};
+            return { ...state, jumpStartList: [...state.jumpStartList.map(data => data.id === action.payload.id ? action.payload : data)] };
 
         default:
             return state;
