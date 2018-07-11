4423771f60f253536ea330c3f9a2af0485c1a96f
Prabeen04 <prabeen.strange@gmail.com>
2018-07-11 21:21:35 +0530

    ant modal added

--- src/container/dashboard/jumpStartModal.js
+++ src/container/dashboard/jumpStartModal.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
+import { Modal, Button } from 'antd';
 
 class JumpStartModal extends Component {
   render() {
     return (
       <div>
-        <h1>JumpStartModal Component</h1>
+        <Modal
+          title="Basic Modal"
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+        >
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </Modal>
       </div>
     )
   }
