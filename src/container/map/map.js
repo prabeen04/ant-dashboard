be4a9771de183ea57a411ea82cb75c6d64835978
Prabeen04 <prabeen.strange@gmail.com>
2018-07-12 20:16:59 +0530

    map component created

---
+++ src/container/map/map.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Map extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Map Component</h2>
+      </div>
+    )
+  }
+}
+
+export default Map;
