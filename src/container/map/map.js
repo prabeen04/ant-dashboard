88160c5a33bd9fdd3046d0b1d69d5851791c6f6b
Prabeen04 <prabeen.strange@gmail.com>
2018-07-13 20:20:20 +0530

    bug fixes

--- src/container/map/map.js
+++ src/container/map/map.js
@@ -23,7 +23,7 @@ class MyMap extends Component {
         this.state = {
             lat: 51.505,
             lng: -0.09,
-            zoom: 4,
+            zoom: 13,
         }
     }
     render() {
