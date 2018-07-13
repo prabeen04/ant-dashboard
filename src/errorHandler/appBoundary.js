a881001bc4104737d756c311961aafcdd9c2527f
Prabeen04 <prabeen.strange@gmail.com>
2018-07-13 09:36:13 +0530

    componentDidCatch method added

--- src/errorHandler/appBoundary.js
+++ src/errorHandler/appBoundary.js
@@ -5,7 +5,12 @@ class AppBoundary extends Component {
         super(props);
         this.state = { hasError: false };
     }
-
+    componentDidCatch(error, info) {
+        // Display fallback UI
+        this.setState({ hasError: true });
+        // You can also log the error to an error reporting service
+        logErrorToMyService(error, info);
+    }
     render() {
         if (this.state.hasError) {
             // You can render any custom fallback UI
