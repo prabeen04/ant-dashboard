d53dfe62ecab45cb6ce7a461a1eaaf2aadd24943
Prabeen04 <prabeen.strange@gmail.com>
2018-07-13 09:34:08 +0530

    app boundary error boundary file created

---
+++ src/errorHandler/appBoundary.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class AppBoundary extends Component {
+  render() {
+    return (
+      <div>
+        <h3>AppBoundary Error Container</h3>
+      </div>
+    )
+  }
+}
+
+export default AppBoundary;
