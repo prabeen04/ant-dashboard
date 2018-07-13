341c2a55fa755106f4b4fbec0313a7aaa97892f7
Prabeen04 <prabeen.strange@gmail.com>
2018-07-13 20:31:32 +0530

    navbar code cleaned

--- src/components/layouts/navbar.js
+++ src/components/layouts/navbar.js
@@ -47,20 +47,6 @@ class Navbar extends React.Component {
                     <NavMenu
                         collapsed={this.state.collapsed}
                         toggleCollapsed={this.toggle} />
-                    {/* <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-                        <Menu.Item key="1">
-                            <Icon type="user" />
-                            <span>nav 1</span>
-                        </Menu.Item>
-                        <Menu.Item key="2">
-                            <Icon type="video-camera" />
-                            <span>nav 2</span>
-                        </Menu.Item>
-                        <Menu.Item key="3">
-                            <Icon type="upload" />
-                            <span>nav 3</span>
-                        </Menu.Item>
-                    </Menu> */}
                 </Sider>
                 <Layout>
                     <Header className="flex-container" style={{ background: '#fff', padding: 0, height: 45 }}>
