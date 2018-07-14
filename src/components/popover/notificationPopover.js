a8c1dca99747e0a7601adcea2a252ad0e29734f4
Prabeen04 <prabeen.strange@gmail.com>
2018-07-14 23:31:03 +0530

    popover added to notification item

--- src/components/popover/notificationPopover.js
+++ src/components/popover/notificationPopover.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react'
-import { Badge, Icon, Tooltip } from 'antd';
+import { Badge, Icon, Tooltip, Popover } from 'antd';
 
 class NotificationPopover extends Component {
     render() {
         return (
             <Tooltip title="Notifications">
-                <Badge count={10} >
-                    <Icon type="bell" />
-                </Badge>
-            </Tooltip>
-        )
-    }
-}
+                <Popover
+                    content={
+                        <div>
+                            <SettingTab hide={this.hide} />
+                        </div>
 
+                    }
+                    // title={<a onClick={this.hide}>Close</a>}
+                    trigger="click"
+                    placement="bottomRight"
+                    visible={true}
+                    // onVisibleChange={}
+                >
+                    <Badge count={10} >
+                        <Icon type="bell" />
+                    </Badge>
+                    </Popover>
+            </Tooltip>
+                )
+            }
+        }
+        
 export default NotificationPopover;
