7401da412d8e75bdaab8b8fc1533117fe4b00ec8
Prabeen04 <prabeen.strange@gmail.com>
2018-07-14 23:14:37 +0530

    notification popover component created

---
+++ src/components/popover/notificationPopover.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class NotificationPopover extends Component {
+  render() {
+    return (
+      <div>
+        <h2>NotificationPopover Component</h2>
+      </div>
+    )
+  }
+}
+
+export default NotificationPopover;
