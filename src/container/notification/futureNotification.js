a812263a5b826f0fd94deab192debe070742bfae
Prabeen04 <prabeen.strange@gmail.com>
2018-07-15 17:06:43 +0530

    future notification component created

---
+++ src/container/notification/futureNotification.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class FutureNotification extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default FutureNotification;
