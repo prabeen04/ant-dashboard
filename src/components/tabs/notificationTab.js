ebdb182b92454e9b210fc1337d34a6dfe06e6f58
Prabeen04 <prabeen.strange@gmail.com>
2018-07-15 17:10:20 +0530

    notifications added to componnets

--- src/components/tabs/notificationTab.js
+++ src/components/tabs/notificationTab.js
@@ -3,6 +3,7 @@ import { Tabs, Icon, Button, List, Checkbox } from 'antd';
 import PastNotification from '../../container/notification/pastNotification';
 import PresentNotification from '../../container/notification/presentNotification';
 import FutureNotification from '../../container/notification/futureNotification';
+
 const TabPane = Tabs.TabPane;
 
 class NotificationTab extends Component {
@@ -10,14 +11,14 @@ class NotificationTab extends Component {
         return (
             <div>
                 <Tabs defaultActiveKey="2" style={{ width: 400 }}>
-                    <TabPane tab={<span style={{color: 'skyblue'}}><Icon type="team" />Yesterday</span>} key="1">
-                        <h3>Yesterday Notificationd</h3>
+                    <TabPane tab={<span style={{ color: 'skyblue' }}><Icon type="team" />Yesterday</span>} key="1">
+                        <PastNotification />
                     </TabPane>
-                    <TabPane tab={<span style={{color: 'lightgreen'}}><Icon type="trophy" />Today</span>} key="2">
-                        <h3>Today Notificationd</h3>
+                    <TabPane tab={<span style={{ color: 'lightgreen' }}><Icon type="trophy" />Today</span>} key="2">
+                        <PresentNotification />
                     </TabPane>
-                    <TabPane tab={<span style={{color: 'tomato'}}><Icon type="line-chart" />Tommorow</span>} key="3">
-                        <h3>Tommorow Notificationd</h3>
+                    <TabPane tab={<span style={{ color: 'tomato' }}><Icon type="line-chart" />Tommorow</span>} key="3">
+                        <FutureNotification />
                     </TabPane>
                 </Tabs>
             </div>
