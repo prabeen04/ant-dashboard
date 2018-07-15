1a7412873ff632e7802b34110a1fe090304481ee
Prabeen04 <prabeen.strange@gmail.com>
2018-07-15 17:04:17 +0530

    past notification component created

---
+++ src/container/notification/pastNotification.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class PastNotification extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default PastNotification;
