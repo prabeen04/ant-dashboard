a51ddb9e98927eb331a12ae3ed8eefd17db9cc34
Prabeen04 <prabeen.strange@gmail.com>
2018-07-15 17:06:02 +0530

    present notification component created

---
+++ src/container/notification/presentNotification.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class PresentNotification extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default PresentNotification;
