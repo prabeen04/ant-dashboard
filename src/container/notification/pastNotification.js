98bf9a4b966f837bc1d938528e2ff5b8883eac31
Prabeen04 <prabeen.strange@gmail.com>
2018-07-15 17:13:22 +0530

    List added

--- src/container/notification/pastNotification.js
+++ src/container/notification/pastNotification.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { List, Button } from 'antd'; 
 
 class PastNotification extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <List
+                    size="small"
+                    bordered
+                    header={<div><h4> Team</h4></div>}
+                    footer={<div style={{ textAlign: 'right' }}> <Button
+                        type="primary"
+                        onClick={this.props.hide}>Close</Button></div>}
+                    dataSource={data}
+                    renderItem={item => (<List.Item>{item}</List.Item>)}
+                />
+            </div>
+        )
+    }
 }
 
 export default PastNotification;
