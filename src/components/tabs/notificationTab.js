19c47e6ad21f35220909f9267b6a02eb1d1958d3
Prabeen04 <prabeen.strange@gmail.com>
2018-07-15 16:38:23 +0530

    ant tabs added

--- src/components/tabs/notificationTab.js
+++ src/components/tabs/notificationTab.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react'
+import { Tabs, Icon, Button, List, Checkbox } from 'antd';
+const TabPane = Tabs.TabPane;
 
 class NotificationTab extends Component {
-  render() {
-    return (
-      <div>
-        <h2>NotificationTab Component</h2>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <Tabs defaultActiveKey="1" style={{ width: 400 }}>
+                    <TabPane tab={<span><Icon type="team" />Yesterday</span>} key="1">
+
+
+                    </TabPane>
+                    <TabPane tab={<span><Icon type="trophy" />Today</span>} key="2">
+
+                    </TabPane>
+                    <TabPane tab={<span><Icon type="line-chart" />Tommorow</span>} key="3">
+
+                    </TabPane>
+                </Tabs>
+            </div>
+        )
+    }
 }
 
 export default NotificationTab;
