25cbc20e6f6362427a8f2c90c655c46435700ce0
Prabeen04 <prabeen.strange@gmail.com>
2018-07-16 16:17:07 +0530

    notification initialState added

--- src/reducers/notificationReducer.js
+++ src/reducers/notificationReducer.js
@@ -1,6 +1,21 @@
 import { GET_PAST_NOTIFICATIONS, GET_PRESENT_NOTIFICATIONS, GET_FUTURE_NOTIFICATIONS } from '../types/notificationActionTypes'
-
-export const notificationReducer = (state = [], action) => {
+const initialState = {
+    pastNotifications: [
+        {
+            title: 'Ant Design Title 1',
+        },
+        {
+            title: 'Ant Design Title 2',
+        },
+        {
+            title: 'Ant Design Title 3',
+        },
+        {
+            title: 'Ant Design Title 4',
+        }
+    ],
+}
+export const notificationReducer = (state = initialState, action) => {
     switch (action.type) {
         case GET_PAST_NOTIFICATIONS:
             return state;
@@ -9,7 +24,7 @@ export const notificationReducer = (state = [], action) => {
         case GET_FUTURE_NOTIFICATIONS:
             return state;
         default:
-            return state;    
+            return state;
     }
 
     return state
