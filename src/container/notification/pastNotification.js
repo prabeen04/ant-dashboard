588dd800026acaf1f46475d895e35eeb40dbae95
Prabeen04 <prabeen.strange@gmail.com>
2018-07-16 15:49:11 +0530

    past notification data removed

--- src/container/notification/pastNotification.js
+++ src/container/notification/pastNotification.js
@@ -1,13 +1,6 @@
 import React, { Component } from 'react';
 import { List, Button, Checkbox } from 'antd'; 
-const data = [
-    <Checkbox>Manchester United</Checkbox>,
-    <Checkbox>Arsenal</Checkbox>,
-    <Checkbox>Chelsea</Checkbox>,
-    <Checkbox>Manchester City</Checkbox>,
-    <Checkbox>Liverpool</Checkbox>,
-    <Checkbox>Tottenham</Checkbox>
-];
+
 class PastNotification extends Component {
     render() {
         return (
