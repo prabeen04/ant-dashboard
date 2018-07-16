a83dd722a986117808a59c48ee1fa919cb43e509
Prabeen04 <prabeen.strange@gmail.com>
2018-07-16 15:50:01 +0530

    new list data added

--- src/container/notification/pastNotification.js
+++ src/container/notification/pastNotification.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
-import { List, Button, Checkbox } from 'antd'; 
-
+import { List, Button, Checkbox } from 'antd';
+const data = [
+    {
+        title: 'Ant Design Title 1',
+    },
+    {
+        title: 'Ant Design Title 2',
+    },
+    {
+        title: 'Ant Design Title 3',
+    },
+    {
+        title: 'Ant Design Title 4',
+    },
+];
 class PastNotification extends Component {
     render() {
         return (
