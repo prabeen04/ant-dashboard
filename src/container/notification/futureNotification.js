7b294adeab2ff6565e801eeb33bfaf3bbc5218a0
Prabeen04 <prabeen.strange@gmail.com>
2018-07-17 19:43:32 +0530

    redux connected to FutureNotification

--- src/container/notification/futureNotification.js
+++ src/container/notification/futureNotification.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { getFutureNotifications } from '../../actions/notificationActions';
 class FutureNotification extends Component {
   render() {
     return (
@@ -10,4 +12,15 @@ class FutureNotification extends Component {
   }
 }
 
-export default FutureNotification;
+const mapStateToProps = state => {
+  return{
+    isFetching: state.notificationReducer.isFetching,
+    futureNotifications: state.notificationReducer.futureNotifications,
+  }
+ }
+ const mapDispatchToProps = dispatch => {
+   return bindActionCreators({
+       getFutureNotifications
+   }, dispatch)
+ }
+ export default connect(mapStateToProps, mapDispatchToProps)(FutureNotification);
