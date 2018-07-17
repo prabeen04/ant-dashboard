7194daecafcd9f13f2b770170df60413c50322b9
Prabeen04 <prabeen.strange@gmail.com>
2018-07-17 19:45:49 +0530

    List rendered on future notification

--- src/container/notification/futureNotification.js
+++ src/container/notification/futureNotification.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { getFutureNotifications } from '../../actions/notificationActions';
+import { List, Avatar, Spin } from 'antd';
+
 class FutureNotification extends Component {
   componentDidMount() {
     this.props.getFutureNotifications();
@@ -9,7 +11,25 @@ class FutureNotification extends Component {
   render() {
     return (
       <div>
-
+        <List
+          dataSource={this.props.presentNotifications.results}
+          renderItem={item => (
+            <List.Item key={item.id}>
+              <List.Item.Meta
+                avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                title={<a href="https://ant.design">{item.name.last}</a>}
+                description={item.email}
+              />
+              <div>Content</div>
+            </List.Item>
+          )}
+        >
+          {this.props.isFetching && (
+            <div style={{ textAlign: 'center' }}>
+              <Spin />
+            </div>
+          )}
+        </List>
       </div>
     )
   }
