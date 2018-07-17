704a982f8c2cbd850ab0d08d53ed04e5f33a20d3
Prabeen04 <prabeen.strange@gmail.com>
2018-07-17 19:37:33 +0530

    getFutureNotification action created

--- src/actions/notificationActions.js
+++ src/actions/notificationActions.js
@@ -1,11 +1,14 @@
-import { GET_PRESENT_NOTIFICATIONS, GET_PRESENT_NOTIFICATIONS_SUCCESS, GET_PRESENT_NOTIFICATIONS_FAILURE } from '../types/notificationActionTypes';
+import {
+    GET_PRESENT_NOTIFICATIONS, GET_PRESENT_NOTIFICATIONS_SUCCESS, GET_PRESENT_NOTIFICATIONS_FAILURE,
+    GET_FAILURE_NOTIFICATIONS, GET_FAILURE_NOTIFICATIONS_SUCCESS, GET_FAILURE_NOTIFICATIONS_FAILURE
+} from '../types/notificationActionTypes';
 import axios from 'axios';
 
 const notificationURL = 'https://randomuser.me/api/?results=5&inc=name,gender,email,nat&noinfo';
 
 export const getPresentNotifications = () => dispatch => {
     console.log('inside notification action')
-     dispatch({
+    dispatch({
         type: GET_PRESENT_NOTIFICATIONS
     })
     return axios.get(`${notificationURL}`)
@@ -16,10 +19,30 @@ export const getPresentNotifications = () => dispatch => {
                 payload: res.data
             })
         })
-        .catch(err =>{ 
+        .catch(err => {
             console.log(err);
             dispatch({
                 type: GET_PRESENT_NOTIFICATIONS_FAILURE,
+                payload: err
+            })
+        })
+}
+export const getFutureNotifications = () => dispatch => {
+    dispatch({
+        type: GET_FUTURE_NOTIFICATIONS
+    })
+    return axios.get(`${notificationURL}`)
+        .then(res => {
+            console.log(res)
+            dispatch({
+                type: GET_FUTURE_NOTIFICATIONS_SUCCESS,
+                payload: res.data
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            dispatch({
+                type: GET_FUTURE_NOTIFICATIONS_FAILURE,
                 payload: err
             })
         })
