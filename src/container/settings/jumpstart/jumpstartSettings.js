fe24562173d4a4809b7b979a58fab86a08012af5
Prabeen04 <prabeen.strange@gmail.com>
2018-07-18 21:40:39 +0530

    jumpstartBox rendered on jumpstart settings

--- src/container/settings/jumpstart/jumpstartSettings.js
+++ src/container/settings/jumpstart/jumpstartSettings.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from "redux";
+import '../settings.css';
 
 class JumpstartSettings extends Component {
   render() {
+    const renderJumpstartBox = this.props.jumpStartList.map((box, index) => {
+      return <p key={box.id}>{box.value}</p>
+    })
     return (
       <div>
         <h1>JumpstartSettings Component</h1>
+        {renderJumpstartBox}
       </div>
     )
   }
