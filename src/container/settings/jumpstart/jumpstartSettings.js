0d1eddf939d163df8f2dbace966fd77c3d0e34e5
Prabeen04 <prabeen.strange@gmail.com>
2018-07-18 20:19:51 +0530

    jumpstartSetting component created

---
+++ src/container/settings/jumpstart/jumpstartSettings.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class JumpstartSettings extends Component {
+  render() {
+    return (
+      <div>
+        <h1>JumpstartSettings Component</h1>
+      </div>
+    )
+  }
+}
+
+export default JumpstartSettings;
