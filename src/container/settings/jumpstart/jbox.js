7fc9107ea49f697e4c7eae905961ec2a1918db2c
Prabeen04 <prabeen.strange@gmail.com>
2018-07-20 09:22:17 +0530

    dnd mmethods created

--- src/container/settings/jumpstart/jbox.js
+++ src/container/settings/jumpstart/jbox.js
@@ -18,6 +18,77 @@ function collect(connect, monitor) {
         isDragging: monitor.isDragging(),
     }
 }
+const cardSource = {
+    beginDrag(props) {
+        console.log(props.index)
+        return {
+            id: props.id,
+            index: props.index,
+        }
+    },
+}
+
+const cardTarget = {
+    hover(props, monitor, component) {
+        if (!component) {
+            return null
+        }
+        const dragIndex = monitor.getItem().index
+        const hoverIndex = props.index
+
+        // Don't replace items with themselves
+        if (dragIndex === hoverIndex) {
+            console.log('drag === hover')
+            return
+        }
+
+        // Determine rectangle on screen
+        const hoverBoundingRect = (findDOMNode(component)).getBoundingClientRect()
+
+        // Get vertical middle
+        const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2
+
+        // Determine mouse position
+        const clientOffset = monitor.getClientOffset()
+
+        // Get pixels to the top
+        const hoverClientY = (clientOffset).y - hoverBoundingRect.top
+
+        // Only perform the move when the mouse has crossed half of the items height
+        // When dragging downwards, only move when the cursor is below 50%
+        // When dragging upwards, only move when the cursor is above 50%
+        // Dragging downwards
+        if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+            return
+        }
+
+        // Dragging upwards
+        if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+            return
+        }
+
+        // Time to actually perform the action
+        props.moveItem(dragIndex, hoverIndex)
+
+        // Note: we're mutating the monitor item here!
+        // Generally it's better to avoid mutations,
+        // but it's good here for the sake of performance
+        // to avoid expensive index searches.
+        monitor.getItem().index = hoverIndex
+    },
+}
+function collect1(connect, monitor) {
+    return {
+        connectDropTarget: connect.dropTarget(),
+    }
+}
+function collect(connect, monitor) {
+    return {
+        connectDragSource: connect.dragSource(),
+        isDragging: monitor.isDragging(),
+    }
+}
+
 class Jbox extends Component {
     render() {
         const { connectDragSource, isDragging } = this.props;
