dac59d01680effe767dc2a6527b6a2567267d2ad
Prabeen04 <prabeen.strange@gmail.com>
2018-07-21 22:10:46 +0530

    ant Drawer added in Drawer component

--- src/container/post/editDrawer.js
+++ src/container/post/editDrawer.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react'
+import { Drawer, Button } from 'antd';
 
 class EditDrawer extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
     }
-  render() {
-    return (
-      <div>
-        <h3>EditDrawer Component</h3>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <Button type="primary" onClick={this.showDrawer}>Open</Button>
+                <Drawer
+                    title="Basic Drawer"
+                    placement="right"
+                    closable={false}
+                    onClose={this.onClose}
+                    visible={this.state.visible}
+                >
+                    <p>Some contents...</p>
+                    <p>Some contents...</p>
+                    <p>Some contents...</p>
+                </Drawer>
+            </div>
+        )
+    }
 }
 
 export default EditDrawer;
