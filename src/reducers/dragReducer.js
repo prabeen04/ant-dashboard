7869ecfe3cab9cfbd5586e3c35e7e617f67e1016
Prabeen04 <prabeen.strange@gmail.com>
2018-07-22 23:51:26 +0530

    GET_TEAMS case reducer stuff

--- src/reducers/dragReducer.js
+++ src/reducers/dragReducer.js
@@ -1,7 +1,14 @@
+import { GET_TEAMS, GET_TEAMS_SUCCESS, GET_TEAMS_FAILURE } from '../types/dragActionTypes';
 const initialState = {
 
 }
 
-export const dragReducer = (state=initialState, action) => {
+export const dragReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case GET_TEAMS:
+            return state
+        default:
+            return state;
+    }
     return state;
 }
