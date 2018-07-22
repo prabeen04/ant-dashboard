4af54eadb5c82a6f09709d3e2cc1d35696017b6d
Prabeen04 <prabeen.strange@gmail.com>
2018-07-22 23:20:06 +0530

    Drag component created

---
+++ src/container/drag/drag.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Drag extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Drag Component</h2>
+      </div>
+    )
+  }
+}
+
+export default Drag;
