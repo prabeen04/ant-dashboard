bea7559a18871618b01bf8b8407b96134b7eecc7
Prabeen04 <prabeen.strange@gmail.com>
2018-07-24 01:09:09 +0530

    ChampStage component created

---
+++ src/container/drag/champStage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ChampStage extends Component {
+  render() {
+    return (
+      <div>
+        <h3>ChampStage Component</h3>
+      </div>
+    )
+  }
+}
+
+export default ChampStage;
