856365111d72036a36e62493a63647b62a07a1a4
Prabeen04 <prabeen.strange@gmail.com>
2018-07-23 12:43:07 +0530

    initial state of team added

--- src/reducers/dragReducer.js
+++ src/reducers/dragReducer.js
@@ -1,6 +1,16 @@
 import { GET_TEAMS, GET_TEAMS_SUCCESS, GET_TEAMS_FAILURE } from '../types/dragActionTypes';
 const initialState = {
-
+    teams: [
+        { id: 1, Name: 'Manutd' },
+        { id: 2, Name: 'Chelsea' },
+        { id: 3, Name: 'Mancity' },
+        { id: 4, Name: 'Arsenal' },
+        { id: 5, Name: 'Liverpool' },
+        { id: 6, Name: 'Tottenham' },
+        { id: 7, Name: 'Everton' },
+        { id: 8, Name: 'Burnley' },
+        { id: 9, Name: 'West Ham' },
+      ]
 }
 
 export const dragReducer = (state = initialState, action) => {
