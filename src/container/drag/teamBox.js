a3f40f3a528dd97b476e439c3caddb07895a6d78
Prabeen04 <prabeen.strange@gmail.com>
2018-07-23 22:07:05 +0530

    DragSource implemented in TeamBOx

--- src/container/drag/teamBox.js
+++ src/container/drag/teamBox.js
@@ -1,8 +1,34 @@
 import React, { Component } from 'react'
+import { DragSource } from 'react-dnd'
+
+const itemSource = {
+    beginDrag(props, monitor, component) {
+        return props.team;
+    },
+
+    endDrag(props, monitor, component) {
+        if (!monitor.didDrop()) {
+            console.log('item didn\'t drop in target')
+            return;
+        }
+        const item = monitor.getItem();
+        const dropResult = monitor.getDropResult();
+        return props.handleDrag(props.item.id)
+    }
+}
+function collect(connect, monitor) {
+    return {
+        connectDragSource: connect.dragSource(),
+        connectDragPreview: connect.dragPreview(),
+        isDragging: monitor.isDragging()
+    }
+}
 
 class TeamBox extends Component {
   render() {
-    return (
+    const { isDragging, connectDragSource, item } = this.props;
+
+    return connectDragSource(
       <div className="team-box">
         <h3>{this.props.team.name}</h3>
       </div>
@@ -10,4 +36,4 @@ class TeamBox extends Component {
   }
 }
 
-export default TeamBox;
+export default DragSource('stage', itemSource, collect)(TeamBox);
