a3dc84e43be8dd412cb2c905062c5249e89e5c14
Prabeen04 <prabeen.strange@gmail.com>
2018-07-23 18:13:32 +0530

    _groupBy added to filter by stages

--- src/container/drag/drag.js
+++ src/container/drag/drag.js
@@ -4,13 +4,17 @@ import { bindActionCreators } from 'redux';
 import HTML5Backend from 'react-dnd-html5-backend';
 import { DragDropContext } from 'react-dnd';
 import TeamStage from './teamStage';
+import _ from 'lodash';
 import './drag.css';
 
 class Drag extends Component {
   render() {
+    const { teams } = this.props;
+    let teamGroup = _.groupBy(teams, 'stage');
+    console.log(teamGroup)
     return (
       <div className="drag-wrapper">
-        {
+        {/* {
           this.props.teams.map((team, index) => {
             if (team.stage == 'cl') {
               return <TeamStage key={index} team={team} backgroundColor={'red'} />
@@ -28,7 +32,7 @@ class Drag extends Component {
               return <TeamStage key={index} team={team} backgroundColor={'cyan'} />
             }
           })
-        }
+        } */}
       </div>
     )
   }
