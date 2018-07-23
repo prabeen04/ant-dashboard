8b1058e93ce9a565f520f5d4842602643e1c1661
Prabeen04 <prabeen.strange@gmail.com>
2018-07-23 21:20:59 +0530

    code cleaned

--- src/container/drag/drag.js
+++ src/container/drag/drag.js
@@ -4,51 +4,23 @@ import { bindActionCreators } from 'redux';
 import HTML5Backend from 'react-dnd-html5-backend';
 import { DragDropContext } from 'react-dnd';
 import TeamStage from './teamStage';
-import _ from 'lodash';
 import './drag.css';
 
 class Drag extends Component {
   render() {
-    const { teams } = this.props;
-    let teamGroup = _.groupBy(teams, 'stage');
-    // console.log(teamGroup)
-    const renderStage = Object.keys(teamGroup)
-    // console.log(renderStage)
     return (
       <div className="drag-wrapper">
-
         {
           this.props.stages.map((stage, index) => {
             return <TeamStage key={index} stage={stage} />
           })
         }
-
-        {/* {
-          this.props.teams.map((team, index) => {
-            if (team.stage == 'cl') {
-              return <TeamStage key={index} team={team} backgroundColor={'red'} />
-            }
-            if (team.stage == 'el') {
-              return <TeamStage key={index} team={team} backgroundColor={'green'} />
-            }
-            if (team.stage == 'th') {
-              return <TeamStage key={index} team={team} backgroundColor={'lightblue'} />
-            }
-            if (team.stage == 'bh') {
-              return <TeamStage key={index} team={team} backgroundColor={'orange'} />
-            }
-            if (team.stage == 'rel') {
-              return <TeamStage key={index} team={team} backgroundColor={'cyan'} />
-            }
-          })
-        } */}
       </div>
     )
   }
 }
 const mapStateToProps = state => {
   return {
-    teams: state.dragReducer.teams,
     stages: state.dragReducer.stages
   }
 }
--- src/container/drag/teamStage.js
+++ src/container/drag/teamStage.js
@@ -8,16 +8,10 @@ class TeamStage extends Component {
         super(props)
     }
     render() {
-        const backgroundColor = this.props.backgroundColor || '#fff';
         const renderTeams = this.props.teams
-            .filter((team) => {
-                return team.stage === this.props.stage
-            })
-            .map((team, index) => {
-                return (
-                    <TeamBox key={index} team={team}/>
-                )
-            })
+            .filter((team) => team.stage === this.props.stage)
+            .map((team, index) => <TeamBox key={index} team={team} />)
+
         return (
             <div className="team-stage">
                 {this.props.stage}
@@ -28,7 +22,6 @@ class TeamStage extends Component {
 }
 const mapStateToProps = state => {
     return {
-        stages: state.dragReducer.stages,
         teams: state.dragReducer.teams
     }
 }
