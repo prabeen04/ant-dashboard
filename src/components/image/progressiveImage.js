b01a2dc025535fc2a7537fdfebc828124cde2e44
Prabeen04 <prabeen.strange@gmail.com>
2018-07-24 22:18:18 +0530

    state and lifecycle method added

--- src/components/image/progressiveImage.js
+++ src/components/image/progressiveImage.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react'
 
 class ProgressiveImage extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+    state = {
+        currentImage: this.props.preview,
+        loading: true,
+    }
+    componentDidMount() {
+        this.fetchImage(this.props.image)
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.image !== this.props.image) {
+            this.setState({ currentImage: nextProps.preview, loading: true }, () => {
+                this.fetchImage(nextProps.image)
+            })
+        }
+    }
+
+    componentWillUnmount() {
+        if (this.loadingImage) {
+            this.loadingImage.onload = null
+        }
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
 }
 
 export default ProgressiveImage
