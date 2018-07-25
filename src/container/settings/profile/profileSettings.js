06bfb62ebbeeb116f0a80b48a2058515d5c82425
Prabeen04 <prabeen.strange@gmail.com>
2018-07-25 22:48:36 +0530

    Select prop stuff

--- src/container/settings/profile/profileSettings.js
+++ src/container/settings/profile/profileSettings.js
@@ -5,12 +5,15 @@ const options = [
     { value: 'chocolate', label: 'Chocolate' },
     { value: 'strawberry', label: 'Strawberry' },
     { value: 'vanilla', label: 'Vanilla' }
-  ]
+]
 class ProfileSettings extends Component {
     render() {
         return (
             <div>
                 <h3>ProfileSettings Component</h3>
+                <Select
+                    options={options}
+                    allowCreate={true} />
             </div>
         )
     }
