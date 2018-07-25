f97e0252b30f592262163065068dbf5170d9e12c
Prabeen04 <prabeen.strange@gmail.com>
2018-07-25 22:12:24 +0530

    ProfileSettings Component created

---
+++ src/container/settings/profile/profileSettings.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ProfileSettings extends Component {
+    render() {
+        return (
+            <div>
+                <h3>ProfileSettings Component</h3>
+            </div>
+        )
+    }
+}
+
+export default ProfileSettings;
