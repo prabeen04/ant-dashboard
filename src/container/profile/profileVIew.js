a490764e5f5e5aae4a06977009a5cce475e7fadc
Prabeen04 <prabeen.strange@gmail.com>
2018-07-26 23:33:29 +0530

    showDrawer method created

--- src/container/profile/profileVIew.js
+++ src/container/profile/profileVIew.js
@@ -10,37 +10,43 @@ class ProfileView extends Component {
   state = {
     isDrawerOpen: false
   }
-  render() {
-    return (
-      <div className="profile-view">
-        {this.props.profile.name &&
-          <div>
+  this.showDrawer = this.showDrawer.bind(this);
+showDrawer = () => {
+  this.setState({
+    isDrawerOpen: true,
+  });
+};
+render() {
+  return (
+    <div className="profile-view">
+      {this.props.profile.name &&
+        <div>
 
-            <div className="panel-header">
-              <h3>{this.props.profile.name}</h3>
-            </div>
-            <Card
-              style={{ width: '100%' }}
-              cover={<img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />}
-              actions={[
-                <Icon onClick={() => console.log('settings clicked')} type="setting" />,
-                <Icon type="edit" onClick={() => this.props.openEditProfile(this.props.profile)} />,
-                <Icon type="ellipsis" />
-              ]}
-            >
-              <Meta
-                avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-                title={this.props.profile.name}
-                description={this.props.profile.email}
-              />
-            </Card>
+          <div className="panel-header">
+            <h3>{this.props.profile.name}</h3>
           </div>
+          <Card
+            style={{ width: '100%' }}
+            cover={<img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />}
+            actions={[
+              <Icon onClick={() => console.log('settings clicked')} type="setting" />,
+              <Icon type="edit" onClick={() => this.props.openEditProfile(this.props.profile)} />,
+              <Icon type="ellipsis" />
+            ]}
+          >
+            <Meta
+              avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+              title={this.props.profile.name}
+              description={this.props.profile.email}
+            />
+          </Card>
+        </div>
 
-        }
-        <ProfileDrawer />
-      </div>
-    )
-  }
+      }
+      <ProfileDrawer />
+    </div>
+  )
+}
 }
 const mapStateToProps = (state) => {
   return {
