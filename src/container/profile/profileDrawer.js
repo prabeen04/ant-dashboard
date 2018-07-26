742ce049c22a59c773fbf41a3f1eec65fc1538b9
Prabeen04 <prabeen.strange@gmail.com>
2018-07-26 23:25:00 +0530

    ant-drawer added to profileDrawer

--- src/container/profile/profileDrawer.js
+++ src/container/profile/profileDrawer.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
+import { Drawer, Button } from 'antd';
 
 class ProfileDrawer extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <Drawer
+                    title={'cgcgh' >}
+                    placement="right"
+                    width={600}
+                    closable={true}
+                >
+                    hjghj
+                </Drawer>
+            </div>
+        )
+    }
 }
 export default ProfileDrawer;
