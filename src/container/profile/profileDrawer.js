632767131591303c5000796ecfccdb3585bf5c24
Prabeen04 <prabeen.strange@gmail.com>
2018-07-26 23:06:12 +0530

    profileDrawer component created

---
+++ src/container/profile/profileDrawer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class ProfileDrawer extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+export default ProfileDrawer;
