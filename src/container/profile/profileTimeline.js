125cf1f64eeef9a3c3ce4d16e41dbb8c34e07c00
Prabeen04 <prabeen.strange@gmail.com>
2018-07-27 20:29:04 +0530

    EventTimeline component added

--- src/container/profile/profileTimeline.js
+++ src/container/profile/profileTimeline.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react'
 import { connect } from "react-redux";
+import { Timeline, TimelineEvent } from 'react-event-timeline'
+
 class ProfileTimeline extends Component {
-  render() {
-    return (
-      <div>
-        {this.props.profiles.map((post, index) => {
-            return <p>{post.name}</p>
-        })}
-      </div>
-    )
-  }
+    render() {
+        const timelineEvents = this.props.profiles.map((post, index) => {
+            return <TimelineEvent title="John Doe sent a SMS"
+                                  createdAt="2016-09-12 10:06 PM"
+                                  icon={<i className="material-icons md-18">textsms</i>}
+                                 >
+                                I received the payment for $543. Should be shipping the item within a couple of hours.
+                    </TimelineEvent>
+        });
+        return (
+            <Timeline>
+                {timelineEvents}
+            </Timeline>
+        )
+    }
 }
 const mapStateToProps = state => {
     console.log(state.profileReducer.profiles)
-    return{
+    return {
         profiles: state.profileReducer.profiles
     }
 }
