9103b5ad6aef41dbe6872f2865831498ab495b5e
Prabeen04 <prabeen.strange@gmail.com>
2018-07-28 00:07:14 +0530

    firebase config file created

---
+++ src/config/firebase.js
@@ -0,0 +1,11 @@
+import * as firebase from 'firebase';
+  // Initialize Firebase
+  const config = {
+    apiKey: "AIzaSyBj3i3WQ_vChtU6ZdbfJDia9bsKTy5Chz0",
+    authDomain: "react-40b7f.firebaseapp.com",
+    databaseURL: "https://react-40b7f.firebaseio.com",
+    projectId: "react-40b7f",
+    storageBucket: "react-40b7f.appspot.com",
+    messagingSenderId: "95036717125"
+  };
+  firebase.initializeApp(config);
