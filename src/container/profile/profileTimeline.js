67f198d5e2b9f454086a2fc0e673b146240c8c9e
Prabeen04 <prabeen.strange@gmail.com>
2018-07-27 20:24:00 +0530

    Profiles fetched and rendered on ProfileDrawer

--- src/container/profile/profileTimeline.js
+++ src/container/profile/profileTimeline.js
@@ -4,14 +4,17 @@ class ProfileTimeline extends Component {
   render() {
     return (
       <div>
-        
+        {this.props.profiles.map((post, index) => {
+            return <p>{post.name}</p>
+        })}
       </div>
     )
   }
 }
 const mapStateToProps = state => {
+    console.log(state.profileReducer.profiles)
     return{
-        
+        profiles: state.profileReducer.profiles
     }
 }
 
