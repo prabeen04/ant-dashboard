209001b534c94bd4dbce66aea4b411bf4888d9ea
Prabeen04 <prabeen.strange@gmail.com>
2018-07-27 20:15:06 +0530

    ProfileTimeline component created

---
+++ src/container/profile/profileTimeline.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ProfileTimeline extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default ProfileTimeline;
