0f067ca93c1df84487a37e06820a63d1f8d2b7b0
Prabeen04 <prabeen.strange@gmail.com>
2018-07-28 12:14:41 +0530

    cratableSelect component created

---
+++ src/container/settings/profile/creatableSelect.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class CreatableSelect extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default CreatableSelect;
