cf05a2b93e4e3f7c1152023da62bfa5a83b63adc
Prabeen04 <prabeen.strange@gmail.com>
2018-07-28 12:15:58 +0530

    CreateSelect component added

--- src/container/settings/profile/creatableSelect.js
+++ src/container/settings/profile/creatableSelect.js
@@ -1,13 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import CreatableSelect from 'react-select/lib/Creatable';
+import { colourOptions } from './options';
 
 class CreatableSelect extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+    handleChange = (newValue: any, actionMeta: any) => {
+        console.group('Value Changed');
+        console.log(newValue);
+        console.log(`action: ${actionMeta.action}`);
+        console.groupEnd();
+    };
+    handleInputChange = (inputValue: any, actionMeta: any) => {
+        console.group('Input Changed');
+        console.log(inputValue);
+        console.log(`action: ${actionMeta.action}`);
+        console.groupEnd();
+    }
+    render() {
+        return (
+            <CreatableSelect
+                isClearable
+                onChange={this.handleChange}
+                onInputChange={this.handleInputChange}
+                options={colourOptions}
+            />
+        );
+    }
 }
 
 export default CreatableSelect;
