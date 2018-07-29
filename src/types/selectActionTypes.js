dd1674611b87e7d45025d261dce1ed05d949550c
Prabeen04 <prabeen.strange@gmail.com>
2018-07-29 16:06:22 +0530

    select Action types created

---
+++ src/types/selectActionTypes.js
@@ -0,0 +1,4 @@
+export const OPTION_CHANGE = 'OPTION_CHANGE';
+export const NEW_OPTION_CREATE = 'NEW_OPTION_CREATE';
+export const SELECT_CHANGE = 'SELECT_CHANGE';
+export const SELECT_CHANGE = 'SELECT_CHANGE';
