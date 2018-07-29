0f7f51694c89c1327968f113b015bda98ee2a8e9
Prabeen04 <prabeen.strange@gmail.com>
2018-07-29 16:54:50 +0530

    false Promise for new option creation added

--- src/actions/selectAction.js
+++ src/actions/selectAction.js
@@ -1,4 +1,5 @@
 import { OPTION_CHANGE, NEW_OPTION_CREATE, NEW_OPTION_CREATE_SUCCESS, NEW_OPTION_CREATE_FAILURE } from "../types/selectActionTypes";
+import { resolve } from "path";
 
 export const optionChange = value => dispatch => {
     console.log(value)
@@ -9,7 +10,19 @@ export const optionChange = value => dispatch => {
 }
 export const newOptionCreate = (inputValue) => dispatch => {
     console.log('new option create', inputValue)
-    return dispatch({
-        type: NEW_OPTION_CREATE
+    const sleep = () => new Promise(resolve => {
+        dispatch({
+            type: NEW_OPTION_CREATE
+        })
     })
+    sleep()
+        .then(() => {
+            setTimeout(() => {
+                return dispatch({
+                    type: NEW_OPTION_CREATE_SUCCESS,
+                    payload: inputValue
+                })
+            }, 2000)
+        })
+
 }
