f0eba0a41d2a9a40b0a3bfed380626afebb77784
Prabeen04 <prabeen.strange@gmail.com>
2018-08-01 20:54:15 +0530

    treesort component created

---
+++ src/container/treesort/treeSort.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class TreeSort extends Component {
+  render() {
+    return (
+      <div>
+        <h3>TreeSort Component</h3>
+      </div>
+    )
+  }
+}
+
+export default TreeSort
