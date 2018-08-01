0076ece4004b5270b785ee3fdaca543ee70d3dff
Prabeen04 <prabeen.strange@gmail.com>
2018-08-01 22:21:18 +0530

    App test file modified

--- test/app.test.js
+++ test/app.test.js
@@ -1,7 +1,10 @@
 // Just a demo test to wireup
-
+import React from 'react';
+import { shallow } from "enzyme";
+import App from '../src/App';
 describe('App', () => {
     it('should a classBase Component', () => {
+        const component = shallow(<App />)
         expect(component).to.have('render Method')
     })
 })
