4e6e878cf9f4fa44a84c33d7dd470bb0d7207256
Prabeen04 <prabeen.strange@gmail.com>
2018-08-01 20:59:46 +0530

    state and react-sortable-tree impors

--- src/container/treesort/treeSort.js
+++ src/container/treesort/treeSort.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
+import SortableTree from 'react-sortable-tree';
+import 'react-sortable-tree/style.css';
 
 class TreeSort extends Component {
-  render() {
-    return (
-      <div>
-        <h3>TreeSort Component</h3>
-      </div>
-    )
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            treeData: [{ title: 'Chicken', children: [{ title: 'Egg' }] }],
+        };
+    }
+    render() {
+        return (
+            <div>
+                <h3>TreeSort Component</h3>
+            </div>
+        )
+    }
 }
 
 export default TreeSort
