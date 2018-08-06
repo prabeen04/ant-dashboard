8c275afb726f086bcf473545b92e4fe24cef452c
Prabeen04 <prabeen.strange@gmail.com>
2018-08-06 23:46:23 +0530

    onSubmit method created

--- src/container/forms/reuseForm.js
+++ src/container/forms/reuseForm.js
@@ -3,10 +3,12 @@ import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { reduxForm, Field } from "redux-form";
 class ReuseForm extends Component {
+    onSubmit = values => console.log(values)
     render() {
+        const { handleSubmit } = this.props;
         return (
             <div>
-                <form>
+                <form onSubmit={handleSubmit(this.onSubmit)}>
                     <Field name="field1" component="input" />
                     <button>Submit</button>
                 </form>
