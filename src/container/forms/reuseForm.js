95f77ce77914f17a318d01ef3c50de6851daefb6
Prabeen04 <prabeen.strange@gmail.com>
2018-08-06 23:27:57 +0530

    MSTP and MDTP created

--- src/container/forms/reuseForm.js
+++ src/container/forms/reuseForm.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react'
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 class ReuseForm extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
-}
+    render() {
+        return (
+            <div>
 
+            </div>
+        )
+    }
+}
+const mapStateToProps = state => {
+    return {
+
+    }
+}
+const mapDispatchToProps = dispatch => {
+    return bindActionCreators({
+
+    })
+}
 export default connect(mapStateToProps, mapDispatchToProps)(ReuseForm);
