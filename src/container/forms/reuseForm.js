49334a99e78baf93236812553651c560113e23ff
Prabeen04 <prabeen.strange@gmail.com>
2018-08-06 23:50:26 +0530

    Custom inputRenderer created

--- src/container/forms/reuseForm.js
+++ src/container/forms/reuseForm.js
@@ -4,12 +4,18 @@ import { bindActionCreators } from "redux";
 import { reduxForm, Field } from "redux-form";
 class ReuseForm extends Component {
     onSubmit = values => console.log(values)
+    renderInput = (props) => (
+        <div>
+            <label>Field1</label>
+            <input type="text"/>
+        </div>
+    )
     render() {
         const { handleSubmit } = this.props;
         return (
             <div>
                 <form onSubmit={handleSubmit(this.onSubmit)}>
-                    <Field name="field1" component="input" />
+                    <Field name="field1" component={this.renderInput} />
                     <button>Submit</button>
                 </form>
             </div>
