8093a0076ecf29e3179e0438112301d577648d1a
Prabeen04 <prabeen.strange@gmail.com>
2018-08-06 23:31:36 +0530

    ReuseForm wrapped in reduxForm

--- src/container/forms/reuseForm.js
+++ src/container/forms/reuseForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
+import { reduxForm, Field } from "redux-form";
 class ReuseForm extends Component {
     render() {
         return (
@@ -10,6 +11,10 @@ class ReuseForm extends Component {
         )
     }
 }
+ReuseForm = reduxForm({
+    form: 'reuseForm'
+})(ReuseForm)
+
 const mapStateToProps = state => {
     return {
 
