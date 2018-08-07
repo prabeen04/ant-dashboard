da51c96ed3ce9ceb61766ea14684e9af33976e2f
Prabeen04 <prabeen.strange@gmail.com>
2018-08-07 20:26:58 +0530

    Map component added

--- src/components/google/googleMap.js
+++ src/components/google/googleMap.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react'
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 
 class GoogleMap extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <Map google={this.props.google} zoom={14}>
+
+                    <Marker onClick={this.onMarkerClick}
+                        name={'Current location'} />
+
+                    <InfoWindow onClose={this.onInfoWindowClose}>
+                        <div>
+                            <h1>{this.state.selectedPlace.name}</h1>
+                        </div>
+                    </InfoWindow>
+                </Map>
+            </div>
+        )
+    }
 }
 
-export default GoogleMap;
+export default GoogleApiWrapper({
+    apiKey: 'AIzaSyAYl3tV8A0nsXc7TVHUTEFFHPE9fBmy03s'
+})(GoogleMap)
