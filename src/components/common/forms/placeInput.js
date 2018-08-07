6086120c89247f81cb96d0d9985930f77db21cc1
Prabeen04 <prabeen.strange@gmail.com>
2018-08-07 12:53:17 +0530

    PlaceInput component created

---
+++ src/components/common/forms/placeInput.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Script from 'react-load-script';
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
+
+class PlaceInput extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default PlaceInput;
