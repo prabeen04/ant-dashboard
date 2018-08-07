1a624ed99ef47419efb3620a4c462b319aac7c04
Prabeen04 <prabeen.strange@gmail.com>
2018-08-07 13:00:35 +0530

    PlaceAutocomplete and Srcipt loader added

--- src/components/common/forms/placeInput.js
+++ src/components/common/forms/placeInput.js
@@ -3,13 +3,33 @@ import Script from 'react-load-script';
 import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
 
 class PlaceInput extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+    constructor(props) {
+        super(props)
+        this.state = {
+            isScriptLoaded: false,
+            value: null
+        }
+    }
+    handleScriptLoad = () => {
+        console.log('script is loaded')
+        this.setState({ isScriptLoaded: true })
+    }
+    render() {
+        return (
+            <div>
+                <Script
+                    url="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"
+                    onLoad={this.handleScriptLoad.bind(this)}
+                />
+                {this.state.isScriptLoaded &&
+                    <PlacesAutocomplete
+                        value={this.state.value}
+                        onChange={value => this.setState({ value })}
+                    />
+                }
+            </div>
+        )
+    }
 }
 
 export default PlaceInput;
