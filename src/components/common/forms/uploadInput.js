37b9c4aa11f50b9df38e202d01b7368903df6efe
Prabeen04 <prabeen.strange@gmail.com>
2018-08-08 21:10:24 +0530

    Upload props added

--- src/components/common/forms/uploadInput.js
+++ src/components/common/forms/uploadInput.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
 import { Upload, message, Button, Icon } from 'antd';
-
+const props = {
+    name: 'file',
+    action: '//jsonplaceholder.typicode.com/posts/',
+    headers: {
+        authorization: 'authorization-text',
+    },
+    onChange(info) {
+        if (info.file.status !== 'uploading') {
+            console.log(info.file, info.fileList);
+        }
+        if (info.file.status === 'done') {
+            message.success(`${info.file.name} file uploaded successfully`);
+        } else if (info.file.status === 'error') {
+            message.error(`${info.file.name} file upload failed.`);
+        }
+    },
+};
 class UploadInput extends Component {
+
     render() {
         return (
             <div>
-                <Upload>
+                <Upload {...props}>
                     <Button>
                         <Icon type="upload" /> Click to Upload
                     </Button>
