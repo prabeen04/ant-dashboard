1f8eb27b7d18a37c8c0e46ea58d7118f95b36747
Prabeen04 <prabeen.strange@gmail.com>
2018-08-08 21:06:17 +0530

    ant Upload component added

--- src/components/common/forms/uploadInput.js
+++ src/components/common/forms/uploadInput.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
+import { Upload, message, Button, Icon } from 'antd';
 
 class UploadInput extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <Upload>
+                    <Button>
+                        <Icon type="upload" /> Click to Upload
+                    </Button>
+                </Upload>
+            </div>
+        )
+    }
 }
 
 export default UploadInput;
