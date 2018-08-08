587b82ed1bba5beb9d900e51d7b73fc3fc6e2b7b
Prabeen04 <prabeen.strange@gmail.com>
2018-08-08 21:04:37 +0530

    UploadInput component created

---
+++ src/components/common/forms/uploadInput.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class UploadInput extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default UploadInput;
