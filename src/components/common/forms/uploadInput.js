9982922bc74d2ac9350f58deedd3ce7435bc78fa
Prabeen04 <prabeen.strange@gmail.com>
2018-08-09 22:01:32 +0530

    onChange in fileupload changed

--- src/components/common/forms/uploadInput.js
+++ src/components/common/forms/uploadInput.js
@@ -9,19 +9,14 @@ class UploadInput extends Component {
         }
     }
     onChange = (info) => {
-        axios.post('http://46.249.53.111:8080/salesxl/api/v2.0/upload/image', info.file, {
-            headers: { 'content-type': 'multipart/form-data' }
-        })
-                        .then(res => console.log(res))
-                        .catch(err => 'some error occoured')
-        // if (info.file.status !== 'uploading') {
-        //     console.log(info.file, info.fileList);
-        // }
-        // if (info.file.status === 'done') {
-        //     message.success(`${info.file.name} file uploaded successfully`);
-        // } else if (info.file.status === 'error') {
-        //     message.error(`${info.file.name} file upload failed.`);
-        // }
+        if (info.file.status !== 'uploading') {
+            console.log(info.file, info.fileList);
+        }
+        if (info.file.status === 'done') {
+            message.success(`${info.file.name} file uploaded successfully`);
+        } else if (info.file.status === 'error') {
+            message.error(`${info.file.name} file upload failed.`);
+        }
     }
     render() {
         return (
@@ -29,7 +24,7 @@ class UploadInput extends Component {
                 <Upload name='file'
                     action='http://46.249.53.111:8080/salesxl/api/v2.0/upload/image'
                     onChange={this.onChange}
-                    onPreview={(file)=>this.setState({file: file.url})}
+                    onPreview={(file) => this.setState({ file: file.url })}
                 >
                     <Button>
                         <Icon type="upload" /> Click to Upload
