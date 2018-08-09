0f131f5f983f661be992dc92109485b16c7e33a2
Prabeen04 <prabeen.strange@gmail.com>
2018-08-09 19:57:56 +0530

    marker and map style added

--- src/components/google/googleMap.js
+++ src/components/google/googleMap.js
@@ -4,21 +4,28 @@ import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 class GoogleMap extends Component {
     render() {
         return (
-            <div>
+            <div className='flex-container'>
                 <Map
                     google={this.props.google}
+                    style={{ width: 800, height: 500 }}
                     zoom={13}
                     initialCenter={{
                         lat: 40.854885,
                         lng: -88.081807
                     }}
-                    center= {this.props.latLng}
+                    center={this.props.latLng}
                 >
 
                     <Marker onClick={this.onMarkerClick}
-                        name={'Current location'} 
-                        position={this.props.latLng}/>
+                        name={'Current location'}
+                        position={this.props.latLng} />
 
+                    <Marker onClick={this.onMarkerClick}
+                        name={'Current location'}
+                        position={{
+                            lat: 40.854885,
+                            lng: -88.081807
+                        }} />
                     <InfoWindow onClose={this.onInfoWindowClose}>
                         <div>
                             <h1>{'this.state.selectedPlace.name'}</h1>
