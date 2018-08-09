51d9a5885391b1965f674f3902bc0a0a6171d843
Prabeen04 <prabeen.strange@gmail.com>
2018-08-09 21:19:54 +0530

    axios upload added to onChange

--- src/components/common/forms/uploadInput.js
+++ src/components/common/forms/uploadInput.js
@@ -14,14 +14,17 @@ class UploadInput extends Component {
         return;
     }
     onChange = (info) => {
-        if (info.file.status !== 'uploading') {
-            console.log(info.file, info.fileList);
-        }
-        if (info.file.status === 'done') {
-            message.success(`${info.file.name} file uploaded successfully`);
-        } else if (info.file.status === 'error') {
-            message.error(`${info.file.name} file upload failed.`);
-        }
+        axios.post('http://46.249.53.111:8080/salesxl/api/v2.0/upload/image', info.file)
+                        .then(res => console.log(res))
+                        .catch(err => 'some error occoured')
+        // if (info.file.status !== 'uploading') {
+        //     console.log(info.file, info.fileList);
+        // }
+        // if (info.file.status === 'done') {
+        //     message.success(`${info.file.name} file uploaded successfully`);
+        // } else if (info.file.status === 'error') {
+        //     message.error(`${info.file.name} file upload failed.`);
+        // }
     }
     render() {
         return (
@@ -29,8 +32,8 @@ class UploadInput extends Component {
                 <Upload name='file'
                     action={(file) => {
                         console.log(file)
-                        axios.post('http://46.249.53.111:8080/salesxl/api/v2.0/upload/image')
-                        .then(res => res.json())
+                        axios.post('http://46.249.53.111:8080/salesxl/api/v2.0/upload/image', file)
+                        .then(res => console.log(res))
                         .catch(err => 'some error occoured')
                     }}
                     beforeUpload={this.beforeUpload}
