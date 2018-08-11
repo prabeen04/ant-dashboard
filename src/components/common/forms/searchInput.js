1c16ecc5518049ba3d0c53628ea15aa6018284f6
Prabeen04 <prabeen.strange@gmail.com>
2018-08-11 23:33:48 +0530

    Switch Component created

--- src/components/common/forms/searchInput.js
+++ src/components/common/forms/searchInput.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Switch } from 'antd';
+
+const onChange = (checked) => console.log(checked)
 
 const SearchInput =  (props) => {
   return (
     <div>
-      
+        <Switch defaultChecked onChange={onChange} />
     </div>
   )
 }
