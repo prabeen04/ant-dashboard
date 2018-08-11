ab019164bb8c7b34e718453918c7a5dc9bea1193
Prabeen04 <prabeen.strange@gmail.com>
2018-08-11 23:18:54 +0530

    modal and image added

--- src/components/common/forms/uploadInput.js
+++ src/components/common/forms/uploadInput.js
@@ -12,10 +12,10 @@ class UploadInput extends Component {
     }
     handlePreview = (file) => {
         this.setState({
-          previewImage: file.url || file.thumbUrl,
-          previewVisible: true,
+            previewImage: file.url || file.thumbUrl,
+            previewVisible: true,
         });
-      }
+    }
     // actionHandler = (file) => {
     //     axios.post('http://46.249.53.111:8080/salesxl/api/v2.0/upload/image', file)
     //          .then(res => console.log(res))
@@ -23,17 +23,18 @@ class UploadInput extends Component {
     // }
     onChange = (info) => {
         console.log(info)
-            this.setState({ file: info.file })
-            if (info.file.status !== 'uploading') {
-                console.log(info.file, info.fileList);
-            }
-            if (info.file.status === 'done') {
-                message.success(`${info.file.name} file uploaded successfully`);
-            } else if (info.file.status === 'error') {
-                message.error(`${info.file.name} file upload failed.`);
-            }
+        this.setState({ file: info.file })
+        if (info.file.status !== 'uploading') {
+            console.log(info.file, info.fileList);
+        }
+        if (info.file.status === 'done') {
+            message.success(`${info.file.name} file uploaded successfully`);
+        } else if (info.file.status === 'error') {
+            message.error(`${info.file.name} file upload failed.`);
+        }
     }
     render() {
+        const { previewVisible, previewImage } = this.state
         return (
             <div>
                 <Upload name='file'
@@ -46,6 +47,9 @@ class UploadInput extends Component {
                         <Icon type="upload" /> Click to Upload
                     </Button>
                 </Upload>
+                <Modal visible={previewVisible} footer={null} onCancel={this.handleCancel}>
+                    <img alt="example" style={{ width: '100%' }} src={previewImage} />
+                </Modal>
                 {this.state.file && <img src={this.state.file} alt="" style={{ width: 200, height: 200 }} />}
             </div >
         )
