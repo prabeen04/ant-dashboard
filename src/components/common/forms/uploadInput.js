98d49951280bcac28f7a7e3c468f5e27e10731d5
Prabeen04 <prabeen.strange@gmail.com>
2018-08-12 22:44:55 +0530

    code cleaned

--- src/components/common/forms/uploadInput.js
+++ src/components/common/forms/uploadInput.js
@@ -23,24 +23,7 @@ class UploadInput extends Component {
         });
     }
     handleChange = ({ fileList }) => this.setState({ fileList })
-
-    // actionHandler = (file) => {
-    //     axios.post('http://46.249.53.111:8080/salesxl/api/v2.0/upload/image', file)
-    //          .then(res => console.log(res))
-    //          .catch(err => console.log(err))
-    // }
-    // onChange = (info) => {
-    //     console.log(info)
-    //     this.setState({ file: info.file })
-    //     if (info.file.status !== 'uploading') {
-    //         console.log(info.file, info.fileList);
-    //     }
-    //     if (info.file.status === 'done') {
-    //         message.success(`${info.file.name} file uploaded successfully`);
-    //     } else if (info.file.status === 'error') {
-    //         message.error(`${info.file.name} file upload failed.`);
-    //     }
-    // }
+    
     render() {
         const { previewVisible, previewImage } = this.state
         return (
