536778955667ae4de0bf6a8aea2802fa8f027947
Prabeen04 <prabeen.strange@gmail.com>
2018-08-13 23:22:49 +0530

    small stuff

--- src/components/common/forms/uploadInput.js
+++ src/components/common/forms/uploadInput.js
@@ -71,7 +71,7 @@ class UploadInput extends Component {
                 </Modal>
                 <div style={{ minHeight: 200, backgroundColor: '#f4f4f4', width: '100%' }}>
                     {this.state.uploadResponse && <img src={`http://46.249.53.111:8080/salesxl/api/v2.0/upload/image/${this.state.uploadResponse}`} alt="" />}
-                    {this.state.uploadResponse && <ImageCropper imageURL={`http://46.249.53.111:8080/salesxl/api/v2.0/upload/image/${this.state.uploadResponse}`}/>}
+                    <ImageCropper/>
                 </div>
             </div >
         )
