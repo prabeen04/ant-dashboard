0d2c64068019be704b1210719bdd4154f995a896
Prabeen04 <prabeen.strange@gmail.com>
2018-08-13 23:25:25 +0530

    cropper prop added

--- src/components/utils/ImageCropper.js
+++ src/components/utils/ImageCropper.js
@@ -6,7 +6,13 @@ export default class ImageCropper extends Component {
   render() {
     return (
       <div>
-        <ReactCrop src={'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png'}/>
+        <ReactCrop src={'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png'}
+          onChange= (crop) => {
+          console.log(crop)
+        }
+        crop = {
+          aspect: 16/9
+       }/>
       </div>
     )
   }
