74e4f8b3bc01fef17275796e1d6a6c02dc2de3f9
Prabeen04 <prabeen.strange@gmail.com>
2018-08-13 23:03:48 +0530

    ReactCropper component created

--- src/components/utils/ImageCropper.js
+++ src/components/utils/ImageCropper.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import ReactCrop from 'react-image-crop';
+import 'react-image-crop/dist/ReactCrop.css';
 
 export default class ImageCropper extends Component {
   render() {
     return (
       <div>
-        
+        <ReactCrop src="" />
       </div>
     )
   }
