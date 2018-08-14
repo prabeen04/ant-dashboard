67e51630a042153f2ea64e206178f544e02ed8ad
Prabeen04 <prabeen.strange@gmail.com>
2018-08-14 22:09:37 +0530

    src prop passed to CropImage component

--- src/components/common/forms/uploadInput.js
+++ src/components/common/forms/uploadInput.js
@@ -76,10 +76,10 @@ class UploadInput extends Component {
                 <Modal visible={previewVisible} footer={null} closable maskClosable onCancel={this.handleCancel}>
                     <img alt="example" style={{ width: '100%' }} src={previewImage} />
                 </Modal>
-                {/* <div style={{ minHeight: 200, backgroundColor: '#f4f4f4', width: '100%' }}>
-                    {this.state.uploadResponse && <img src={`https://fokuswork.com:8443/salesxl/api/v2.0/upload/image/${this.state.uploadResponse}`} alt="" />}
-                    <ImageCropper/>
-                </div> */}
+                <div style={{ minHeight: 200, backgroundColor: '#f4f4f4', width: '100%' }}>
+                    {/* {this.state.uploadResponse && <img src={`https://fokuswork.com:8443/salesxl/api/v2.0/upload/image/${this.state.uploadResponse}`} alt="" />} */}
+                    <ImageCropper src={`https://fokuswork.com:8443/salesxl/api/v2.0/upload/image/${this.state.uploadResponse}`}/>
+                </div>
             </div >
         )
     }
