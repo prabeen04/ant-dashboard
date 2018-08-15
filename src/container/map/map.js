87470987da4220392964ce8d4cdfb0cba6db349f
Prabeen04 <prabeen.strange@gmail.com>
2018-08-15 21:51:07 +0530

    more marker added in leaflet map

--- src/container/map/map.js
+++ src/container/map/map.js
@@ -32,7 +32,11 @@ class MyMap extends Component {
             { key: 'marker1', position: [11.5, 10.1], children: 'My first popup' },
             { key: 'marker2', position: [51.51, -0.1], children: 'My second popup' },
             { key: 'marker3', position: [2.49, 20.05], children: 'My third popup' },
+            { key: 'marker4', position: [52.49, 20.05], children: 'My fourth popup' },
+            { key: 'marker5', position: [29.49, 20.05], children: 'My fifth popup' },
+            { key: 'marker6', position: [2.49, 9.05], children: 'My sixth popup' },
         ]
+
         return (
             <div>
                 <Map center={position} zoom={this.state.zoom} viewport={this.state.viewport} className="leaflet-container">
