4d2d3f2fc810aa3cb83f5728e938b58207f066b0
Prabeen04 <prabeen.strange@gmail.com>
2018-08-16 22:09:13 +0530

    CustomField component created

---
+++ src/container/custom/customField.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class CustomField extends Component {
+  render() {
+    return (
+      <div>
+        <h2>CustomField Component</h2>
+      </div>
+    )
+  }
+}
+export default CustomField
