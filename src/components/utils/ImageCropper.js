e880917d220c110896481dabfa4aa28e8d651149
Prabeen04 <prabeen.strange@gmail.com>
2018-08-21 20:28:42 +0530

    getCroppedImage component created

--- src/components/utils/ImageCropper.js
+++ src/components/utils/ImageCropper.js
@@ -12,6 +12,37 @@ export default class ImageCropper extends Component {
       aspect: 1
     }
   }
+  function getCroppedImg(image, pixelCrop, fileName) {
+
+    const canvas = document.createElement('canvas');
+    canvas.width = pixelCrop.width;
+    canvas.height = pixelCrop.height;
+    const ctx = canvas.getContext('2d');
+  
+    ctx.drawImage(
+      image,
+      pixelCrop.x,
+      pixelCrop.y,
+      pixelCrop.width,
+      pixelCrop.height,
+      0,
+      0,
+      pixelCrop.width,
+      pixelCrop.height
+    );
+  
+    // As Base64 string
+    // const base64Image = canvas.toDataURL('image/jpeg');
+  
+    // As a blob
+    return new Promise((resolve, reject) => {
+      canvas.toBlob(file => {
+        file.name = fileName;
+        resolve(file);
+      }, 'image/jpeg');
+    });
+  }
+  
   handleCrop = () => {
     console.log(this.props)
   }
