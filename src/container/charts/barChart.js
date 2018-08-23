24964f7101272fef83e30ea1bb3615b4aa03b51d
Prabeen04 <prabeen.strange@gmail.com>
2018-08-23 19:41:27 +0530

    more barchart data added

--- src/container/charts/barChart.js
+++ src/container/charts/barChart.js
@@ -4,6 +4,8 @@ const data = [
     { name: 'food', uv: 2000, pv: 2013, amt: 4500, time: 1, uvError: [100, 50], pvError: [110, 20] },
     { name: 'cosmetic', uv: 3300, pv: 2000, amt: 6500, time: 2, uvError: 120, pvError: 50 },
     { name: 'storage', uv: 3200, pv: 1398, amt: 5000, time: 3, uvError: [120, 80], pvError: [200, 100] },
+    { name: 'async', uv: 3200, pv: 1398, amt: 5000, time: 3, uvError: [120, 80], pvError: [200, 100] },
+    { name: 'analog', uv: 3200, pv: 1398, amt: 5000, time: 3, uvError: [120, 80], pvError: [200, 100] },
     { name: 'digital', uv: 2800, pv: 2800, amt: 4000, time: 4, uvError: 100, pvError: 30 },
 ];
 
