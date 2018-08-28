960eb53e1a556b9e898343c53f2026a926d450a3
Prabeen04 <prabeen.strange@gmail.com>
2018-08-28 19:52:37 +0530

    moreStyles added

--- src/container/settings/calendar/progressLine.js
+++ src/container/settings/calendar/progressLine.js
@@ -2,30 +2,31 @@ import React, { Component } from 'react'
 import './progressLine.css';
 
 class ProgessLine extends Component {
-    constructor(props){
-        super(props)
+  constructor(props) {
+    super(props)
 
-        this.state = {
-            data: [
-                {title: 'value1', progress: 10},
-                {title: 'value2', progress: 15},
-                {title: 'value3', progress: 10},
-                {title: 'value4', progress: 25},
-                {title: 'value5', progress: 30},
-                {title: 'value6', progress: 10},
-            ]
-        }
+    this.state = {
+      data: [
+        { title: 'value1', progress: 10 },
+        { title: 'value2', progress: 15 },
+        { title: 'value3', progress: 10 },
+        { title: 'value4', progress: 25 },
+        { title: 'value5', progress: 30 },
+        { title: 'value6', progress: 10 },
+      ]
     }
+  }
   render() {
+    const progressRender = this.state.data.map((item, index) => {
+      return <div key={index} className="progress-item">
+        {item.title}
+      </div>
+    })
     return (
-      <div className="progressLine-wrapper">
-          <div className="progressLine">
-          {
-            this.state.data.map((item, index) => {
-              return <p key={index}>{item.title}</p>
-            })
-          }
-          </div>
+      <div className="progress-line-wrapper">
+        <div className="progress-line">
+          {progressRender }
+        </div>
       </div>
     )
   }
