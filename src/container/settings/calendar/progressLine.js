b20d16e4a20831d40b4996af15bac2f1bca81de0
Prabeen04 <prabeen.strange@gmail.com>
2018-08-28 21:01:30 +0530

    touched state added

--- src/container/settings/calendar/progressLine.js
+++ src/container/settings/calendar/progressLine.js
@@ -7,31 +7,36 @@ class ProgessLine extends Component {
 
     this.state = {
       data: [
-        { id: 1, title: 'value1', progress: 10, loading: false },
-        { id: 2, title: 'value2', progress: 15, loading: false },
-        { id: 3, title: 'value3', progress: 10, loading: false },
-        { id: 4, title: 'value4', progress: 25, loading: false },
-        { id: 5, title: 'value5', progress: 30, loading: false },
-        { id: 6, title: 'value6', progress: 10, loading: false },
+        { id: 1, title: 'value1', progress: 10, loading: false, touched: false },
+        { id: 2, title: 'value2', progress: 15, loading: false, touched: false },
+        { id: 3, title: 'value3', progress: 10, loading: false, touched: false },
+        { id: 4, title: 'value4', progress: 25, loading: false, touched: false },
+        { id: 5, title: 'value5', progress: 30, loading: false, touched: false },
+        { id: 6, title: 'value6', progress: 10, loading: false, touched: false },
       ]
     }
   }
   handleClick = (item) => {
-    let newData = this.state.data.map(element => element.id === item.id ? {...element, loading: true}: element)
-      this.setState({
-        data: newData
-      })
+    let newData = this.state.data.map(element => {
+      return element.id === item.id ? { ...element, loading: true, touched: true } : element
+    }
+    )
+    this.setState({
+      data: newData
+    })
 
   }
   render() {
     const renderProgress = this.state.data.map((item, index) => {
       const flexBasis = `${item.progress}%`
+      const backgroundColor =  item.touched ? 'green': '#fff';
+      const color = item.touched ? '#fff': '#444';
       return (<div
         key={index}
         className="progress-item"
-        style={{ flexBasis }}
+        style={{ flexBasis, backgroundColor, color }}
         onClick={() => this.handleClick(item)}>
-        {item.loading ? 'Loading...': item.title}
+        {item.loading ? 'Loading...' : item.title}
       </div>)
     })
     return (
