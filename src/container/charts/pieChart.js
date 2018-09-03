c71c212e92cea3e5ca5520d64937b292f2456978
Prabeen04 <prabeen.strange@gmail.com>
2018-09-03 20:35:44 +0530

    rgba colors added

--- src/container/charts/pieChart.js
+++ src/container/charts/pieChart.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react'
 import { PieChart, Pie, Legend, Cell, Tooltip, ResponsiveContainer, Sector, Label, LabelList } from 'recharts';
 const data01 = [
-  { name: 'Group A', value: 400, v: 89, color: '#4286f4' },
-  { name: 'Group B', value: 300, v: 100, color: '#f44256' },
-  { name: 'Group C', value: 200, v: 200, color: '#70ed36' },
-  { name: 'Group D', value: 200, v: 20, color: '#7c89ba' },
-  { name: 'Group E', value: 278, v: 40, color: '#4245f4' },
-  { name: 'Group F', value: 189, v: 60, color: '#ddf738' },
+  { name: 'Group A', value: 400, v: 89, color: 'rgba(52, 182, 193, 0.5)' },
+  { name: 'Group B', value: 300, v: 100, color: 'rgba(56, 206, 33, 0.5)' },
+  { name: 'Group C', value: 200, v: 200, color: 'rgba(208, 88, 216, 0.5)' },
+  { name: 'Group D', value: 200, v: 20, color: 'rgba(206, 66, 47, 0.5)' },
+  { name: 'Group E', value: 278, v: 40, color: 'rgba(72, 84, 191, 0.5)' },
+  { name: 'Group F', value: 189, v: 60, color: 'rgba(222, 249, 47, 0.5)' },
 ];
 
 const data02 = [
-  { name: 'Group A', value: 2400, color: '#4286f4' },
-  { name: 'Group B', value: 4567, color: '#70ed36' },
-  { name: 'Group C', value: 1398, color: '#ddf738' },
-  { name: 'Group D', value: 9800, color: '#4245f4' },
-  { name: 'Group E', value: 3908, color: '#7c89ba' },
-  { name: 'Group F', value: 4800, color: '#f44256' },
+  { name: 'Group A', value: 2400, color: 'rgba(52, 182, 193, 0.5)' },
+  { name: 'Group B', value: 4567, color: 'rgba(208, 88, 216, 0.5)' },
+  { name: 'Group C', value: 1398, color: 'rgba(56, 206, 33, 0.5)' },
+  { name: 'Group D', value: 9800, color: 'rgba(206, 66, 47, 0.5)' },
+  { name: 'Group E', value: 3908, color: 'rgba(72, 84, 191, 0.5)' },
+  { name: 'Group F', value: 4800, color: 'rgba(222, 249, 47, 0.5)' },
 ];
 class PieChart1 extends Component {
   constructor(props){
