20af008fee1e758881f47a5669d02c512d832093
Prabeen04 <prabeen.strange@gmail.com>
2018-09-03 21:41:49 +0530

    code cleaned

--- src/components/utils/ImageCropper.js
+++ src/components/utils/ImageCropper.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import ReactCrop, {makeAspectCrop} from 'react-image-crop';
+import ReactCrop, { makeAspectCrop } from 'react-image-crop';
 import { Button } from 'antd'
 import 'react-image-crop/dist/ReactCrop.css';
 
@@ -33,47 +33,8 @@ export default class ImageCropper extends Component {
 
     this.setState({ crop });
   }
-  getCroppedImg = (image, pixelCrop, fileName) => {
-    const canvas = document.createElement('canvas');
-    canvas.width = pixelCrop.width;
-    canvas.height = pixelCrop.height;
-    const ctx = canvas.getContext('2d');
-    const myImage = document.createElement('img')
-    myImage.src = this.props.src
-    // console.log(canvas.toDataURL('image/jpeg'))
-    ctx.drawImage(
-      myImage,
-      pixelCrop.x,
-      pixelCrop.y,
-      pixelCrop.width,
-      pixelCrop.height,
-      0,
-      0,
-      pixelCrop.width,
-      pixelCrop.height
-    );
-
-    // As Base64 string
-    const base64Image = canvas.toDataURL('image/jpeg');
-    console.log(base64Image)
-    return base64Image
-    // As a blob
-    // return new Promise((resolve, reject) => {
-    //   canvas.toBlob(file => {
-    //     // file.name = fileName;
-    //     resolve(file);
-    //   }, 'image/jpeg');
-    // });
-  }
-
   handleCrop = () => {
-    const testImage = this.getCroppedImg(this.props.file, this.state.crop, 'testImage')
-    this.setState({
-      croppedImage: testImage,
-      showCroppedImage: true
-    })
-    // .then(res => console.log(typeof res))
-    // .catch(err => console.log(err))
+    console.log('handle crop called')
   }
   handleChange = (crop) => {
     this.setState({ crop })
@@ -84,7 +45,7 @@ export default class ImageCropper extends Component {
         <ReactCrop
           src={this.props.src || 'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png'}
           onChange={this.handleChange}
-          onImageLoaded = {this.onImageLoaded}
+          onImageLoaded={this.onImageLoaded}
           crop={this.state.crop}
         />
         {this.props.src && <Button type="primary" onClick={this.handleCrop}>Crop Here</Button>}
