449f405ffd014c061676a9da76b6577833a73894
Prabeen04 <prabeen.strange@gmail.com>
2018-09-06 17:35:18 +0530

    chart data fetched from remote api

--- src/container/charts/pieChart.js
+++ src/container/charts/pieChart.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { PieChart, Pie, Legend, Cell, Tooltip, ResponsiveContainer, Sector, Label, LabelList } from 'recharts';
+import axios from 'axios';
 const data01 = [
   { name: 'Group A', value: 400, v: 89, color: 'rgba(52, 182, 193, 0.7)' },
   { name: 'Group B', value: 300, v: 100, color: 'rgba(56, 206, 33, 0.7)' },
@@ -20,21 +21,36 @@ const data02 = [
 class PieChart1 extends Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+       chartData: []
+    }
   }
+  componentDidMount = () => {
+    axios.get('http://192.168.1.61:3000/oppo')
+    .then(res =>{ 
+      console.log(res)
+      this.setState({
+        chartData: res.data
+      })
+    })
+    .catch(err => console.log(err))
+  }
+  
   render() {
     return (
       <div style={{ margin: '0.2rem' }}>
         <PieChart width={this.props.width} height={this.props.height} >
-          <Pie data={data01} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={50}>
+          <Pie data={this.state.chartData} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={50}>
             {
               data01.map((entry, index) => <Cell fill={entry.color} />)
             }
           </Pie>
-          <Pie data={data02} dataKey="value" nameKey="name" cx="50%" cy="50%" innerRadius={60} outerRadius={80} label>
+          {/* <Pie data={data02} dataKey="value" nameKey="name" cx="50%" cy="50%" innerRadius={60} outerRadius={80} label>
             {
               data02.map((entry, index) => <Cell fill={entry.color} />)
             }
-          </Pie>
+          </Pie> */}
           <Tooltip />
           {/* <Legend/> */}
         </PieChart>
