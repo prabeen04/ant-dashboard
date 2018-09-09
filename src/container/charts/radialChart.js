b62b2c3a1cae9ef0cada0b982bc07f2fc45acdc8
Prabeen04 <prabeen.strange@gmail.com>
2018-09-09 20:51:26 +0530

    RadialChart components imported and dummy data added

--- src/container/charts/radialChart.js
+++ src/container/charts/radialChart.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react'
+import { RadialBarChart, RadialBar, Legend } from "rechart";
+const data = [
+    { name: '18-24', uv: 31.47, pv: 2400, fill: '#8884d8' },
+    { name: '25-29', uv: 26.69, pv: 4567, fill: '#83a6ed' },
+    { name: '30-34', uv: 15.69, pv: 1398, fill: '#8dd1e1' },
+    { name: '35-39', uv: 8.22, pv: 9800, fill: '#82ca9d' },
+    { name: '40-49', uv: 8.63, pv: 3908, fill: '#a4de6c' },
+    { name: '50+', uv: 2.63, pv: 4800, fill: '#d0ed57' },
+    { name: 'unknow', uv: 6.67, pv: 4800, fill: '#ffc658' }
+];
 
 class RadialChart1 extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
 }
 
 export default RadialChart1;
