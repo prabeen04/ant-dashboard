c4525edec41501cbb53707ea4b090c4641224daf
Prabeen04 <prabeen.strange@gmail.com>
2018-09-09 20:48:17 +0530

    radial chart component created

---
+++ src/container/charts/radialChart.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class RadialChart extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default RadialChart;
