705a7c91d4ab4e4eb929234d17aa44f4e4770120
Prabeen04 <prabeen.strange@gmail.com>
2018-09-09 20:52:47 +0530

    style added

--- src/container/charts/radialChart.js
+++ src/container/charts/radialChart.js
@@ -9,6 +9,11 @@ const data = [
     { name: '50+', uv: 2.63, pv: 4800, fill: '#d0ed57' },
     { name: 'unknow', uv: 6.67, pv: 4800, fill: '#ffc658' }
 ];
+const style = {
+    top: 0,
+    left: 350,
+    lineHeight: '24px'
+};
 
 class RadialChart1 extends Component {
     render() {
