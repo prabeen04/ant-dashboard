9fecc216da7c58801426a3579976919cb62fce13
Prabeen04 <prabeen.strange@gmail.com>
2018-09-09 21:22:29 +0530

    state added

--- src/container/settings/transfer/transfer.js
+++ src/container/settings/transfer/transfer.js
@@ -2,26 +2,33 @@ import React, { Component } from 'react'
 import { Transfer } from "antd";
 const mockData = [];
 for (let i = 0; i < 20; i++) {
-  mockData.push({
-    key: i.toString(),
-    title: `content${i + 1}`,
-    description: `description of content${i + 1}`,
-    disabled: i % 3 < 1,
-  });
+    mockData.push({
+        key: i.toString(),
+        title: `content${i + 1}`,
+        description: `description of content${i + 1}`,
+        disabled: i % 3 < 1,
+    });
 }
 
 const targetKeys = mockData
-  .filter(item => +item.key % 3 > 1)
-  .map(item => item.key);
-  
+    .filter(item => +item.key % 3 > 1)
+    .map(item => item.key);
+
 class Transfer extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+    constructor(props) {
+        super(props)
+        this.state = {
+            targetKeys,
+            selectedKeys: [],
+        }
+    }
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
 }
 
 export default Transfer;
