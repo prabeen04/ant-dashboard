aab47d8957f506c428bc0baf1c3a336cb0cca47c
Prabeen04 <prabeen.strange@gmail.com>
2018-09-09 21:32:41 +0530

    code cleaned

--- src/container/settings/transfer/columnTransfer.js
+++ src/container/settings/transfer/columnTransfer.js
@@ -24,17 +24,10 @@ class ColumnTransfer extends Component {
     }
     handleChange = (nextTargetKeys, direction, moveKeys) => {
         this.setState({ targetKeys: nextTargetKeys });
-
-        console.log('targetKeys: ', targetKeys);
-        console.log('direction: ', direction);
-        console.log('moveKeys: ', moveKeys);
     }
 
     handleSelectChange = (sourceSelectedKeys, targetSelectedKeys) => {
         this.setState({ selectedKeys: [...sourceSelectedKeys, ...targetSelectedKeys] });
-
-        console.log('sourceSelectedKeys: ', sourceSelectedKeys);
-        console.log('targetSelectedKeys: ', targetSelectedKeys);
     }
 
     handleScroll = (direction, e) => {
