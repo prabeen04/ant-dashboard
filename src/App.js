feb93b3c322fe5ab86939bf5c242d04cdd736b40
Prabeen04 <prabeen.strange@gmail.com>
2018-09-15 19:20:38 +0530

    code cleaned

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import React, { Component } from 'react';
-import Loadable from "react-loadable";
 import Navbar from './components/layouts/navbar';
 import './App.css';
-import AppLoading from './components/loading/appLoading'; 
 
-// const AsyncNavbar = Loadable({
-//   loader: () => import('./components/layouts/navbar'),
-//   loading: AppLoading,
-// })
 const App = (props) => {
   return (
     <div className="wrapper">
