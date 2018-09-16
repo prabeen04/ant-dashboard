5232cbcbd032e516ba73612e5704c977e9e8a7cb
Prabeen04 <prabeen.strange@gmail.com>
2018-09-16 16:59:55 +0530

    left and right component expected at @actionHeader

--- src/components/layouts/ActionHeader.js
+++ src/components/layouts/ActionHeader.js
@@ -4,7 +4,8 @@ import './ActionHeader.css';
 const ActionHeader = (props) => {
     return (
         <div className="action-header">
-            <h3>test</h3>
+            <h3>{props.leftComponent}</h3>
+            <h3>{props.rightComponent}</h3>
         </div>
     )
 }
