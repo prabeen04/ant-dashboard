45dd3643c272fbdc02593ffca45ea5393f881fad
Prabeen04 <prabeen.strange@gmail.com>
2018-09-16 17:12:07 +0530

    prop-types experiment

--- src/components/layouts/ActionHeader.js
+++ src/components/layouts/ActionHeader.js
@@ -5,13 +5,13 @@ import './ActionHeader.css';
 const ActionHeader = (props) => {
     return (
         <div className="action-header">
-            <h3>{props.leftComponent()}</h3>
-            <h3>{props.rightComponent}</h3>
+            <div>{props.leftComponent()}</div>
+            <div>{props.rightComponent}</div>
         </div>
     )
 }
-ActionHeader.prototype = {
-    leftComponent: PropTypes.string,
+ActionHeader.propTypes = {
+    leftComponent: PropTypes.func.isRequired,
     rightComponent: PropTypes.string,
 }
 export default ActionHeader
