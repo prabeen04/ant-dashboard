1b47c9aacfe571684d359fef1b4e3c9c92d9832e
Prabeen04 <prabeen.strange@gmail.com>
2018-09-19 20:43:12 +0530

    advancedbarchart component created

---
+++ src/container/charts/AdvancedBarChart.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class AdvancedBarChart extends Component {
+  render() {
+    return (
+      <div>
+        <h3>AdvancedBarChart Component</h3>
+      </div>
+    )
+  }
+}
+
+export default AdvancedBarChart;
