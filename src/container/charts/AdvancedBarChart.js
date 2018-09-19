137bc0ae24bcb54c56e665c677551db65b424957
Prabeen04 <prabeen.strange@gmail.com>
2018-09-19 21:39:42 +0530

    small stuff

--- src/container/charts/AdvancedBarChart.js
+++ src/container/charts/AdvancedBarChart.js
@@ -18,36 +18,41 @@ class AdvancedBarChart extends Component {
                 { name: '8', amt: 5410 },
                 { name: '9', amt: 5000 },
                 { name: '10', amt: 1222 },
-                { name: '11', amt: 8744 },
-                { name: '12', amt: 9800 },
-                { name: '13', amt: 5410 },
-                { name: '14', amt: 5000 },
-                { name: '15', amt: 1222 },
-                { name: '16', amt: 8744 },
-                { name: '17', amt: 9800 },
-                { name: '18', amt: 5410 },
-                { name: '19', amt: 5000 },
-                { name: '20', amt: 1222 },
-                { name: '21', amt: 8744 },
-                { name: '22', amt: 9800 },
-                { name: '23', amt: 5410 },
-                { name: '24', amt: 5000 },
-                { name: '25', amt: 1222 },
-                { name: '26', amt: 8744 },
-                { name: '27', amt: 9800 },
-                { name: '28', amt: 5410 },
-                { name: '29', amt: 5000 },
-                { name: '30', amt: 1222 },
+                // { name: '11', amt: 8744 },
+                // { name: '12', amt: 9800 },
+                // { name: '13', amt: 5410 },
+                // { name: '14', amt: 5000 },
+                // { name: '15', amt: 1222 },
+                // { name: '16', amt: 8744 },
+                // { name: '17', amt: 9800 },
+                // { name: '18', amt: 5410 },
+                // { name: '19', amt: 5000 },
+                // { name: '20', amt: 1222 },
+                // { name: '21', amt: 8744 },
+                // { name: '22', amt: 9800 },
+                // { name: '23', amt: 5410 },
+                // { name: '24', amt: 5000 },
+                // { name: '25', amt: 1222 },
+                // { name: '26', amt: 8744 },
+                // { name: '27', amt: 9800 },
+                // { name: '28', amt: 5410 },
+                // { name: '29', amt: 5000 },
+                // { name: '30', amt: 1222 },
             ]
         }
+    }
+    displayTick = (tick) => {
+        return `Day ${tick}`;
     }
     render() {
         return (
             <div>
                 <h3>AdvancedBarChart Component</h3>
-                <BarChart width={this.props.width} height={this.props.height} data={this.state.chartData}>
+                <BarChart width={this.props.width} height={this.props.height} data={this.state.data}>
                     <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="name">
+                    <XAxis
+                        dataKey="name"
+                        tickFormatter={this.displayTick}>
                         <Label value="Pages of my website" offset={0} position="insideBottom" />
                     </XAxis>
                     <YAxis />
