eff914f2667074504ba591c72c0a8a118ba006e2
Prabeen04 <prabeen.strange@gmail.com>
2018-09-19 20:48:46 +0530

    chart data in state added

--- src/container/charts/AdvancedBarChart.js
+++ src/container/charts/AdvancedBarChart.js
@@ -1,13 +1,50 @@
 import React, { Component } from 'react'
 
 class AdvancedBarChart extends Component {
-  render() {
-    return (
-      <div>
-        <h3>AdvancedBarChart Component</h3>
-      </div>
-    )
-  }
+    constructor(props) {
+        super(props)
+        this.state = {
+            data: [
+                { name: '1', amt: 4500 },
+                { name: '2', amt: 6500 },
+                { name: '3', amt: 5000 },
+                { name: '4', amt: 5000 },
+                { name: '5', amt: 5000 },
+                { name: '6', amt: 4000 },
+                { name: '7', amt: 9800 },
+                { name: '8', amt: 5410 },
+                { name: '9', amt: 5000 },
+                { name: '10', amt: 1222 },
+                { name: '11', amt: 8744 },
+                { name: '12', amt: 9800 },
+                { name: '13', amt: 5410 },
+                { name: '14', amt: 5000 },
+                { name: '15', amt: 1222 },
+                { name: '16', amt: 8744 },
+                { name: '17', amt: 9800 },
+                { name: '18', amt: 5410 },
+                { name: '19', amt: 5000 },
+                { name: '20', amt: 1222 },
+                { name: '21', amt: 8744 },
+                { name: '22', amt: 9800 },
+                { name: '23', amt: 5410 },
+                { name: '24', amt: 5000 },
+                { name: '25', amt: 1222 },
+                { name: '26', amt: 8744 },
+                { name: '27', amt: 9800 },
+                { name: '28', amt: 5410 },
+                { name: '29', amt: 5000 },
+                { name: '30', amt: 1222 },
+            ]
+        }
+    }
+    render() {
+        return (
+            <div>
+                <h3>AdvancedBarChart Component</h3>
+            </div>
+        )
+    }
 }
 
 export default AdvancedBarChart;
