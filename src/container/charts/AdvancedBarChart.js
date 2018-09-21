a4bb2ac03147f9b015d9689517508a173b17ef9e
Prabeen04 <prabeen.strange@gmail.com>
2018-09-21 22:12:56 +0530

    small stuff

--- src/container/charts/AdvancedBarChart.js
+++ src/container/charts/AdvancedBarChart.js
@@ -121,15 +121,14 @@ class AdvancedBarChart extends Component {
     componentDidMount() {
         const newData = this.state.data.map((event, i) => ({ eventId: event.eventId, eventType: event.eventType, startDate: moment(event.startDate).day() }))
             .reduce((acc, data, i, arr) => {
-                if (!acc[data.eventType]) {
-                    acc[data.eventType] = 1;
-                } else {
-                    acc[data.eventType] += 1;
-                }
-                return acc;
+                // if (!acc[data.eventType]) {
+                //     acc[data.eventType] = 1;
+                // } else {
+                //     acc[data.eventType] += 1;
+                // }
+                // return acc;
             }, {});
         console.log(newData);
-        console.log(Object.entries(newData));
         // this.setState({
         //     chartData: newData
         // })
