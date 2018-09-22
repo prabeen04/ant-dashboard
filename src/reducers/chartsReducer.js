53ca8b9dc84a7cf4a308b545795fb6ae15f6f072
Prabeen04 <prabeen.strange@gmail.com>
2018-09-22 22:29:47 +0530

    initialData addedd

--- src/reducers/chartsReducer.js
+++ src/reducers/chartsReducer.js
@@ -1,4 +1,108 @@
-const initialState = {};
+const initialState = {
+    data: [
+        {
+            eventId: "EVEN00001",
+            eventType: "call",
+            eventOwnerId: "user00001",
+            eventHeading: "eventHeading",
+            eventDescription: "eventDescription",
+            timeZone: "UCT",
+            startDate: "2018-09-01T21:30:17+05:30",
+            endDate: "2018-09-19T21:35:17+05:30",
+            eventClosureDate: "2018-09-19T21:30:17+05:30"
+        },
+        {
+            eventId: "EVEN00002",
+            eventType: "call",
+            eventOwnerId: "user00001",
+            eventHeading: "eventHeading",
+            eventDescription: "eventDescription",
+            timeZone: "UCT",
+            startDate: "2018-09-02T21:30:17+05:30",
+            endDate: "2018-09-19T21:35:17+05:30",
+            eventClosureDate: "2018-09-19T21:30:17+05:30"
+        },
+        {
+            eventId: "EVEN00003",
+            eventType: "meeting",
+            eventOwnerId: "user00001",
+            eventHeading: "eventHeading",
+            eventDescription: "eventDescription",
+            timeZone: "UCT",
+            startDate: "2018-09-03T21:30:17+05:30",
+            endDate: "2018-09-19T21:35:17+05:30",
+            eventClosureDate: "2018-09-19T21:30:17+05:30"
+        },
+        {
+            eventId: "EVEN00004",
+            eventType: "call",
+            eventOwnerId: "user00001",
+            eventHeading: "eventHeading",
+            eventDescription: "eventDescription",
+            timeZone: "UCT",
+            startDate: "2018-09-04T21:30:17+05:30",
+            endDate: "2018-09-19T21:35:17+05:30",
+            eventClosureDate: "2018-09-19T21:30:17+05:30"
+        },
+        {
+            eventId: "EVEN00005",
+            eventType: "meeting",
+            eventOwnerId: "user00001",
+            eventHeading: "eventHeading",
+            eventDescription: "eventDescription",
+            timeZone: "UCT",
+            startDate: "2018-09-05T21:30:17+05:30",
+            endDate: "2018-09-19T21:35:17+05:30",
+            eventClosureDate: "2018-09-19T21:30:17+05:30"
+        },
+        {
+            eventId: "EVEN00006",
+            eventType: "email",
+            eventOwnerId: "user00001",
+            eventHeading: "eventHeading",
+            eventDescription: "eventDescription",
+            timeZone: "UCT",
+            startDate: "2018-09-06T21:30:17+05:30",
+            endDate: "2018-09-19T21:35:17+05:30",
+            eventClosureDate: "2018-09-19T21:30:17+05:30"
+        },
+        {
+            eventId: "EVEN00007",
+            eventType: "call",
+            eventOwnerId: "user00001",
+            eventHeading: "eventHeading",
+            eventDescription: "eventDescription",
+            timeZone: "UCT",
+            startDate: "2018-09-07T21:30:17+05:30",
+            endDate: "2018-09-19T21:35:17+05:30",
+            eventClosureDate: "2018-09-19T21:30:17+05:30"
+        },
+        {
+            eventId: "EVEN00008",
+            eventType: "meeting",
+            eventOwnerId: "user00001",
+            eventHeading: "eventHeading",
+            eventDescription: "eventDescription",
+            timeZone: "UCT",
+            startDate: "2018-09-08T21:30:17+05:30",
+            endDate: "2018-09-19T21:35:17+05:30",
+            eventClosureDate: "2018-09-19T21:30:17+05:30"
+        },
+        {
+            eventId: "EVEN00009",
+            eventType: "call",
+            eventOwnerId: "user00001",
+            eventHeading: "eventHeading",
+            eventDescription: "eventDescription",
+            timeZone: "UCT",
+            startDate: "2018-09-09T21:30:17+05:30",
+            endDate: "2018-09-19T21:35:17+05:30",
+            eventClosureDate: "2018-09-19T21:30:17+05:30"
+        },
+
+    ],
+
+};
 export const chartsReducer = (state = initialState, action) => {
     return state;
 }
