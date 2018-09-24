e867efc22110d44fdd444926af8fc44d24036f50
Prabeen04 <prabeen.strange@gmail.com>
2018-09-24 21:45:46 +0530

    reduce method implemention cleared

--- src/actions/chartsAction.js
+++ src/actions/chartsAction.js
@@ -65,36 +65,6 @@ export const setBarChartData = data => dispatch => {
 export const setBarChartData2 = data => dispatch => {
     const trimedData = data.map((item, i) => ({ eventId: item.eventId, eventType: item.eventType, startDate: moment(item.startDate).format('ddd') }))
         .reduce((acc, item, i, arr) => {
-            console.log(acc[i]);
-            if (acc && acc.length) {
-                console.log(acc);
-                console.log('inside first if')
-                if (acc[i] && acc[i].hasOwnProperty('date')) {
-                    console.log('inside second if')
-                    acc.push({
-                        date: item.startDate,
-                        value: 1
-                    })
-                } else {
-                    console.log('inside second else')
-                    acc.push({
-                        date: item.startDate,
-                        value: item.eventType
-                    })
-                }
-            }
-            else {
-                console.log('inside else')
-                let obj = {
-                    date: item.startDate,
-                    value: 1
-                }
-                acc.push(obj)
-            }
-            // console.log(JSON.stringify(acc, null, 4))
-            // console.log(JSON.stringify(acc[i], null, 4))
-            console.log(acc[i])
-            return acc;
         }, []);
     console.log(trimedData)
 }
