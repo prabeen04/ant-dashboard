c79eae8ad57b013104c55cc32b892c7dc4bbb5a6
Prabeen04 <prabeen.strange@gmail.com>
2018-09-25 08:40:41 +0530

    small stuff

--- src/components/utils/TimeInterval.js
+++ src/components/utils/TimeInterval.js
@@ -2,24 +2,22 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class TimeInterval extends Component {
-  render() {
-      const { times } = this.props;
-    return (
-      <div>
-        {times.map((time, i) => {
-            return (
-                <div className="time-interval">
-                    <p>{time}</p>
-                </div>
-            )
-        })}
-      </div>
-    )
-  }
+    render() {
+        const { times } = this.props;
+        return (
+            <div>
+                {times.map((time, i) => {
+                    return <div className="time-interval" key={i}>
+                        <p>{time}</p>
+                    </div>
+                })}
+            </div>
+        )
+    }
 }
 
-TimeInterval.propTypes({
+TimeInterval.propTypes = {
     times: PropTypes.array
-})
+}
 
 export default TimeInterval;
