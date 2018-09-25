886bbdce537a2c0973fde6b81f46e10e7d971932
Prabeen04 <prabeen.strange@gmail.com>
2018-09-25 08:26:41 +0530

    prop-types added

--- src/components/utils/TimeInterval.js
+++ src/components/utils/TimeInterval.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class TimeInterval extends Component {
   render() {
+      const { times } = this.props;
     return (
       <div>
-        
+        {}
       </div>
     )
   }
 }
+
+TimeInterval.propTypes({
+    times: PropTypes.array;
+})
