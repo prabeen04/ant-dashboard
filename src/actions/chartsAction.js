107fc4b8ddd66ece72422eaa97f503fcb8c04233
Prabeen04 <prabeen.strange@gmail.com>
2018-10-04 20:33:49 +0530

    small stuff

--- src/actions/chartsAction.js
+++ src/actions/chartsAction.js
@@ -1,36 +1,36 @@
 import { GET_CHART_DATA, SET_INTERNAL_VIEW_TYPE, SET_CHART_VIEW_TYPE, SET_BAR_CHART_DATA, SET_PIE_CHART_DATA } from "../types/chartActionTypes";
 import moment from 'moment';
 import { Math } from "core-js";
-export const setPieChartData = (viewType, data) => dispatch => {
-    console.log(viewType)
-    const newData = data.map((event, i) => ({ eventId: event.eventId, eventType: event.eventType, startDate: moment(event.startDate).day() }))
-        .reduce((acc, data, i, arr) => {
-            if (!acc[data.eventType]) {
-                console.log('inside IF')
-                acc[data.eventType] = 1;
-            } else {
-                console.log('inside ELSE')
-                acc[data.eventType] += 1;
-            }
-            return acc; 
-        }, {});
-    console.log(newData)
-    const eventArr = Object.entries(newData)
-        .reduce((acc, node, i, arr) => {
-            let [type, val] = node;
-            acc.push({
-                name: type,
-                value: val
-            })
-            return acc;
-        }, []);
-    dispatch({
-        type: SET_PIE_CHART_DATA,
-        payload: eventArr
-    })
-}
+// export const setPieChartData = (viewType, data) => dispatch => {
+//     console.log(viewType)
+//     const newData = data.map((event, i) => ({ eventId: event.eventId, eventType: event.eventType, startDate: moment(event.startDate).day() }))
+//         .reduce((acc, data, i, arr) => {
+//             if (!acc[data.eventType]) {
+//                 console.log('inside IF')
+//                 acc[data.eventType] = 1;
+//             } else {
+//                 console.log('inside ELSE')
+//                 acc[data.eventType] += 1;
+//             }
+//             return acc; 
+//         }, {});
+//     console.log(newData)
+//     const eventArr = Object.entries(newData)
+//         .reduce((acc, node, i, arr) => {
+//             let [type, val] = node;
+//             acc.push({
+//                 name: type,
+//                 value: val
+//             })
+//             return acc;
+//         }, []);
+//     dispatch({
+//         type: SET_PIE_CHART_DATA,
+//         payload: eventArr
+//     })
+// }
 
-export const setPieChartData1 = (viewType, data) => dispatch => {
+export const setPieChartData = (viewType, data) => dispatch => {
     console.log(viewType)
     const newData = data.map((event, i) => ({ eventId: event.eventId, eventType: event.eventType, startDate: moment(event.startDate).day() }))
         .reduce((acc, data, i, arr) => {
