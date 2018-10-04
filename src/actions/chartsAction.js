bb950cf359f036c54ffb45539f0305ea3cf13742
Prabeen04 <prabeen.strange@gmail.com>
2018-10-04 21:44:25 +0530

    return moved to conditions

--- src/actions/chartsAction.js
+++ src/actions/chartsAction.js
@@ -34,21 +34,22 @@ export const setPieChartData = (viewType, data) => dispatch => {
     console.log(viewType)
     const newData = data.map((event, i) => ({ eventId: event.eventId, eventType: event.eventType, startDate: moment(event.startDate).day() }))
         .reduce((acc, data, i, arr) => {
-            console.log(acc.length)
+            console.log(acc)
             if (acc.length && acc[i] && acc[i]['call']) {
                 console.log('inside IF')
                 acc.push({
                     'eventType': data.eventType
                 })
+                return acc;
             } else {
                 console.log('inside ELSE')                
                 // console.log(acc)
                 acc.push({
                     [data.eventType]: 1
                 })
+                return acc;
             }
             console.log(acc)
-            return acc;
         }, []);
 }
 
