0a2c3798c71a59ab8ef82fd98fde628fef252893
Prabeen04 <prabeen.strange@gmail.com>
2018-10-05 23:39:56 +0530

    small stuff

--- src/container/FullCalendar/FullCalendar.js
+++ src/container/FullCalendar/FullCalendar.js
@@ -4,6 +4,13 @@ import { calendar } from 'fullcalendar';
 import './FullCalendar.css';
 
 class FullCalendar extends Component {
+  constructor(props){
+    super(props)
+
+  }
+  componentDidMount(){
+    console.log($)
+  }
   render() {
     return (
       <div className="full-calendar-wrapper">
