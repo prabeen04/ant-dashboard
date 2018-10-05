e996ac31abc39b7b71e458ba8d3302bffdf43314
Prabeen04 <prabeen.strange@gmail.com>
2018-10-05 23:12:09 +0530

    full calendar component created

---
+++ src/container/FullCalendar/FullCalendar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class FullCalendar extends Component {
+  render() {
+    return (
+      <div>
+        <h3>FullCalendar Component</h3>
+      </div>
+    )
+  }
+}
+
+export default FullCalendar;
