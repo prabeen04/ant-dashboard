a2d0abf9acba3b13ececa5abc421eafa088774af
Prabeen04 <prabeen.strange@gmail.com>
2018-10-06 17:12:16 +0530

    fullcalendar rendered

--- src/container/FullCalendar/FullCalendar.js
+++ src/container/FullCalendar/FullCalendar.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
 import $ from 'jquery';
+import 'moment/min/moment.min.js';
 import 'fullcalendar/dist/fullcalendar.css';
-import 'fullcalendar/dist/fullcalendar.print.min.css';
+// import 'fullcalendar/dist/fullcalendar.print.min.css';
 import 'fullcalendar/dist/fullcalendar.js';
 import './FullCalendar.css';
 
 class FullCalendar extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
 
   }
-  componentDidMount(){
+  componentDidMount() {
     const { calendar } = this.refs;
 
-    $(calendar).fullCalendar({events: this.props.events});
+    $(calendar).fullCalendar({
+      events: [
+        {
+          start: '2018-10-06',
+          end: '2018-10-08',
+          // rendering: 'background',
+          color: '#00FF00'
+        },
+      ]
+    });
   }
   render() {
     return (
