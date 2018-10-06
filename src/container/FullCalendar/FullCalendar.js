33bb0edfba2e11ca212edbe685605bba90364b48
Prabeen04 <prabeen.strange@gmail.com>
2018-10-06 17:16:30 +0530

    header options added

--- src/container/FullCalendar/FullCalendar.js
+++ src/container/FullCalendar/FullCalendar.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import $ from 'jquery';
 import 'moment/min/moment.min.js';
-import 'fullcalendar/dist/fullcalendar.css';
 // import 'fullcalendar/dist/fullcalendar.print.min.css';
+import 'fullcalendar/dist/fullcalendar.css';
 import 'fullcalendar/dist/fullcalendar.js';
 import './FullCalendar.css';
 
@@ -15,23 +15,29 @@ class FullCalendar extends Component {
     const { calendar } = this.refs;
 
     $(calendar).fullCalendar({
-      events: [
-        {
-          start: '2018-10-06',
-          end: '2018-10-08',
-          // rendering: 'background',
-          color: '#00FF00'
-        },
-      ]
-    });
+      header: {
+        left: 'title',
+        center: '',
+        right: 'today prev,next'
+      },
+  events: [
+    {
+      start: '2018-10-06',
+      end: '2018-10-08',
+      title: 'Test event',
+      // rendering: 'background',
+      color: '#00FF00'
+    },
+  ]
+});
   }
-  render() {
-    return (
-      <div className="full-calendar-wrapper">
-        <div ref="calendar"></div>
-      </div>
-    )
-  }
+render() {
+  return (
+    <div className="full-calendar-wrapper">
+      <div ref="calendar"></div>
+    </div>
+  )
+}
 }
 
 export default FullCalendar;
