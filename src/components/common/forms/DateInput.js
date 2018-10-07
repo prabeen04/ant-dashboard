b033d56c49ed20082105e9ab5122d25967da10fa
Prabeen04 <prabeen.strange@gmail.com>
2018-10-07 19:59:17 +0530

    datepicker implemented

--- src/components/common/forms/DateInput.js
+++ src/components/common/forms/DateInput.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
+import { DatePicker } from "antd";
+import './form.css';
 
 class DateInput extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <DatePicker className="text-input" {...this.props} />
+            </div>
+        )
+    }
 }
 
 export default DateInput;
