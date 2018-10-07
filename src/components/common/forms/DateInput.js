918c0906b56eae0bd4c72e9176073b9e24b985fc
Prabeen04 <prabeen.strange@gmail.com>
2018-10-07 19:56:40 +0530

    DateInput component created

---
+++ src/components/common/forms/DateInput.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class DateInput extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default DateInput;
