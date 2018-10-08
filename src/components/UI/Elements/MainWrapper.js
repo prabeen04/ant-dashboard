421a9d908ba3ce4aa4c8cbb65b9037d7a62f8899
Prabeen04 <prabeen.strange@gmail.com>
2018-10-08 21:55:28 +0530

    dark/light backgroud prop added

--- src/components/UI/Elements/MainWrapper.js
+++ src/components/UI/Elements/MainWrapper.js
@@ -3,7 +3,13 @@ import styled from 'styled-components';
 const MainWrapper = styled.div`
 border-radius: 0.2rem;
 box-shadow: 0 1px 4px 1px rgb(211, 208, 208);
+${props => props.dark ? `
+background: #444;
+color: white;
+`: `
 background: #fff;
+color: #444;
+`}
 margin: 0.3rem;
 padding: 0.3rem;
 `
