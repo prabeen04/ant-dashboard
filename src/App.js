d89abb5c3a48ff5de3a781cd3dca861dd106edad
Prabeen04 <prabeen.strange@gmail.com>
2018-10-12 20:28:28 +0530

    more theme properties added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import { ThemeProvider } from "styled-components";
 import Navbar from './components/layouts/navbar';
 import './App.css';
 
+const theme = {
+  applicationBackground: '#001529'
+  applicationColor: '#fff',
+  backgroundColor: '#001529',
+  color: '#eeeeee',
+  borderColor: '#001529',
+  boxShadowColor: '#001529',
+  opacity: 0.3
+}
 const App = (props) => {
   return (
-    <ThemeProvider theme={{
-      backgroundColor: '#001529',
-      color: '#eeeeee'
-    }}>
+    <ThemeProvider theme={theme}>
       <Navbar />
     </ThemeProvider>
   );
--- src/components/UI/Elements/MainWrapper.js
+++ src/components/UI/Elements/MainWrapper.js
@@ -5,6 +5,7 @@ border-radius: 0.2rem;
 box-shadow: 0 1px 4px 1px rgb(211, 208, 208);
 background-color: ${props => props.theme.backgroundColor};
 color: ${props => props.theme.color};
+margin: 0.3rem;
 padding: 0.3rem;
 `
 export default MainWrapper;
