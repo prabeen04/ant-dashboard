c367bd86d4d757f988584e9e116609e39f37efd4
Prabeen04 <prabeen.strange@gmail.com>
2018-10-12 19:47:17 +0530

    theme prop injected in MainWrapper

--- src/components/UI/Elements/MainWrapper.js
+++ src/components/UI/Elements/MainWrapper.js
@@ -3,13 +3,8 @@ import styled from 'styled-components';
 const MainWrapper = styled.div`
 border-radius: 0.2rem;
 box-shadow: 0 1px 4px 1px rgb(211, 208, 208);
-${props => props.dark ? `
-background: #001529;
-color: white;
-`: `
-background: #fff;
-color: #444;
-`}
+background-color: ${props => props.theme.backgroundColor};
+background-color: ${props => props.theme.color};
 margin: 0.3rem;
 padding: 0.3rem;
 `
