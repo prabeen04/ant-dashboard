32071321472b1af52a95dc11d1346051597db56b
Prabeen04 <prabeen.strange@gmail.com>
2018-10-12 23:03:35 +0530

    styled Option creted

---
+++ src/components/UI/Elements/Option.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+const Optiom = styled.select`
+    border-radius: 3px;
+    border: 1px solid ${props => props.theme.borderColor};
+    background-color: ${props => props.theme.backgroundColor};
+    color: ${props => props.theme.color};
+    display: block;
+    margin: 0 0 1em;
+    border-radius: 0.3rem;
+    outline: none;
+    box-shadow: 0px 4px 10px -4px  ${props => props.theme.boxShadowColor};
+    padding: 0.3rem 1rem;
+   &:hover{
+    box-shadow: 0px 4px 10px -2px  #777;
+    }
+    ::placeholder {
+      color: #888;
+    }
+   `
+export default Optiom;
