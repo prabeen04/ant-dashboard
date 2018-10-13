6b78d3e45beba45438f21b37209878403e5a7a69
Prabeen04 <prabeen.strange@gmail.com>
2018-10-13 13:22:10 +0530

    App connected to redux

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from "react-redux";
 import { ThemeProvider } from "styled-components";
 import Navbar from './components/layouts/navbar';
 import { ApplicationWrapper } from "./components/UI/Layout";
 import './App.css';
 
-const theme = {
-  // applicationBackground: '#001529',
-  // applicationColor: '#fff',
-  // backgroundColor: 'rgba(121, 140, 173, 0.3)',
-  // color: '#eeeeee',
-  // borderColor: '#001529',
-  // boxShadowColor: '#001529',
-  // opacity: 0.3,
-  applicationBackground: '#eee',
-  applicationColor: '#444',
-  backgroundColor: '#fff',
-  color: '#444',
-  borderColor: '#eee',
-  boxShadowColor: '#aaa',
-  opacity: 1
-}
+// const theme = {
+//   // applicationBackground: '#001529',
+//   // applicationColor: '#fff',
+//   // backgroundColor: 'rgba(121, 140, 173, 0.3)',
+//   // color: '#eeeeee',
+//   // borderColor: '#001529',
+//   // boxShadowColor: '#001529',
+//   // opacity: 0.3,
+//   applicationBackground: '#eee',
+//   applicationColor: '#444',
+//   backgroundColor: '#fff',
+//   color: '#444',
+//   borderColor: '#eee',
+//   boxShadowColor: '#aaa',
+//   opacity: 1
+// }
 const App = (props) => {
+  console.log(props)
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={props.theme}>
         <Navbar />
     </ThemeProvider>
   );
 }
 
-export default App;
+const mapStateToProps = ({ themeReducer }) => ({theme: themeReducer.theme})
+export default connect(mapStateToProps)(App);
--- src/reducers/themeReducer.js
+++ src/reducers/themeReducer.js
@@ -1,5 +1,26 @@
 import { SET_DARK_THEME, SET_LIGHT_THEME } from "../types/themeActionTypes";
 
-export const themeReducer = (state, action) => {
+const initialState = {
+    themeType: 'light',
+    theme: {
+        applicationBackground: '#eee',
+        applicationColor: '#444',
+        backgroundColor: '#fff',
+        color: '#444',
+        borderColor: '#eee',
+        boxShadowColor: '#aaa',
+        opacity: 1
+    }
+}
+
+export const themeReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case SET_LIGHT_THEME:
+            return { ...state, theme: ...action.payload }
+        case SET_DARK_THEME:
+            return { ...state, theme: ...action.payload }
+        default:
+            return state;
+    }
     return state;
 }
