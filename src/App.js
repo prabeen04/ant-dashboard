bccafc47a166ee06bc0c114a28da0616a1d5b9c4
Prabeen04 <prabeen.strange@gmail.com>
2018-10-13 12:58:09 +0530

    light/dark styles separated

--- src/App.js
+++ src/App.js
@@ -5,22 +5,25 @@ import { ApplicationWrapper } from "./components/UI/Layout";
 import './App.css';
 
 const theme = {
-  applicationBackground: '#001529',
-  applicationColor: '#fff',
-  // backgroundColor: '#fff',
-  backgroundColor: 'rgba(121, 140, 173, 0.3)',
-  color: '#eeeeee',
-  // color: '#444',
-  borderColor: '#001529',
-  boxShadowColor: '#001529',
+  // applicationBackground: '#001529',
+  // applicationColor: '#fff',
+  // backgroundColor: 'rgba(121, 140, 173, 0.3)',
+  // color: '#eeeeee',
+  // borderColor: '#001529',
+  // boxShadowColor: '#001529',
+  // opacity: 0.3,
+  applicationBackground: '#eee',
+  applicationColor: '#444',
+  backgroundColor: '#fff',
+  color: '#444',
+  borderColor: '#eee',
+  boxShadowColor: '#aaa',
   opacity: 0.3
 }
 const App = (props) => {
   return (
     <ThemeProvider theme={theme}>
-      {/* <ApplicationWrapper> */}
         <Navbar />
-      {/* </ApplicationWrapper> */}
     </ThemeProvider>
   );
 }
