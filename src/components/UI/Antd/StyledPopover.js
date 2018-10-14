8eff68fdd5509307d3dffb4b5d02fcfa8f4ab70b
Prabeen04 <prabeen.strange@gmail.com>
2018-10-14 21:32:28 +0530

    small stuff

--- src/components/UI/Antd/StyledPopover.js
+++ src/components/UI/Antd/StyledPopover.js
@@ -2,13 +2,15 @@ import { Popover } from 'antd';
 import styled from 'styled-components';
 
 const StyledPopover = styled(Popover)`
-    .ant-popover-inner{
+    .ant-popover-inner {
         color: ${props => props.theme.color};
         background-color: ${props => props.theme.applicationBackground};
         box-shadow: 0 1px 4px 1px ${props => props.theme.boxShadowColor};
         border: 3px solid red;
     }  
     .ant-popover-inner-content {
+        color: ${props => props.theme.color};
+        background-color: ${props => props.theme.applicationBackground};
         border: 3px solid red;
     }  
     .ant-popover {
