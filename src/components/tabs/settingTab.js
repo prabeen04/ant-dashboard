d2b5e942400e80cd524cacf415a3388433d208c6
Prabeen04 <prabeen.strange@gmail.com>
2018-10-14 20:37:44 +0530

    styled tab removed

--- src/components/tabs/settingTab.js
+++ src/components/tabs/settingTab.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react'
 import { Tabs, Icon, Button, List, Checkbox } from 'antd';
-import { TabsWrapper } from "../UI/Layout";
-import { StyledTabs } from "../UI/Antd";
-
 const TabPane = Tabs.TabPane;
 const data = [
     <Checkbox>Manchester United</Checkbox>,
@@ -33,47 +30,45 @@ class SettingTab extends Component {
     render() {
         return (
             <div>
-                <TabsWrapper>
-                    <StyledTabs defaultActiveKey="1" style={{ width: 400 }}>
-                        <TabPane tab={<span><Icon type="team" />Team</span>} key="1">
-                            <List
-                                size="small"
-                                bordered
-                                header={<div><h4> Team</h4></div>}
-                                footer={<div style={{ textAlign: 'right' }}> <Button
-                                    type="primary"
-                                    onClick={this.props.hide}>Close</Button></div>}
-                                dataSource={data}
-                                renderItem={item => (<List.Item>{item}</List.Item>)}
-                            />
+                <Tabs defaultActiveKey="1" style={{width: 400}}>
+                    <TabPane tab={<span><Icon type="team" />Team</span>} key="1">
+                        <List
+                            size="small"
+                            bordered
+                            header={<div><h4> Team</h4></div>}
+                            footer={<div style={{ textAlign: 'right' }}> <Button
+                                type="primary"
+                                onClick={this.props.hide}>Close</Button></div>}
+                            dataSource={data}
+                            renderItem={item => (<List.Item>{item}</List.Item>)}
+                        />
 
-                        </TabPane>
-                        <TabPane tab={<span><Icon type="trophy" />Trophy</span>} key="2">
-                            <List
-                                size="small"
-                                bordered
-                                header={<div><h4>Filter by Trophies</h4></div>}
-                                footer={<div style={{ textAlign: 'right' }}> <Button
-                                    type="primary"
-                                    onClick={this.props.hide}>Close</Button></div>}
-                                dataSource={data2}
-                                renderItem={item => (<List.Item>{item}</List.Item>)}
-                            />
-                        </TabPane>
-                        <TabPane tab={<span><Icon type="line-chart" />Statistics</span>} key="3">
-                            <List
-                                size="small"
-                                bordered
-                                header={<div><h4>Filter by Stats</h4></div>}
-                                footer={<div style={{ textAlign: 'right' }}> <Button
-                                    type="primary"
-                                    onClick={this.props.hide}>Close</Button></div>}
-                                dataSource={data3}
-                                renderItem={item => (<List.Item>{item}</List.Item>)}
-                            />
-                        </TabPane>
-                    </StyledTabs>
-                </TabsWrapper>
+                    </TabPane>
+                    <TabPane tab={<span><Icon type="trophy" />Trophy</span>} key="2">
+                        <List
+                            size="small"
+                            bordered
+                            header={<div><h4>Filter by Trophies</h4></div>}
+                            footer={<div style={{ textAlign: 'right' }}> <Button
+                                type="primary"
+                                onClick={this.props.hide}>Close</Button></div>}
+                            dataSource={data2}
+                            renderItem={item => (<List.Item>{item}</List.Item>)}
+                        />
+                    </TabPane>
+                    <TabPane tab={<span><Icon type="line-chart" />Statistics</span>} key="3">
+                        <List
+                            size="small"
+                            bordered
+                            header={<div><h4>Filter by Stats</h4></div>}
+                            footer={<div style={{ textAlign: 'right' }}> <Button
+                                type="primary"
+                                onClick={this.props.hide}>Close</Button></div>}
+                            dataSource={data3}
+                            renderItem={item => (<List.Item>{item}</List.Item>)}
+                        />
+                    </TabPane>
+                </Tabs>
             </div>
         )
     }
