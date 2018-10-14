dafb1ceb1c73b863456feea8240eac3637041211
Prabeen04 <prabeen.strange@gmail.com>
2018-10-14 21:16:10 +0530

    more popover styles added

--- src/components/UI/Antd/StyledPopover.js
+++ src/components/UI/Antd/StyledPopover.js
@@ -6,7 +6,13 @@ const StyledPopover = styled(Popover)`
         color: ${props => props.theme.color};
         background-color: ${props => props.theme.applicationBackground};
         box-shadow: 0 1px 4px 1px ${props => props.theme.boxShadowColor};
-        border: 1px solid ${props => props.theme.borderColor}
-    }    
+        border: 3px solid red;
+    }  
+    .ant-popover-inner-content {
+        border: 3px solid red;
+    }  
+    .ant-popover {
+        border: 3px solid red;
+    }  
 `
 export default StyledPopover;
