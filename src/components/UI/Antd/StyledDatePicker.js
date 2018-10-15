dfe0facb55a4bb5808ded24b7d9c8232a83df88c
Prabeen04 <prabeen.strange@gmail.com>
2018-10-15 21:47:56 +0530

    more date picker styles added

--- src/components/UI/Antd/StyledDatePicker.js
+++ src/components/UI/Antd/StyledDatePicker.js
@@ -2,7 +2,15 @@ import { DatePicker } from "antd";
 import styled from 'styled-components'
 
 const StyledDatePicker = styled(DatePicker)`
-    color: ${props => props.theme.color};
+    border-radius: 3px;
+    border: 1px solid ${props => props.theme.borderColor};
     background-color: ${props => props.theme.backgroundColor};
+    color: ${props => props.theme.color};
+    display: block;
+    margin: 0.5rem;
+    border-radius: 0.3rem;
+    outline: none;
+    box-shadow: 0px 4px 10px -4px  ${props => props.theme.boxShadowColor};
+    padding: 0.3rem 1rem;
 `
 export default StyledDatePicker;
