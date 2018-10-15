d02b1a2aa8f83d6656570f515b8f1ecf159cdc38
Prabeen04 <prabeen.strange@gmail.com>
2018-10-15 22:49:04 +0530

    styled ReactSelect created

---
+++ src/components/UI/Elements/StyledReactSelect.js
@@ -0,0 +1,16 @@
+import Select from 'react-select'
+import styled from 'styled-components'
+
+const StyledReactSelect = styled(Select)`
+    border-radius: 3px;
+    border: 1px solid ${props => props.theme.borderColor};
+    background-color: ${props => props.theme.backgroundColor};
+    color: ${props => props.theme.color};
+    display: block;
+    margin: 0.2rem 0.5rem;
+    border-radius: 0.3rem;
+    outline: none;
+    box-shadow: 0px 4px 10px -4px  ${props => props.theme.boxShadowColor};
+    padding: 0.3rem 1rem;
+`
+export default StyledReactSelect;
