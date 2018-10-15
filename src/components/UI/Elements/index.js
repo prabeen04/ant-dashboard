1a1074fc5ddb86c5241860024d949a8064163a95
Prabeen04 <prabeen.strange@gmail.com>
2018-10-15 22:50:08 +0530

    StyledReactSelect exported

--- src/components/UI/Elements/index.js
+++ src/components/UI/Elements/index.js
@@ -3,3 +3,4 @@ export { default as Title } from "./Title";
 export { default as Input } from "./Input";
 export { default as SelectInput } from "./SelectInput";
 export { default as Option } from "./Option";
+export { default as StyledReactSelect } from "./StyledReactSelect";
