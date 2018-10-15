e449a851590e05ed47c6d480357cebf0f59a3c96
Prabeen04 <prabeen.strange@gmail.com>
2018-10-15 23:02:31 +0530

    more styling stuff

--- src/components/UI/Elements/StyledReactSelect.js
+++ src/components/UI/Elements/StyledReactSelect.js
@@ -7,11 +7,31 @@ const StyledReactSelect = styled(ReactSelect )`
     .Select-value {
         display: inline-flex;
         align-items: center;
+        border-radius: 3px;
+        border: 1px solid ${props => props.theme.borderColor};
+        background-color: ${props => props.theme.backgroundColor};
+        color: ${props => props.theme.color};
+        display: block;
+        margin: 0.2rem 0.5rem;
+        border-radius: 0.3rem;
+        outline: none;
+        box-shadow: 0px 4px 10px -4px  ${props => props.theme.boxShadowColor};
+        padding: 0.3rem 1rem;
     }		
 }
 
 & .Select-placeholder {
     font-size: smaller;
+    border-radius: 3px;
+    border: 1px solid ${props => props.theme.borderColor};
+    background-color: ${props => props.theme.backgroundColor};
+    color: ${props => props.theme.color};
+    display: block;
+    margin: 0.2rem 0.5rem;
+    border-radius: 0.3rem;
+    outline: none;
+    box-shadow: 0px 4px 10px -4px  ${props => props.theme.boxShadowColor};
+    padding: 0.3rem 1rem;
 }
 input{
     border-radius: 3px;
