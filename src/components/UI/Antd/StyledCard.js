f4befd28736e58585d79b48e37fc1685de224c84
Prabeen04 <prabeen.strange@gmail.com>
2018-10-16 22:57:55 +0530

    antd card action theme property

--- src/components/UI/Antd/StyledCard.js
+++ src/components/UI/Antd/StyledCard.js
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 const StyledCard = styled(Card)`
     background-color: ${props => props.theme.applicationBackground} !important;
     color: ${props => props.theme.color};
+    box-shadow: 0px 4px 10px -4px  ${props => props.theme.boxShadowColor};
     border: none !important;
     .ant-card {
         background-color: ${props => props.theme.applicationBackground} !important;
@@ -17,6 +18,10 @@ const StyledCard = styled(Card)`
         background-color: ${props => props.theme.backgroundColor};
         color: ${props => props.theme.color};
         border: none !important;
+    }
+    .ant-card-actions {
+        background-color: ${props => props.theme.backgroundColor};
+        color: ${props => props.theme.color};
     }
 `
 export default StyledCard;
