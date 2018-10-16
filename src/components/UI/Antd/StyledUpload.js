fb7f448193f78db1ba03937b46c14381c45475dd
Prabeen04 <prabeen.strange@gmail.com>
2018-10-16 20:43:15 +0530

    StyledUpload created

---
+++ src/components/UI/Antd/StyledUpload.js
@@ -0,0 +1,16 @@
+import { Upload } from "antd";
+import styled from 'styled-components'
+
+const StyledUpload = styled(Upload)`
+    border-radius: 3px;
+    border: 1px solid ${props => props.theme.borderColor};
+    background-color: ${props => props.theme.backgroundColor};
+    color: ${props => props.theme.color};
+    display: block;
+    margin: 0.2rem 0.5rem;
+    border-radius: 0.3rem;
+    outline: none;
+    box-shadow: 0px 4px 10px -4px  ${props => props.theme.boxShadowColor};
+    padding: 0.3rem 1rem;
+`
+export default StyledUpload;
