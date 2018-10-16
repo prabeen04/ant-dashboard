b7cbcc0b41f41ad6479ffaad5d0b31df5932d53c
Prabeen04 <prabeen.strange@gmail.com>
2018-10-16 20:53:19 +0530

    StyledInput added in addProfile

--- src/container/profile/addProfile.js
+++ src/container/profile/addProfile.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { Field, reduxForm } from 'redux-form'
-import { Input, Button, Icon, Card,  message } from 'antd';
+import { Button, Icon, Card,  message } from 'antd';
 import { StyledUpload } from "../../components/UI/Antd";
+import { Input } from "../../components/UI/Elements";
 import './profile.css'
 import { addProfile } from '../../actions/profile_actions';
 
