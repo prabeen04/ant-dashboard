ddda22750645ca3983eefb3337d02cadcecc8c86
Prabeen04 <prabeen.strange@gmail.com>
2018-10-16 20:46:54 +0530

    ant-upload class override

--- src/components/UI/Antd/StyledUpload.js
+++ src/components/UI/Antd/StyledUpload.js
@@ -2,11 +2,26 @@ import { Upload } from "antd";
 import styled from 'styled-components'
 
 const StyledUpload = styled(Upload)`
+.ant-upload.ant-upload-select-picture-card {
+    border: 1px dashed #d9d9d9;
+    width: 104px;
+    height: 104px;
+    border-radius: 4px;
+    background-color: #fafafa; 
+    text-align: center;
+    cursor: pointer;
+    -webkit-transition: border-color 0.3s ease;
+    -o-transition: border-color 0.3s ease;
+    transition: border-color 0.3s ease;
+    vertical-align: top;
+    margin-right: 8px;
+    margin-bottom: 8px;
+    display: table;
+}
     border-radius: 3px;
     border: 1px solid ${props => props.theme.borderColor};
     background-color: ${props => props.theme.backgroundColor};
     color: ${props => props.theme.color};
-    display: block;
     margin: 0.2rem 0.5rem;
     border-radius: 0.3rem;
     outline: none;
