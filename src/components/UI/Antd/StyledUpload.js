54ab7bb9bb0074cd564c7e6ab5ec9122ea1de091
Prabeen04 <prabeen.strange@gmail.com>
2018-10-16 20:47:54 +0530

    styled refactored

--- src/components/UI/Antd/StyledUpload.js
+++ src/components/UI/Antd/StyledUpload.js
@@ -3,11 +3,12 @@ import styled from 'styled-components'
 
 const StyledUpload = styled(Upload)`
 .ant-upload.ant-upload-select-picture-card {
-    border: 1px dashed #d9d9d9;
+    border: 1px dashed ${props => props.theme.borderColor};
     width: 104px;
     height: 104px;
     border-radius: 4px;
-    background-color: #fafafa; 
+    background-color: ${props => props.theme.backgroundColor};
+    color: ${props => props.theme.color};
     text-align: center;
     cursor: pointer;
     -webkit-transition: border-color 0.3s ease;
@@ -18,14 +19,5 @@ const StyledUpload = styled(Upload)`
     margin-bottom: 8px;
     display: table;
 }
-    border-radius: 3px;
-    border: 1px solid ${props => props.theme.borderColor};
-    background-color: ${props => props.theme.backgroundColor};
-    color: ${props => props.theme.color};
-    margin: 0.2rem 0.5rem;
-    border-radius: 0.3rem;
-    outline: none;
-    box-shadow: 0px 4px 10px -4px  ${props => props.theme.boxShadowColor};
-    padding: 0.3rem 1rem;
 `
 export default StyledUpload;
