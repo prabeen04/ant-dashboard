d480cf53cfb41c18b6289bafb11eb5d869a906fe
Prabeen04 <prabeen.strange@gmail.com>
2018-10-16 21:14:32 +0530

    ant card bordered stuff

--- src/components/UI/Antd/StyledCard.js
+++ src/components/UI/Antd/StyledCard.js
@@ -2,14 +2,21 @@ import { Card } from "antd";
 import styled from 'styled-components'
 
 const StyledCard = styled(Card)`
-.ant-card {
     background-color: ${props => props.theme.applicationBackground} !important;
     color: ${props => props.theme.color};
-}
-&& .ant-card-body {
-    background-color: ${props => props.theme.backgroundColor};
-    color: ${props => props.theme.color};
-    border: none;
-}
+    border: none !important;
+    .ant-card {
+        background-color: ${props => props.theme.applicationBackground} !important;
+        color: ${props => props.theme.color};
+        border: none !important;
+    }
+    .ant-card-bordered {
+        border: none !important;
+    }
+    .ant-card-body {
+        background-color: ${props => props.theme.backgroundColor};
+        color: ${props => props.theme.color};
+        border: none !important;
+    }
 `
 export default StyledCard;
