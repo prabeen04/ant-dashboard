6c7241207655ed25b02fb00cc073909e5d99e8d4
Prabeen04 <prabeen.strange@gmail.com>
2018-10-17 20:29:45 +0530

    styles in drawer

--- src/components/UI/Antd/StyledDrawer.js
+++ src/components/UI/Antd/StyledDrawer.js
@@ -6,25 +6,10 @@ const StyledDrawer = styled(Drawer)`
     color: ${props => props.theme.color};
     box-shadow: 0px 4px 10px -4px  ${props => props.theme.boxShadowColor};
     border: none !important;
-    ..ant-drawer-content-wrapper {
+    .ant-drawer-content-wrapper {
         background-color: ${props => props.theme.applicationBackground} !important;
         color: ${props => props.theme.color};
         border: none !important;
-    }
-    .ant-card-bordered {
-        border: none !important;
-    }
-    .ant-card-body {
-        background-color: ${props => props.theme.backgroundColor};
-        color: ${props => props.theme.color};
-        border: none !important;
-    }
-    .ant-card-actions {
-        background-color: ${props => props.theme.backgroundColor};
-        color: ${props => props.theme.color};
-    }
-    .anticon{
-        color: ${props => props.theme.color};
     }
 `
 export default StyledDrawer;
