c6007e8c7c731a36fa23856ac05d518f0c32bf09
Prabeen04 <prabeen.strange@gmail.com>
2018-10-17 20:36:18 +0530

    Drawer className override

--- src/components/UI/Antd/StyledDrawer.js
+++ src/components/UI/Antd/StyledDrawer.js
@@ -2,11 +2,12 @@ import { Drawer } from "antd";
 import styled from 'styled-components'
 
 const StyledDrawer = styled(Drawer)`
-    background-color: ${props => props.theme.applicationBackground} !important;
-    color: ${props => props.theme.color};
-    box-shadow: 0px 4px 10px -4px  ${props => props.theme.boxShadowColor};
-    border: none !important;
     .ant-drawer-content-wrapper {
+        background-color: ${props => props.theme.applicationBackground} !important;
+        color: ${props => props.theme.color};
+        border: none !important;
+    }
+    .ant-drawer-body {
         background-color: ${props => props.theme.applicationBackground} !important;
         color: ${props => props.theme.color};
         border: none !important;
