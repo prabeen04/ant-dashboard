e6ca32e4140fb28c8b67d66b25bbdb8bd6a28fba
Prabeen04 <prabeen.strange@gmail.com>
2018-10-17 20:26:45 +0530

    StyledDrawer component created

---
+++ src/components/UI/Antd/StyledDrawer.js
@@ -0,0 +1,30 @@
+import { Drawer } from "antd";
+import styled from 'styled-components'
+
+const StyledDrawer = styled(Drawer)`
+    background-color: ${props => props.theme.applicationBackground} !important;
+    color: ${props => props.theme.color};
+    box-shadow: 0px 4px 10px -4px  ${props => props.theme.boxShadowColor};
+    border: none !important;
+    .ant-card {
+        background-color: ${props => props.theme.applicationBackground} !important;
+        color: ${props => props.theme.color};
+        border: none !important;
+    }
+    .ant-card-bordered {
+        border: none !important;
+    }
+    .ant-card-body {
+        background-color: ${props => props.theme.backgroundColor};
+        color: ${props => props.theme.color};
+        border: none !important;
+    }
+    .ant-card-actions {
+        background-color: ${props => props.theme.backgroundColor};
+        color: ${props => props.theme.color};
+    }
+    .anticon{
+        color: ${props => props.theme.color};
+    }
+`
+export default StyledDrawer;
--- src/components/UI/Antd/index.js
+++ src/components/UI/Antd/index.js
@@ -1,4 +1,5 @@
 export { default as StyledTabs } from "./StyledTabs";
+export { default as StyledDrawer } from "./StyledDrawer";
 export { default as StyledCard } from "./StyledCard";
 export { default as StyledPopover } from "./StyledPopover";
 export { default as StyledSkeleton } from "./StyledSkeleton";
