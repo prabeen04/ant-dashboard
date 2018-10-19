50259987f8cefad2e37f0e6b156b2e9232013513
Prabeen04 <prabeen.strange@gmail.com>
2018-10-19 19:00:47 +0530

    StyledTimeInterval added

--- src/components/utils/TimeInterval.js
+++ src/components/utils/TimeInterval.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
+const StyledTimeinterval = styled.div`
+    display: flex;
+    flex-direction: row;
+    color: ${props => props.theme.color};
+    background-color: ${props => props.theme.backgroundColor};
+    font-size: 20px;
+`
 class TimeInterval extends Component {
     render() {
         const { times, handleClick } = this.props;
         return (
-            <div>
+            <StyledTimeinterval>
                 {times.map((time, i) => {
                     return (
                         <span 
@@ -15,7 +23,7 @@ class TimeInterval extends Component {
                         </span>
                     )
                 })}
-            </div>
+            </StyledTimeinterval>
         )
     }
 }
