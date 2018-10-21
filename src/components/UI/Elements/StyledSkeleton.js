683ad2cc4edee0084db4cec3e920f032f3827e17
Prabeen04 <prabeen.strange@gmail.com>
2018-10-21 17:32:24 +0530

    Skeleton styles added

--- src/components/UI/Elements/StyledSkeleton.js
+++ src/components/UI/Elements/StyledSkeleton.js
@@ -2,8 +2,8 @@ import ContentLoader{ Facebook } from "react-content-loader";
 import styled from "styled-components";
 
 const StyledSkeleton  = styled(ContentLoader)`
-
-
+    background-color: ${props => props.theme.backgroundColor};
+    color: ${props => props.theme.color};
 ` 
 
 export default StyledSkeleton;
