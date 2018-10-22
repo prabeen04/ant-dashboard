35094db83269a1841bc38862654c330eee5a6f59
Prabeen04 <prabeen.strange@gmail.com>
2018-10-22 19:57:09 +0530

    MixedBarChart component created

---
+++ src/container/charts/MixedBarChart.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class MixedBarChart extends Component {
+  render() {
+    return (
+      <div>
+        <h3>MixedBarChart Component</h3>
+      </div>
+    )
+  }
+}
+
+export default MixedBarChart;
