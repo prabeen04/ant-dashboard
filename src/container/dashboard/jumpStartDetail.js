65f5d2a1b872a40646f7fa4574c7912e53474c1d
Prabeen04 <prabeen.strange@gmail.com>
2018-10-23 23:13:32 +0530

    condition caret icon added

--- src/container/dashboard/jumpStartDetail.js
+++ src/container/dashboard/jumpStartDetail.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from "redux";
-import  classNames  from 'classnames';
+import classNames from 'classnames';
 import { openJumpstartModal } from "../../actions/dashboardAction";
-import {Icon } from 'antd'
+import { Icon } from 'antd'
 const JumpStartDetail = (props) => {
     return (
         <div className={'jumpstart-box'}
             onClick={() => props.openJumpstartModal(props.data)}
-            >
+        >
             <div>
                 <h3 style={{ color: '#aaa' }}>{props.data.title}</h3>
                 <h2 style={{ color: '#eee' }}>{props.data.value}
-                    <span className={props.jumpstartStyle}><Icon type='user'/>{props.data.progress}</span>
+                    <span className={props.jumpstartStyle}>
+                        {this.props.progress > 0 ? <Icon type='caret-up' /> : <Icon type='caret-down' />}
+                        {props.data.progress}</span>
                 </h2>
             </div>
         </div>
