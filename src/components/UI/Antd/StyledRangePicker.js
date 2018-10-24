00c7c7e22cf1338a45536414dd766343fdfaff41
Prabeen04 <prabeen.strange@gmail.com>
2018-10-24 23:24:01 +0530

    small stuff

--- src/components/UI/Antd/StyledRangePicker.js
+++ src/components/UI/Antd/StyledRangePicker.js
@@ -11,7 +11,6 @@ const StyledRangePicker = styled(DatePicker.RangePicker)`
     margin: 0.2rem 0;
     border-radius: 0.3rem;
     outline: none;
-    box-shadow: 0px 4px 10px -4px  ${props => props.theme.boxShadowColor};
     padding: 0.3rem 1rem;
 }
 `
