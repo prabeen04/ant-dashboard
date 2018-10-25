02bfa800c354b70744d2130b4e2cf74d93fa99bd
Prabeen04 <prabeen.strange@gmail.com>
2018-10-25 20:37:20 +0530

    DropdownMenu component created

---
+++ src/components/layouts/DropdownMenu.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class DropdownMenu extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default DropdownMenu;
