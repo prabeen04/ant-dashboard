a1c92bfd3a3543f7d0aa3bd4c77635a1c0f824ae
Prabeen04 <prabeen.strange@gmail.com>
2018-10-25 20:40:12 +0530

    Dropdown menus created

--- src/components/layouts/DropdownMenu.js
+++ src/components/layouts/DropdownMenu.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react'
-
+import { Menu, Icon } from "antd";
+import { StyledDropdown } from "../UI/Antd";
+const menu = (
+    <Menu>
+        <Menu.Item key="0">
+            <a href="http://www.alipay.com/">1st menu item</a>
+        </Menu.Item>
+        <Menu.Item key="1">
+            <a href="http://www.taobao.com/">2nd menu item</a>
+        </Menu.Item>
+        <Menu.Divider />
+        <Menu.Item key="3">3rd menu item</Menu.Item>
+    </Menu>
+);
 class DropdownMenu extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+    render() {
+        return (
+            <StyledDropdown>
+
+            </StyledDropdown>
+        )
+    }
 }
 
 export default DropdownMenu;
