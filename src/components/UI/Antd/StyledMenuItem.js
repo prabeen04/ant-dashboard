f5febd21f62421790db212c6b487c0d0947e243c
Prabeen04 <prabeen.strange@gmail.com>
2018-10-25 22:00:13 +0530

    styles removed

--- src/components/UI/Antd/StyledMenuItem.js
+++ src/components/UI/Antd/StyledMenuItem.js
@@ -6,10 +6,5 @@ const StyledMenu = styled(Menu.Item)`
         background-color: ${props => props.theme.backgroundColor};
         color: ${props => props.theme.color};
     }
-    .ant-menu>a{
-        background-color: ${props => props.theme.backgroundColor};
-        color: ${props => props.theme.color};
-    }
-    
 `
 export default StyledMenu;
