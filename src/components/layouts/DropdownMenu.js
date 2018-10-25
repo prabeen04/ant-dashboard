781e2b38aae0f829cf8e8a3d8f57f4300d0911e0
Prabeen04 <prabeen.strange@gmail.com>
2018-10-25 21:14:44 +0530

    MenuItems styles added

--- src/components/layouts/DropdownMenu.js
+++ src/components/layouts/DropdownMenu.js
@@ -1,24 +1,27 @@
 import React, { Component } from 'react'
 import { Menu, Icon, Avatar } from "antd";
 import { StyledDropdown } from "../UI/Antd";
+import { MainWrapper } from "../UI/Layout";
 import Theme from '../../container/settings/Theme/Theme';
 const menu = (
-    <Menu>
-        <Menu.Item key="0">
-            <a href="http://www.alipay.com/">1st menu item</a>
-        </Menu.Item>
-        <Menu.Item key="1">
-            <a href="http://www.taobao.com/">2nd menu item</a>
-        </Menu.Item>
-        <Menu.Divider />
-        <Menu.Item key="3"><Theme/></Menu.Item>
-    </Menu>
+    <MainWrapper>
+        <Menu>
+            <Menu.Item key="0">
+                <a href="http://www.alipay.com/">Profile</a>
+            </Menu.Item>
+            <Menu.Item key="1">
+                <a href="http://www.taobao.com/">Settings</a>
+            </Menu.Item>
+            <Menu.Divider />
+        </Menu>
+        <Theme />
+    </MainWrapper>
 );
 class DropdownMenu extends Component {
     render() {
         return (
             <StyledDropdown overlay={menu}>
-                <Avatar style={{ backgroundColor: 'red', verticalAlign: 'middle' }} size="medium">
+                <Avatar style={{ backgroundColor: '#1890ff', verticalAlign: 'middle' }} size="medium">
                     {'P'}
                 </Avatar>
             </StyledDropdown>
