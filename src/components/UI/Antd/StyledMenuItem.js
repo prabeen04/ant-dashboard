89afd4b86c91c395355bc43879413dce0fe6d09a
Prabeen04 <prabeen.strange@gmail.com>
2018-10-25 22:05:41 +0530

    small stuff

--- src/components/UI/Antd/StyledMenuItem.js
+++ src/components/UI/Antd/StyledMenuItem.js
@@ -6,5 +6,10 @@ const StyledMenu = styled(Menu.Item)`
         background-color: ${props => props.theme.backgroundColor};
         color: ${props => props.theme.color};
     }
+    .ant-menu>a:before {
+        background-color: ${props => props.theme.backgroundColor};
+        color: ${props => props.theme.color};
+    }
+    
 `
 export default StyledMenu;
