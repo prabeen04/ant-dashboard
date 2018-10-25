a34e6e291522f35acfaa97ed8086b0a45a88319e
Prabeen04 <prabeen.strange@gmail.com>
2018-10-25 21:37:30 +0530

    AplicationWrapper added

--- src/components/layouts/DropdownMenu.js
+++ src/components/layouts/DropdownMenu.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react'
 import { Menu, Icon, Avatar } from "antd";
-import { StyledDropdown } from "../UI/Antd";
-import { MainWrapper } from "../UI/Layout";
+import { StyledDropdown, StyledMenu } from "../UI/Antd";
+import { ApplicationWrapper, MainWrapper } from "../UI/Layout";
 import Theme from '../../container/settings/Theme/Theme';
 const menu = (
-    <MainWrapper>
+    <ApplicationWrapper>
+        <MainWrapper>
         <Menu>
             <Menu.Item key="0">
-                <a href="http://www.alipay.com/">Profile</a>
+                <a href="#">Profile</a>
             </Menu.Item>
             <Menu.Item key="1">
-                <a href="http://www.taobao.com/">Settings</a>
+                <a href="#">Settings</a>
             </Menu.Item>
             <Menu.Divider />
         </Menu>
         <Theme />
-    </MainWrapper>
+        </MainWrapper>
+    </ApplicationWrapper>
 );
 class DropdownMenu extends Component {
     render() {
