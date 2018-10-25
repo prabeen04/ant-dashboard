371617985e75a752a73088eb0be34f738a67199f
Prabeen04 <prabeen.strange@gmail.com>
2018-10-25 21:53:43 +0530

    styled menu styles changes according to classnamed

--- src/components/UI/Antd/StyledMenu.js
+++ src/components/UI/Antd/StyledMenu.js
@@ -2,7 +2,9 @@ import { Menu } from "antd";
 import styled from 'styled-components'
 
 const StyledMenu = styled(Menu)`
-    background-color: ${props => props.theme.backgroundColor};
-    color: ${props => props.theme.color};
+    .ant-menu{
+        background-color: ${props => props.theme.backgroundColor};
+        color: ${props => props.theme.color};
+    }
 `
 export default StyledMenu;
--- src/components/UI/Antd/StyledMenuItem.js
+++ src/components/UI/Antd/StyledMenuItem.js
@@ -2,7 +2,9 @@ import { Menu } from "antd";
 import styled from 'styled-components'
 
 const StyledMenu = styled(Menu.Item)`
-    background-color: ${props => props.theme.backgroundColor};
-    color: ${props => props.theme.color};
+    .ant-menu-item{
+        background-color: ${props => props.theme.backgroundColor};
+        color: ${props => props.theme.color};
+    }
 `
 export default StyledMenu;
