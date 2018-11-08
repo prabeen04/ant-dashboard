b4ec036eec11f0549faf09cfa491f66690202867
Prabeen04 <prabeen.strange@gmail.com>
2018-11-08 15:25:16 +0530

    styled ReactTable added

--- src/components/utils/ReactTable.js
+++ src/components/utils/ReactTable.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
+import ReactTable from "react-table";
+const StyledReactTable = styled(ReactTable)`
+
+`
 
 class ReactTable extends React.Component {
     render() {
-        return (
-
-        );
+        return <StyledReactTable/>
     }
 }
+
+export default ReactTable;
