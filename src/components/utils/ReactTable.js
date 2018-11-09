941adb47caf94feca8aa170ec8c0f0db06a89e2b
Prabeen04 <prabeen.strange@gmail.com>
2018-11-09 20:53:29 +0530

    box-shadow added in layout wrapper

--- src/components/utils/ReactTable.js
+++ src/components/utils/ReactTable.js
@@ -6,9 +6,11 @@ import 'react-table/react-table.css';
 const StyledReactTable = styled(ReactTable)`
     background-color: ${props => props.theme.backgroundColor};
     color: ${props => props.theme.color};
+    box-shadow: 0 1px 4px 1px ${props => props.theme.boxShadowColor};
     .ReactTable{
         background-color: ${props => props.theme.backgroundColor};
         color: ${props => props.theme.color};       
+        box-shadow: 0 1px 4px 1px ${props => props.theme.boxShadowColor};
     }    
 `
 const AppReactTable = (props) => <StyledReactTable {...props} />
