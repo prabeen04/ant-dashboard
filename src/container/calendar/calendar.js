bd4c6d8c289b839c363a086038366860e0b09384
Prabeen04 <prabeen.strange@gmail.com>
2018-11-09 21:13:38 +0530

    calendar dead code removed

--- src/container/calendar/calendar.js
+++ src/container/calendar/calendar.js
@@ -2,20 +2,14 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import CalendarInstance from './calendarInstance';
-import { getEvents, addEvent, setDate } from '../../actions/calendar_actions';
+import { getEvents, setDate } from '../../actions/calendar_actions';
 import EventForm from './eventForm';
-import EventList from './eventList';
 import ActionHeader from '../../components/layouts/ActionHeader';
 import './calendar.css';
 import { Button, Icon, message, Tabs, Tooltip } from 'antd';
 import { MainWrapper } from '../../components/UI/Layout';
 import { StyledTabs } from '../../components/UI/Antd';
-import { StyledSkeleton, StyledCodeSkeleton } from "../../components/UI/Elements";
-import { Code } from "react-content-loader";
 const TabPane = Tabs.TabPane;
-const dateFormat = 'YYYY-MM-DD';
-const timeFormat = 'HH:mm:ss';
-
 
 class Calendar extends Component {
   constructor(props) {
@@ -46,7 +40,6 @@ class Calendar extends Component {
   render() {
     if (this.props.isLoading) {
       return (<div className="flex-container" style={{ height: '80vh', justifyContent: 'center', alignItems: 'center' }}>
-        {/* <StyledCodeSkeleton/> */}
         <Icon type="loading" style={{ fontSize: 60, color: 'tomato' }} spin />
       </div>)
     }
