a763e96ab3a2c992b5cd30707e195c4919f97675
Prabeen04 <prabeen.strange@gmail.com>
2019-01-05 21:51:03 +0530

    funnelChart test file created

---
+++ src/components/charts/funnelChart.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import FunnelChart from './funnelChart';
+
+describe('<FunnelChart />', () => {
+    const wrapper = shallow(<FunnelChart />);
+    it('FunnelChart matches snapshot', () => {
+    })
+})
