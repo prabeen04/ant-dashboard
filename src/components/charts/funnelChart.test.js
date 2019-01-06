20292cb4bc10b3b0e61f720870cde7db85217852
Prabeen04 <prabeen.strange@gmail.com>
2019-01-06 19:23:37 +0530

    FunnelChart test implementation removed

--- src/components/charts/funnelChart.test.js
+++ src/components/charts/funnelChart.test.js
@@ -3,36 +3,6 @@ import { shallow } from 'enzyme';
 import FunnelChart from './funnelChart';
 
 describe('<FunnelChart />', () => {
-    const wrapper = shallow(<FunnelChart />);
-    console.log(wrapper.state())
-    console.log(wrapper.props())
     it('FunnelChart matches snapshot', () => {
-        expect(wrapper.state()).toEqual({
-            data: [
-                { label: 'Inquiries', value: 5000 },
-                { label: 'Applicants', value: 2500 },
-                { label: 'Admits', value: 500 },
-                { label: 'Deposits', value: 20 },
-            ],
-            options: {
-                chart: {
-                    height: height,
-                    width: width,
-                    bottomWidth: 1/7,
-                    animate: 300
-                },
-                block: {
-                    dynamicHeight: true,
-                    minHeight: 15,
-                    highlight: true,
-                    fill: {
-                        type: 'gradient'
-                    }
-                },
-                tooltip: {
-                    enabled: true
-                }
-            }
-        })
     })
 })
