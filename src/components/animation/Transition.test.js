9d1a0d8ad3f4a2fa8d9c31b28b9729b074f54d6c
Prabeen04 <prabeen.strange@gmail.com>
2019-01-07 20:36:05 +0530

    react-test-renderer removed from dependency

--- src/components/animation/Transition.test.js
+++ src/components/animation/Transition.test.js
@@ -6,7 +6,7 @@ import { toJson } from "enzyme-to-json";
 describe('<Transition />', () => {
     const wrapper = shallow(<Transition />)
     const div = <div>this is TransitionComponent</div>
-    console.log(toJson(wrapper))
     it('FunnelChart matches snapshot', () => {
+        expect(toJson(wrapper)).toMatchSnapshot()
     })
 })
