35d255126c9bfe17c3c33a449dc61ddd2b8ac335
Prabeen04 <prabeen.strange@gmail.com>
2019-01-07 20:00:07 +0530

    transition.test.js createad

--- src/components/animation/Transition.test.js
+++ src/components/animation/Transition.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import FunnelChart from './funnelChart';
+import Transition from './transition';
 import { toJson } from "enzyme-to-json";
 
-describe('<FunnelChart />', () => {
+describe('<Transition />', () => {
     it('FunnelChart matches snapshot', () => {
     })
 })
