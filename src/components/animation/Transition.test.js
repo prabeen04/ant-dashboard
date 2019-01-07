f5d4acdaa16494f5287385fd40cb8c926cb3b067
Prabeen04 <prabeen.strange@gmail.com>
2019-01-07 21:00:13 +0530

    small stuff

--- src/components/animation/Transition.test.js
+++ src/components/animation/Transition.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Transition from './transition';
-import { toJson } from "enzyme-to-json";
+import toJson from "enzyme-to-json";
 
 describe('<Transition />', () => {
     const wrapper = shallow(<Transition />)
