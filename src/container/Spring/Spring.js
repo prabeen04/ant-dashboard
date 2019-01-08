5c09e58c092f9c886c3985fabc265093459f8edc
Prabeen04 <prabeen.strange@gmail.com>
2019-01-08 19:21:41 +0530

    React fragment added

--- src/container/Spring/Spring.js
+++ src/container/Spring/Spring.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react'
 class Spring extends Component {
     render() {
         return (
-            <>
+            <React.Fragment>
                 <h1>Spring component</h1>
-            </>
+            </React.Fragment>
         )
     }
 }
