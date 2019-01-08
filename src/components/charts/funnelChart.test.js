6a260347b2d049345a38bc23827163ded6123703
Prabeen04 <prabeen.strange@gmail.com>
2019-01-08 18:52:47 +0530

    small test stuff

--- src/components/charts/funnelChart.test.js
+++ src/components/charts/funnelChart.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import FunnelChart from './funnelChart';
-import  toJson  from "enzyme-to-json";
+import toJson from 'enzyme-to-json';
 
 describe('<FunnelChart />', () => {
     const wrapper = shallow(<FunnelChart/>)
