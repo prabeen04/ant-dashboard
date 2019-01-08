b9c79c6e232c15aaa374cb9c7d0eb3f2b0e7fae1
Prabeen04 <prabeen.strange@gmail.com>
2019-01-08 20:21:50 +0530

    transition detail replaced

--- src/container/Spring/SpringTransition.js
+++ src/container/Spring/SpringTransition.js
@@ -1,25 +1,64 @@
-import React, { Component } from 'react'
-import { Transition } from "react-spring";
+import React from 'react'
+import { Transition, animated } from 'react-spring'
 
-class SpringTransition extends Component {
-    render() {
-        const items =  ['Apples', 'Oranges', 'Bananas']
-        return (
-            <div>
-                <Transition
-                    items={items}
-                    from={{ position: 'absolute', opacity: 0 }}
-                    enter={{ opacity: 1 }}
-                    leave={{ opacity: 0 }}>
-                    {toggle =>
-                        toggle
-                            ? props => <div style={props}>😄</div>
-                            : props => <div style={props}>🤪</div>
-                    }
-                </Transition>
-            </div>
-        )
-    }
+const defaultStyles = {
+  overflow: 'hidden',
+  width: '100%',
+  color: 'white',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  fontSize: '2em',
+  fontFamily: "'Kanit', sans-serif",
+  textTransform: 'uppercase',
 }
 
-export default SpringTransition;
+export default class SpringTransitions extends React.PureComponent {
+  state = { items: [] }
+
+  async componentDidMount() {
+    this.t1 && clearTimeout(this.t1)
+    this.t2 && clearTimeout(this.t2)
+    this.t3 && clearTimeout(this.t3)
+
+    this.setState({ items: ['Apples', 'Oranges', 'Bananas'] })
+    this.t1 = setTimeout(
+      () => this.setState({ items: ['Apples', 'Bananas'] }),
+      1500
+    )
+    this.t2 = setTimeout(
+      () => this.setState({ items: ['Apples', 'Oranges', 'Bananas'] }),
+      3000
+    )
+    this.t3 = setTimeout(() => this.setState({ items: ['Kiwis'] }), 4500)
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          backgroundColor: '#70C1B3',
+          overflow: 'hidden',
+          cursor: 'pointer',
+          margin: 0,
+          padding: 0,
+        }}
+        onClick={() => this.componentDidMount()}>
+        <Transition
+          items={this.state.items}
+          //initial={null}
+          from={{ overflow: 'hidden', height: 0, opacity: 0 }}
+          enter={{ height: 50, opacity: 1, background: '#28d79f' }}
+          leave={{ height: 0, opacity: 0, background: '#c23369' }}
+          update={{ background: '#28b4d7' }}
+          trail={200}>
+          {item => styles => (
+            <animated.div style={{ ...defaultStyles, ...styles }}>
+              {item}
+            </animated.div>
+          )}
+        </Transition>
+      </div>
+    )
+  }
+}
