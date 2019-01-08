72cd4adfbf66da41ed6369643e1e2f287971921c
Prabeen04 <prabeen.strange@gmail.com>
2019-01-08 20:06:08 +0530

    basic spring implemented

--- src/container/Spring/Spring.js
+++ src/container/Spring/Spring.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
-
-class Spring extends Component {
+import { Spring } from 'react-spring'
+class Spring_ extends Component {
     render() {
         return (
             <React.Fragment>
                 <h1>Spring component</h1>
+                <Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
+                    {props => <div style={props}>hello</div>}
+                </Spring>
             </React.Fragment>
         )
     }
 }
 
-export default Spring;
+export default Spring_;
