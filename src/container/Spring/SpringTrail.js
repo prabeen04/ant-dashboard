33bc1a318babf341cd300a8e574e8d0bd878f5db
Prabeen04 <prabeen.strange@gmail.com>
2019-01-09 20:17:04 +0530

    react spring trail implmentation

--- src/container/Spring/SpringTrail.js
+++ src/container/Spring/SpringTrail.js
@@ -1,13 +1,40 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Trail, animated } from 'react-spring'
 import { Title } from "../../components/UI/Elements";
 class SpringTrail extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <Title color='teal'>Spring Trail</Title> 
-      </React.Fragment>
-    )   
-  }
+    state = { toggle: true, items: ['item1', 'item2', 'item3', 'item4', 'item5'] }
+    toggle = () => this.setState(state => ({ toggle: !state.toggle }))
+    render() {
+      const { toggle, items } = this.state
+      return (
+        <div
+          style={{
+            backgroundColor: '#247BA0',
+            position: 'relative',
+            width: '100%',
+            height: '100%',
+          }}>
+          <Trail
+            native
+            reverse={toggle}
+            initial={null}
+            items={items}
+            from={{ opacity: 0, x: -100 }}
+            to={{ opacity: toggle ? 1 : 0.25, x: toggle ? 0 : 100 }}>
+            {item =>({ x, opacity }) => (
+              <animated.div
+                className="box"
+                onClick={this.toggle}
+                style={{
+                  opacity,
+                  transform: x.interpolate(x => `translate3d(${x}%,0,0)`),
+                }}
+              />
+            )}
+          </Trail>
+        </div>
+      )
+    }
 }
 
 export default SpringTrail;
