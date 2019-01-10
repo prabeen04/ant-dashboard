f421afc5890a9f38b9c0ed231d65f6f1b5bba1a6
Prabeen04 <prabeen.strange@gmail.com>
2019-01-10 20:16:39 +0530

    animated content added

--- src/container/Spring/AnimatedIcons.js
+++ src/container/Spring/AnimatedIcons.js
@@ -8,23 +8,50 @@ const paths = [
     'M21 16v-2l-8-5V3.5c0-.83-.67-1.5-1.5-1.5S10 2.67 10 3.5V9l-8 5v2l8-2.5V19l-2 1.5V22l3.5-1 3.5 1v-1.5L13 19v-5.5l8 2.5z',
     'M7 2v11h3v9l7-12h-4l4-8z',
     'M12 3v10.55c-.59-.34-1.27-.55-2-.55-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4V7h4V3h-6z',
-  ]
-  
-  const interpolators = []
-  for (let i = 0; i < paths.length; i++) {
+]
+
+const interpolators = []
+for (let i = 0; i < paths.length; i++) {
     interpolators.push(
-      interpolate(paths[i], paths[i + 1] || paths[0], { maxSegmentLength: 0.1 })
+        interpolate(paths[i], paths[i + 1] || paths[0], { maxSegmentLength: 0.1 })
     )
-  }
-  
+}
+
 class AnimatedIcons extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        
-      </React.Fragment>
-    )
-  }
+    state = { interpolators, index: 0 }
+    goNext = () =>
+        this.setState(({ index, interpolators }) => ({
+            index: index + 1 >= interpolators.length ? 0 : index + 1,
+        }))
+    render() {
+        const { interpolators, index } = this.state
+        const interpolator = interpolators[index]
+        return (
+            <div
+                style={{
+                    background: '#F3FFBD',
+                    width: '100%',
+                    height: '100%',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}>
+                <svg width="180" viewBox="0 0 22 22">
+                    <Gradient id="gradient-morph" />
+                    <g fill="url(#gradient-morph)">
+                        <Spring
+                            reset
+                            native
+                            from={{ t: 0 }}
+                            to={{ t: 1 }}
+                            onRest={this.goNext}>
+                            {({ t }) => <animated.path d={t.interpolate(interpolator)} />}
+                        </Spring>
+                    </g>
+                </svg>
+            </div>
+        )
+    }
 }
 
 export default AnimatedIcons;
