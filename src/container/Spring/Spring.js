28dfa1c2d69d9dc125b94a3360a3fa411623e21e
Prabeen04 <prabeen.strange@gmail.com>
2019-01-10 20:17:48 +0530

    Icon rendered on spring

--- src/container/Spring/Spring.js
+++ src/container/Spring/Spring.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react'
 import { Spring } from 'react-spring'
 import SpringTransition from "./SpringTransition";
 import SpringTrail from "./SpringTrail";
+import AnimatedIcons from './AnimatedIcons'
 class Spring_ extends Component {
     render() {
         return (
             <React.Fragment>
-                <Spring                
+                <Spring
                     from={{ number: 0 }}
                     to={{ number: 100 }}>
                     {props => <h1>{props.number.toFixed()}</h1>}
                 </Spring>
+                <AnimatedIcons />
                 <SpringTransition />
                 <SpringTrail />
             </React.Fragment>
