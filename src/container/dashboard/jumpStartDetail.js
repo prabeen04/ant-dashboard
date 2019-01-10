4fb514f69ff6bba291e0e58708c7b49bd5303df5
Prabeen04 <prabeen.strange@gmail.com>
2019-01-10 20:40:15 +0530

    Spring added in jumpstart detail

--- src/container/dashboard/jumpStartDetail.js
+++ src/container/dashboard/jumpStartDetail.js
@@ -2,22 +2,31 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from "redux";
 import classNames from 'classnames';
-import { openJumpstartModal } from "../../actions/dashboardAction";
 import { Icon } from 'antd'
+import { Spring } from "react-spring";
+import { openJumpstartModal } from "../../actions/dashboardAction";
 const JumpStartDetail = (props) => {
     return (
-        <div className={'jumpstart-box test'}
-            onClick={() => props.openJumpstartModal(props.data)}
-        >
-            <div>
-                <h3 style={{ color: '#aaa' }}>{props.data.title}</h3>
-                <h2 style={{ color: '#eee' }}>{props.data.value}
-                    <span className={props.jumpstartStyle}>
-                        {props.data.progress > 0 ? <Icon type='caret-up' /> : <Icon type='caret-down' />}
-                        &nbsp;{props.data.progress}</span>
-                </h2>
-            </div>
-        </div>
+        <Spring
+            from={{ opacity: 0 }}
+            to={{ opacity: 1 }}>
+            {props => (
+                <div
+                    style={props}
+                    className={'jumpstart-box test'}
+                    onClick={() => props.openJumpstartModal(props.data)}
+                >
+                    <div>
+                        <h3 style={{ color: '#aaa' }}>{props.data.title}</h3>
+                        <h2 style={{ color: '#eee' }}>{props.data.value}
+                            <span className={props.jumpstartStyle}>
+                                {props.data.progress > 0 ? <Icon type='caret-up' /> : <Icon type='caret-down' />}
+                                &nbsp;{props.data.progress}</span>
+                        </h2>
+                    </div>
+                </div>
+            )}
+        </Spring>
     )
 }
 
