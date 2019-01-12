8579bd1d1fe9ed3f0c21e22520a71af9a9d7aab5
Prabeen04 <prabeen.strange@gmail.com>
2019-01-12 20:27:53 +0530

    small stuff

--- src/components/utils/__test__/ReactTable.test.js
+++ src/components/utils/__test__/ReactTable.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from "enzyme";
 import ReactTable from '../ReactTable'
-import { toJson } from "enzyme-to-json";
+import toJson from "enzyme-to-json";
 describe('<ReactTable/>', () => {
     const wrapper = shallow(<ReactTable />)
     it('should matches snapshot', () => {
