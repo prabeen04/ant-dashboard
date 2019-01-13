092f544f57108b801aa258d3b764a65d33e20277
Prabeen04 <prabeen.strange@gmail.com>
2019-01-13 19:39:16 +0530

    Menu state added

--- src/components/layouts/navMenu.js
+++ src/components/layouts/navMenu.js
@@ -5,9 +5,29 @@ import { Link } from 'react-router-dom';
 import Theme from '../../container/settings/Theme/Theme';
 import { Spring } from "react-spring";
 const SubMenu = Menu.SubMenu;
+const menuData = [
+    {path: '/', key: '1', iconType: 'appstore', title: 'DashBoard'},
+    {path: '/calendar', key: '2', iconType: 'appstore', title: 'Calendar'},
+    {path: '/profile', key: '3', iconType: 'appstore', title: 'Profile'},
+    {path: '/post', key: '4', iconType: 'appstore', title: 'Post'},
+    {path: '/forms', key: '5', iconType: 'appstore', title: 'Forms'},
+    {path: '/tables', key: '6', iconType: 'appstore', title: 'Table'},
+    {path: '/settings', key: '7', iconType: 'appstore', title: 'Setting'},
+    {path: '/dnd', key: '8', iconType: 'appstore', title: 'DND'},
+    {path: '/charts', key: '9', iconType: 'appstore', title: 'Charts'},
+    {path: '/map', key: '10', iconType: 'appstore', title: 'Map'},
+    {path: '/drag', key: '11', iconType: 'appstore', title: 'Drag n Change'},
+    {path: '/tree', key: '12', iconType: 'appstore', title: 'Tree'},
+    {path: '/custom', key: '13', iconType: 'appstore', title: 'Custom Fields'},
+    {path: '/fullCalendar', key: '14', iconType: 'appstore', title: 'Full calendar'},
+    {path: '/spring', key: '15', iconType: 'appstore', title: 'Spring'},
+]
 class NavMenu extends React.Component {
-    state = {
-        collapsed: false
+    constructor(props) {
+        super(props)
+        this.state = {
+            collapsed: false,
+        }
     }
     // toggleCollapsed = () => {
     //     this.setState({
