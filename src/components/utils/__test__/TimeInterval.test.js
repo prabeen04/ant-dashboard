3de835fbb2fc31d7ff6fb26cd2543854d9c92889
Prabeen04 <prabeen.strange@gmail.com>
2019-01-14 19:45:38 +0530

    small stuff

--- src/components/utils/__test__/TimeInterval.test.js
+++ src/components/utils/__test__/TimeInterval.test.js
@@ -3,15 +3,7 @@ import { shallow } from "enzyme";
 import TimeInterval from '../TimeInterval'
 import toJson from "enzyme-to-json";
 describe('<TimeInterval/>', () => {
-    const wrapper = shallow(<TimeInterval time=[
-        { id: 1, value: '1W' },
-        { id: 2, value: '4W' },
-        { id: 3, value: '1Y' },
-        { id: 4, value: 'MTD' },
-        { id: 5, value: 'QTD' },
-        { id: 6, value: 'YTD' },
-        { id: 7, value: 'ALL' },
-    ] />)
+    const wrapper = shallow(<TimeInterval />)
     it('should matches snapshot', () => {
         expect(toJson(wrapper)).toMatchSnapshot()
     })
