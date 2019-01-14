42c230a4f10790720c1d65f3571e6a213b65e77d
Prabeen04 <prabeen.strange@gmail.com>
2019-01-14 19:43:33 +0530

    time prop added to TimeInterval test file

--- src/components/utils/__test__/TimeInterval.test.js
+++ src/components/utils/__test__/TimeInterval.test.js
@@ -3,7 +3,15 @@ import { shallow } from "enzyme";
 import TimeInterval from '../TimeInterval'
 import toJson from "enzyme-to-json";
 describe('<TimeInterval/>', () => {
-    const wrapper = shallow(<TimeInterval />)
+    const wrapper = shallow(<TimeInterval time=[
+        { id: 1, value: '1W' },
+        { id: 2, value: '4W' },
+        { id: 3, value: '1Y' },
+        { id: 4, value: 'MTD' },
+        { id: 5, value: 'QTD' },
+        { id: 6, value: 'YTD' },
+        { id: 7, value: 'ALL' },
+    ] />)
     it('should matches snapshot', () => {
         expect(toJson(wrapper)).toMatchSnapshot()
     })
