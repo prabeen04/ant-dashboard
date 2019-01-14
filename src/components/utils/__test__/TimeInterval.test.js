42cbe3fc8e506c86c84cca5028d47ebeadecaaa9
Prabeen04 <prabeen.strange@gmail.com>
2019-01-14 19:34:06 +0530

    Timeinterval test file modified

--- src/components/utils/__test__/TimeInterval.test.js
+++ src/components/utils/__test__/TimeInterval.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from "enzyme";
 import TimeInterval from '../TimeInterval'
-import { toJson } from "enzyme-to-json";
+import toJson from "enzyme-to-json";
 describe('<TimeInterval/>', () => {
     const wrapper = shallow(<TimeInterval />)
     it('should matches snapshot', () => {
