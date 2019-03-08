d462e7a4dd8b8e8c8290a8ad62a3d4d2cfdb6569
Prabeen04 <prabeen.strange@gmail.com>
2019-03-08 23:39:57 +0530

    Drawer states added

--- src/container/profile/profileVIew.js
+++ src/container/profile/profileVIew.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { Card, Icon, Avatar, Drawer } from 'antd'
@@ -26,46 +26,48 @@ const { Meta } = StyledCard;
 //     });
 //   };
 //   onDrawerClose = () => this.setState({ drawerVisible: false })
-  function ProfileView (props) {
-    return (
-      <div>
-        {props.profile.name &&
-          <div>
+function ProfileView(props) {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false)
+  const [drawerVisible, setDrawerVisible] = useState(true)
+  return (
+    <div>
+      {props.profile.name &&
+        <div>
 
-            <div className="panel-header">
-              <h3>{props.profile.name}</h3>
-            </div>
-            <StyledCard
-              style={{ width: '100%' }}
-              cover={<img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />}
-              actions={[
-                <Icon onClick={() => this.showDrawer()} type="setting" />,
-                <Icon type="edit" onClick={() => props.openEditProfile(props.profile)} />,
-                <Icon type="tool" theme="outlined" />
-              ]}
-            >
-              <Meta
-                avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-                title={props.profile.name}
-                description={props.profile.email}
-              />
-            </StyledCard>
+          <div className="panel-header">
+            <h3>{props.profile.name}</h3>
           </div>
+          <StyledCard
+            style={{ width: '100%' }}
+            cover={<img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />}
+            actions={[
+              <Icon onClick={() => this.showDrawer()} type="setting" />,
+              <Icon type="edit" onClick={() => props.openEditProfile(props.profile)} />,
+              <Icon type="tool" theme="outlined" />
+            ]}
+          >
+            <Meta
+              avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+              title={props.profile.name}
+              description={props.profile.email}
+            />
+          </StyledCard>
+        </div>
 
-        }
-        <ProfileDrawer open={this.state.isDrawerOpen} onClose={this.onClose} user={props.profile} />
-        <Drawer
-          title="Multi-level drawer"
-          width={820}
-          closable={true}
-          onClose={this.onDrawerClose}
-        // visible={this.state.drawerVisible}
-        >
-          <EventTimeline />
-        </Drawer>
-      </div>
-    )
-  }
+      }
+      <ProfileDrawer open={this.state.isDrawerOpen} onClose={this.onClose} user={props.profile} />
+      <Drawer
+        title="Multi-level drawer"
+        width={820}
+        closable={true}
+        onClose={this.onDrawerClose}
+      // visible={this.state.drawerVisible}
+      >
+        <EventTimeline />
+      </Drawer>
+    </div>
+  )
+}
 // }
 const mapStateToProps = (state) => ({
   profile: state.profileReducer.singleProfile
