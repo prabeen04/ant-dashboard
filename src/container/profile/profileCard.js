2af15c8fbc380c9cad8a415c25ee37dffb98b9e7
Prabeen04 <prabeen.strange@gmail.com>
2019-03-09 15:54:31 +0530

    ProfileCard made functional component

--- src/container/profile/profileCard.js
+++ src/container/profile/profileCard.js
@@ -10,55 +10,35 @@ import './profile.css';
 const { Meta } = Card;
 const imgUrl = 'https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png';
 
-class ProfileCard extends Component {
-    constructor(props) {
-        super(props)
-
-
-    }
-    loadImage = (src) => {
-        return new Promise((resolve, reject) => {
-            const image = new Image();
-            image.onload = () => resolve(src);
-            image.onerror = err => reject(err);
-            image.src = src;
-        });
-    }
-    render() {
-        return (
-            <div className={this.props.activatedProfile.name === this.props.user.name ? 'activated-profile' : ''}>
-                <Card
-                    hoverable
-                    style={{ width: 155, height: 200, margin: '0.5rem' }}
-                    onClick={() => this.props.getSingleProfile(this.props.user)}
-                    cover={
-                        // <img style={{ width: '100%', height: 150, filter: 'blur(6px)' }} alt="example" src={Preview} />
-                        <ProgressiveImage
-                            preview={Preview}
-                            image={imgUrl}
-                            width={'100%'}
-                            height={150}
-                        />
-                    }>
-                    <Meta
-                        title={this.props.user.name}
-                        style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '25px' }}
+function ProfileCard(props) {
+    return (
+        <div className={props.activatedProfile.name === props.user.name ? 'activated-profile' : ''}>
+            <Card
+                hoverable
+                style={{ width: 155, height: 200, margin: '0.5rem' }}
+                onClick={() => props.getSingleProfile(props.user)}
+                cover={
+                    <ProgressiveImage
+                        preview={Preview}
+                        image={imgUrl}
+                        width={'100%'}
+                        height={150}
                     />
-                </Card>
-            </div>
-        )
-    }
+                }>
+                <Meta
+                    title={props.user.name}
+                    style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '25px' }}
+                />
+            </Card>
+        </div>
+    )
 }
-const mapStateToProps = (state) => {
-    return {
-        profile: state.profileReducer.profile,
-        activatedProfile: state.profileReducer.singleProfile
-    }
-}
+const mapStateToProps = ({ profileReducer }) => ({
+    profile: profileReducer.profile,
+    activatedProfile: profileReducer.singleProfile
+})
 
-const mapDispatchToProps = (dispatch) => {
-    return bindActionCreators({
-        getSingleProfile
-    }, dispatch)
-}
+const mapDispatchToProps = dispatch => bindActionCreators({
+    getSingleProfile
+}, dispatch)
 export default connect(mapStateToProps, mapDispatchToProps)(ProfileCard);
