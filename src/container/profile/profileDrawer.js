c5b8fad38e94896651663b90c03954aa5126860a
Prabeen04 <prabeen.strange@gmail.com>
2019-03-09 16:04:51 +0530

    ProfileDrawer made functional

--- src/container/profile/profileDrawer.js
+++ src/container/profile/profileDrawer.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
 import { Drawer, Button } from 'antd';
 import ProfileTimeline from './profileTimeline';
-class ProfileDrawer extends Component {
-    render() {
-        return (
-            <div>
-                <Drawer
-                    visible={this.props.open}
-                    title={this.props.user.name}
-                    placement="right"
-                    width={600}
-                    closable={true}
-                    onClose={this.props.onClose}
-                >
-                    <ProfileTimeline />
-                </Drawer>
-            </div>
-        )
-    }
+
+function ProfileDrawer(props) {
+    return (
+        <div>
+            <Drawer
+                visible={props.open}
+                title={props.user.name}
+                placement="right"
+                width={600}
+                closable={true}
+                onClose={props.onClose}
+            >
+                <ProfileTimeline />
+            </Drawer>
+        </div>
+    )
+}
 }
 export default ProfileDrawer;
