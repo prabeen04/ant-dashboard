a4559f373ff4bd70c89a8c3ed7fd7a8c873e21ce
Prabeen04 <prabeen.strange@gmail.com>
2019-03-10 19:30:38 +0530

    GooglePlace made functional with useState

--- src/components/google/googlePlace.js
+++ src/components/google/googlePlace.js
@@ -1,22 +1,25 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
 import { setGoogleAddress } from '../../actions/googleAction';
-
 import './google.css';
-class GooglePlace extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { address: '' }
+
+// class GooglePlace extends Component {
+//     constructor(props) {
+//         super(props);
+//         state = { address: '' }
+//     }
+
+
+function GooglePlace(props) {
+    const [address, setAddress] = useState('')
+    const handleChange = (address) => {
+        setAddress(address)
     }
 
-    handleChange = (address) => {
-        this.setState({ address })
-    }
-
-    handleSelect = (address) => {
-        this.props.setGoogleAddress(address)
+    const handleSelect = (address) => {
+        props.setGoogleAddress(address)
         geocodeByAddress(address)
             .then(results => {
                 console.log(results)
@@ -25,55 +28,52 @@ class GooglePlace extends Component {
             .then(latLng => console.log('Success', latLng))
             .catch(error => console.error('Error', error))
     }
-    render() {
-        return (
-            <div style={{ width: 400 }}>
-                <h3>{this.props.address}</h3>
-                <PlacesAutocomplete
-                    value={this.state.address}
-                    onChange={this.handleChange}
-                    onSelect={this.handleSelect}
-                >
-                    {({ getInputProps, suggestions, getSuggestionItemProps }) => (
-                        <div>
-                            <input
-                                {...getInputProps({
-                                    placeholder: 'Search Places ...',
-                                    className: 'location-search-input'
-                                })}
-                            />
-                            <div className="autocomplete-dropdown-container">
-                                {suggestions.map(suggestion => {
-                                    const className = suggestion.active ? 'suggestion-item--active' : 'suggestion-item';
-                                    // inline style for demonstration purpose
-                                    const style = suggestion.active
-                                        ? { backgroundColor: '#fafafa', cursor: 'pointer' }
-                                        : { backgroundColor: '#ffffff', cursor: 'pointer' };
-                                    return (
-                                        <div {...getSuggestionItemProps(suggestion, { className, style })}>
-                                            <span>{suggestion.description}</span>
-                                        </div>
-                                    )
-                                })}
-                            </div>
+
+    return (
+        <div style={{ width: 400 }}>
+            <h3>{props.address}</h3>
+            <PlacesAutocomplete
+                value={address}
+                onChange={handleChange}
+                onSelect={handleSelect}
+            >
+                {({ getInputProps, suggestions, getSuggestionItemProps }) => (
+                    <div>
+                        <input
+                            {...getInputProps({
+                                placeholder: 'Search Places ...',
+                                className: 'location-search-input'
+                            })}
+                        />
+                        <div className="autocomplete-dropdown-container">
+                            {suggestions.map(suggestion => {
+                                const className = suggestion.active ? 'suggestion-item--active' : 'suggestion-item';
+                                // inline style for demonstration purpose
+                                const style = suggestion.active
+                                    ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                                    : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                                return (
+                                    <div {...getSuggestionItemProps(suggestion, { className, style })}>
+                                        <span>{suggestion.description}</span>
+                                    </div>
+                                )
+                            })}
                         </div>
-                    )}
-                </PlacesAutocomplete>
-                <br />
-            </div>
-        )
-    }
+                    </div>
+                )}
+            </PlacesAutocomplete>
+            <br />
+        </div>
+    )
 }
+// }
 
-const mapStateToProps = state => {
-    return {
-        address: state.googleReducer.address
-    }
-}
+const mapStateToProps = ({ googleReducer }) => ({
+    address: googleReducer.address
+})
 
-const mapDispatchToProps = dispatch => {
-    return bindActionCreators({
-        setGoogleAddress
-    }, dispatch)
-}
+const mapDispatchToProps = dispatch => bindActionCreators({
+    setGoogleAddress
+}, dispatch)
+
 export default connect(mapStateToProps, mapDispatchToProps)(GooglePlace);
