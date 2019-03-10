92123ef15add9a9363aed27f14db38dae405df9a
Prabeen04 <prabeen.strange@gmail.com>
2019-03-10 19:19:04 +0530

    FutureNotification made functional and data fetched in useEffect

--- src/container/notification/futureNotification.js
+++ src/container/notification/futureNotification.js
@@ -1,48 +1,43 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { getFutureNotifications } from '../../actions/notificationActions';
 import { List, Avatar, Spin } from 'antd';
 
-class FutureNotification extends Component {
-  componentDidMount() {
-    this.props.getFutureNotifications();
-  }
-  render() {
-    return (
-      <div>
-        <List
-          dataSource={this.props.futureNotifications.results}
-          renderItem={item => (
-            <List.Item key={item.id}>
-              <List.Item.Meta
-                avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-                title={<a href="https://ant.design">{item.name.last}</a>}
-                description={item.email}
-              />
-            </List.Item>
-          )}
-        >
-          {this.props.isFetching && (
-            <div style={{ textAlign: 'center' }}>
-              <Spin />
-            </div>
-          )}
-        </List>
-      </div>
-    )
-  }
+function FutureNotification(props) {
+  useEffect(() => {
+    props.getFutureNotifications();
+  }, [])
+  return (
+    <React.Fragment>
+      <List
+        dataSource={props.futureNotifications.results}
+        renderItem={item => (
+          <List.Item key={item.id}>
+            <List.Item.Meta
+              avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+              title={<a href="https://ant.design">{item.name.last}</a>}
+              description={item.email}
+            />
+          </List.Item>
+        )}
+      >
+        {props.isFetching && (
+          <div style={{ textAlign: 'center' }}>
+            <Spin />
+          </div>
+        )}
+      </List>
+    </React.Fragment>
+  )
 }
 
-const mapStateToProps = state => {
-  return {
-    isFetching: state.notificationReducer.isFetching,
-    futureNotifications: state.notificationReducer.futureNotifications,
-  }
-}
-const mapDispatchToProps = dispatch => {
-  return bindActionCreators({
-    getFutureNotifications
-  }, dispatch)
-}
+const mapStateToProps = ({ notificationReducer }) => ({
+  isFetching: notificationReducer.isFetching,
+  futureNotifications: notificationReducer.futureNotifications,
+})
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+  getFutureNotifications
+}, dispatch)
 export default connect(mapStateToProps, mapDispatchToProps)(FutureNotification);
