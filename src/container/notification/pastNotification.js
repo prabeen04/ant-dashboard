49f0c36cd6aadeca1fd2e5a246129db8aec31b39
Prabeen04 <prabeen.strange@gmail.com>
2019-03-10 19:06:27 +0530

    past notification made functiona component

--- src/container/notification/pastNotification.js
+++ src/container/notification/pastNotification.js
@@ -1,38 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { List, Button, Checkbox, Avatar } from 'antd';
 
-class PastNotification extends Component {
-    render() {
-        return (
-            <div>
-                <List
-                    size="small"
-                    bordered
-                    dataSource={this.props.pastNotifications}
-                    renderItem={item => (
-                        <List.Item>
-                            <List.Item.Meta
-                                avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-                                title={<a href="https://ant.design">{item.title}</a>}
-                                description={item.description}
-                            />
-                        </List.Item>
-                    )}
-                />
-            </div>
-        )
-    }
+function PastNotification(props) {
+    return (
+        <div>
+            <List
+                size="small"
+                bordered
+                dataSource={props.pastNotifications}
+                renderItem={item => (
+                    <List.Item>
+                        <List.Item.Meta
+                            avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                            title={<a href="https://ant.design">{item.title}</a>}
+                            description={item.description}
+                        />
+                    </List.Item>
+                )}
+            />
+        </div>
+    )
 }
-const mapStateToProps = state => {
-    return {
-        pastNotifications: state.notificationReducer.pastNotifications
-    }
-}
-const mapDispatchToProps = dispatch =>{
-    return bindActionCreators({
 
-    }, dispatch)
-}
+const mapStateToProps = ({ notificationReducer }) => ({
+    pastNotifications: notificationReducer.pastNotifications
+})
+const mapDispatchToProps = dispatch => bindActionCreators({
+
+}, dispatch)
 export default connect(mapStateToProps, mapDispatchToProps)(PastNotification);
