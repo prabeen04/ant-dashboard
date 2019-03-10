c7717c6b085b40677603bd3e28a061326dec1282
Prabeen04 <prabeen.strange@gmail.com>
2019-03-10 19:16:05 +0530

    Present Notification made functional and data fetched in useEffect

--- src/container/notification/presentNotification.js
+++ src/container/notification/presentNotification.js
@@ -1,59 +1,44 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { getPresentNotifications } from '../../actions/notificationActions';
 import { List, Avatar, Spin } from 'antd';
 import { InfiniteScroll } from 'react-infinite-scroller';
 
-class PresentNotification extends Component {
-  componentDidMount(){
-    this.props.getPresentNotifications();
-  }
-  render() {
-    return (
-      <div>
-        {/* {this.props.isFetching && <p>Loading...</p>} */}
-        {/* <InfiniteScroll
-          initialLoad={false}
-          pageStart={0}
-          // loadMore={this.handleInfiniteOnLoad}
-          // hasMore={!this.props.isFetching && this.state.hasMore}
-          useWindow={false}
-        > */}
-          <List
-            dataSource={this.props.presentNotifications.results}
-            renderItem={item => (
-              <List.Item key={item.id}>
-                <List.Item.Meta
-                  avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-                  title={<a href="https://ant.design">{item.name.last}</a>}
-                  description={item.email}
-                />
-                <div>Content</div>
-              </List.Item>
-            )}
-          >
-            {this.props.isFetching &&(
-              <div style={{textAlign: 'center'}}>
-                <Spin />
-              </div>
-            )}
-          </List>
-        {/* </InfiniteScroll> */}
+function PresentNotification(props) {
+  useEffect(() => {
+      props.getPresentNotifications()
+  }, [])
+  return (
+    <React.Fragment>
+      <List
+        dataSource={props.presentNotifications.results}
+        renderItem={item => (
+          <List.Item key={item.id}>
+            <List.Item.Meta
+              avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+              title={<a href="https://ant.design">{item.name.last}</a>}
+              description={item.email}
+            />
+            <div>Content</div>
+          </List.Item>
+        )}
+      >
+        {props.isFetching && (
+          <div style={{ textAlign: 'center' }}>
+            <Spin />
+          </div>
+        )}
+      </List>
+    </React.Fragment>
+  )
+}
+const mapStateToProps = ({ notificationReducer }) => ({
+  isFetching: notificationReducer.isFetching,
+  presentNotifications: notificationReducer.presentNotifications,
+})
+const mapDispatchToProps = dispatch => bindActionCreators({
+  getPresentNotifications
+}, dispatch)
 
-      </div>
-    )
-  }
-}
-const mapStateToProps = state => {
- return{
-   isFetching: state.notificationReducer.isFetching,
-   presentNotifications: state.notificationReducer.presentNotifications,
- }
-}
-const mapDispatchToProps = dispatch => {
-  return bindActionCreators({
-      getPresentNotifications
-  }, dispatch)
-}
 export default connect(mapStateToProps, mapDispatchToProps)(PresentNotification);
