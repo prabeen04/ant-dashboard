b77268fb96d81a749f4286af8f4b284e1dfbe2e5
Prabeen04 <prabeen.strange@gmail.com>
2019-03-11 21:16:50 +0530

    unused code cleane

--- src/components/layouts/navMenu.js
+++ src/components/layouts/navMenu.js
@@ -5,31 +5,7 @@ import { Link } from 'react-router-dom';
 import Theme from '../../container/settings/Theme/Theme';
 import { Spring } from "react-spring";
 const SubMenu = Menu.SubMenu;
-const menuData = [
-    { path: '/', key: '1', iconType: 'appstore', title: 'DashBoard' },
-    { path: '/calendar', key: '2', iconType: 'calendar', title: 'Calendar' },
-    { path: '/profile', key: '3', iconType: 'profile', title: 'Profile' },
-    { path: '/post', key: '4', iconType: 'medium', title: 'Post' },
-    { path: '/forms', key: '5', iconType: 'edit', title: 'Forms' },
-    { path: '/tables', key: '6', iconType: 'table', title: 'Table' },
-    { path: '/settings', key: '7', iconType: 'setting', title: 'Setting' },
-    { path: '/dnd', key: '8', iconType: 'select', title: 'DND' },
-    { path: '/charts', key: '9', iconType: 'area-chart', title: 'Charts' },
-    { path: '/map', key: '10', iconType: 'global', title: 'Map' },
-    { path: '/drag', key: '11', iconType: 'edit', title: 'Drag n Change' },
-    { path: '/tree', key: '12', iconType: 'plus', title: 'Tree' },
-    { path: '/custom', key: '13', iconType: 'plus', title: 'Custom Fields' },
-    { path: '/fullCalendar', key: '14', iconType: 'calendar', title: 'Full calendar' },
-    { path: '/spring', key: '15', iconType: 'gift', title: 'Spring' },
-]
-// class NavMenu extends React.Component {
-//     constructor(props) {
-//         super(props)
-//         this.state = {
-//             collapsed: false,
-//             menuData: menuData,
-//         }
-//     }
+
 export default function NavMenu(props) {
     const [collapsed, setCollapsed] = useState(false)
     const [menuData, setMenuData] = useState([
@@ -91,6 +67,3 @@ export default function NavMenu(props) {
         </div>
     );
 }
-// }
-
-// export default NavMenu;
