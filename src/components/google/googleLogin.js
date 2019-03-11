9c212f94d014a0840fd91074bfea88dd8f00ad29
Prabeen04 <prabeen.strange@gmail.com>
2019-03-11 20:33:09 +0530

    GoogleLogin made functional and use useState

--- src/components/google/googleLogin.js
+++ src/components/google/googleLogin.js
@@ -1,47 +1,34 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { GoogleLogin, GoogleLogout } from 'react-google-login';
 
-class LoginWithGoogle extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            isLoggedIn: false,
-            googleUser: {}
-        }
-        this.responseGoogle = this.responseGoogle.bind(this);
-    }
-    responseGoogle = (res) =>{ 
+export default function LoginWithGoogle(props) {
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+    const [googleUser, setGoogleUser] = useState({})
+    const responseGoogle = (res) => {
         console.log(res)
-        if(res.accessToken){
-            this.setState({
-                isLoggedIn: true,
-                googleUser: res.profileObj
-            })
+        if (res.accessToken) {
+            setIsLoggedIn(true)
+            setGoogleUser(res.profileObj)
         }
     }
-    render() {
-        return (
-            <div>
-                {this.state.googleUser && <h3>{this.state.googleUser.name}</h3>}
-               {!this.state.isLoggedIn && <GoogleLogin
-               
-                    clientId="553440503285-rgciog1jtcen74j5hq1ojul6o4b93erh.apps.googleusercontent.com"
-                    buttonText={this.props.buttonText || 'Login with Google'}
-                    onSuccess={this.responseGoogle}
-                    onFailure={this.responseGoogle}
-                />}
-              {this.state.isLoggedIn &&  <GoogleLogout
-                    buttonText="Logout"
-                    onLogoutSuccess={(res)=>{
-                        this.setState({
-                            isLoggedIn: false,
-                            googleUser: {}
-                        })
-                    }}
-                >
-                </GoogleLogout>}
-            </div>
-        )
-    }
+    return (
+        <div>
+            {googleUser && <h3>{googleUser.name}</h3>}
+            {!isLoggedIn && <GoogleLogin
+
+                clientId="553440503285-rgciog1jtcen74j5hq1ojul6o4b93erh.apps.googleusercontent.com"
+                buttonText={props.buttonText || 'Login with Google'}
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+            />}
+            {isLoggedIn && <GoogleLogout
+                buttonText="Logout"
+                onLogoutSuccess={(res) => {
+                    setIsLoggedIn(false)
+                    setGoogleUser({})
+                }}
+            >
+            </GoogleLogout>}
+        </div>
+    )
 }
-export default LoginWithGoogle;
