f1b47abac655951c0b6bb242d7acaed7aabc8109
Prabeen04 <prabeen.strange@gmail.com>
2019-03-11 20:28:46 +0530

    Pagination component made funcitonal

--- src/components/common/pagination/pagination.js
+++ src/components/common/pagination/pagination.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
 import { Pagination } from 'antd';
 
-class PaginationComponent extends Component {
-  render() {
-    return (
-      <div>
-         <Pagination defaultCurrent={1} total={50} /> 
-      </div>
-    )
-  }
+export default function PaginationComponent(props) {
+  return (
+    <div>
+      <Pagination defaultCurrent={1} total={50} />
+    </div>
+  )
 }
-
-export default PaginationComponent;
+}
