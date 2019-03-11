ad7c81b1de996dd9f3472b855942ab495529cd86
Prabeen04 <prabeen.strange@gmail.com>
2019-03-11 21:40:59 +0530

    SettingPopover made functional

--- src/components/popover/settingPopover.js
+++ src/components/popover/settingPopover.js
@@ -1,51 +1,34 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Icon, Popover, Tooltip, Card, Button } from 'antd';
-// import AddProfile from '../../container/profile/addProfile';
 import SettingTab from '../tabs/settingTab';
 import { StyledPopover } from "../UI/Antd";
 
-class SettingPopover extends Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            visible: false
-        }
-    }
-    hide = () => {
-        this.setState({
-            visible: false,
-        });
-    }
-    handleVisibleChange = (visible) => {
-        this.setState({ visible });
-    }
-    render() {
-        return (
-            <div>
-                <Tooltip title="Settings">
-                    <StyledPopover
-                        content={
-                            <div>
-                                <SettingTab hide={this.hide}/>
-                            </div>
+export default function SettingPopover(props) {
+    const [visible, setVisible] = useState(false)
+    const hide = () => setVisible(false)
+    const handleVisibleChange = (visible) => setVisible(visible);
+    return (
+        <div>
+            <Tooltip title="Settings">
+                <StyledPopover
+                    content={
+                        <div>
+                            <SettingTab hide={hide} />
+                        </div>
 
                     }
-                        // title={<a onClick={this.hide}>Close</a>}
-                        trigger="click"
-                        placement="bottomRight"
-                        visible={this.state.visible}
-                        onVisibleChange={this.handleVisibleChange}
-                    >
-                        <Icon
-                            style={{ fontSize: 25, cursor: 'pointer' }}
-                            type="setting"
-                            onClick={() => this.handleVisibleChange()}
-                        />
-                    </StyledPopover>
-                </Tooltip>
-            </div>
-        )
-    }
+                    trigger="click"
+                    placement="bottomRight"
+                    visible={visible}
+                    onVisibleChange={handleVisibleChange}
+                >
+                    <Icon
+                        style={{ fontSize: 25, cursor: 'pointer' }}
+                        type="setting"
+                        onClick={() => handleVisibleChange()}
+                    />
+                </StyledPopover>
+            </Tooltip>
+        </div>
+    )
 }
-export default SettingPopover
