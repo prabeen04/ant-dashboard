acbf7ad15a26f1a2a81051a5f901c48bd4995416
Prabeen04 <prabeen.strange@gmail.com>
2019-03-12 20:42:15 +0530

    EditEventModal made functional

--- src/container/calendar/EditEventModal.js
+++ src/container/calendar/EditEventModal.js
@@ -1,22 +1,18 @@
 import React, { Component } from 'react';
 import { StyledModal } from '../../components/UI/Antd';
 
-class EditEventModal extends Component {
-    render() {
-        const { title, visible, onClose, onOk } = this.props
-        return (
-            <div>
-                <StyledModal
-                    title={title || ''}
-                    visible={visible || null}
-                    onOk={onOk || null}
-                    onCancel={onClose || null}
-                >
-                    {this.props.children}
-                </StyledModal>
-            </div>
-        )
-    }
+export default function EditEventModal({ title, visible, onClose, onOk }) {
+    return (
+        <div>
+            <StyledModal
+                title={title || ''}
+                visible={visible || null}
+                onOk={onOk || null}
+                onCancel={onClose || null}
+            >
+                {this.props.children}
+            </StyledModal>
+        </div>
+    )
 }
 
-export default EditEventModal;
