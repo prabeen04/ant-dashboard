91bedaa1743a28531e8e53a2446c1e7556a3de31
Prabeen04 <prabeen.strange@gmail.com>
2019-03-12 20:39:22 +0530

    Calendar made functional

--- src/container/calendar/calendar.js
+++ src/container/calendar/calendar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { getEvents, setDate } from '../../actions/calendar_actions';
@@ -12,113 +12,113 @@ import { MainWrapper } from '../../components/UI/Layout';
 import { StyledTabs } from '../../components/UI/Antd';
 const TabPane = Tabs.TabPane;
 
-class Calendar extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      activeTab: "1",
-      view: 'week',
-      isModalVisible: false,
-      selectedEvent: {}
-    }
-  }
+// class Calendar extends Component {
+//   constructor(props) {
+//     super(props)
+//     state = {
+//       activeTab: "1",
+//       view: 'week',
+//       isModalVisible: false,
+//       selectedEvent: {}
+//     }
+//   }
 
-  success = () => {
+
+//   componentDidMount() {
+//     props.getEvents();
+//   }
+
+function Calendar(props) {
+  const [activeTab, setActiveTab] = useState('1')
+  const [view, setView] = useState('week')
+  const [isModalVisible, setIsModalVisible] = useState(false)
+  const [selectedEvent, setSelectedEvent] = useState({})
+  const success = () => {
     message.success('Event Added', 10);
   };
-  handleOk = (slot) => {
-    this.props.setDate(slot)
+  const handleOk = (slot) => {
+    props.setDate(slot)
   }
-  eventSelect = (event) => {
+  const eventSelect = (event) => {
     console.log(event)
   }
-  onSelectEvent = (event) => {
-    console.log(event)
-    this.setState({
-      isModalVisible: true,
-      selectedEvent: event
-    })
+  const onSelectEvent = (event) => {
+    setIsModalVisible(true)
+    setSelectedEvent(event)
   }
-  onModalClose = () => this.setState({ isModalVisible: false, selectedEvent: {} })
-  componentDidMount() {
-    this.props.getEvents();
+  const onModalClose = () => {
+    setIsModalVisible(false)
+    setSelectedEvent({})
   }
 
-  render() {
-    if (this.props.isLoading) {
-      return (<div className="flex-container" style={{ height: '80vh', justifyContent: 'center', alignItems: 'center' }}>
-        <Icon type="loading" style={{ fontSize: 60, color: 'tomato' }} spin />
-      </div>)
-    }
-    // if (this.props.isError) {
-    //   return (<p>Some Error occoured...</p>)
-    // }
-    return (
-      <React.Fragment>
-        <ActionHeader
-          leftComponent={<LeftActionHeader />}
-          rightComponent={<RightActionHeader />}
-        />
-
-        <div className="flex-container" style={{ height: '520px', margin: '0.5rem' }}>
-          {this.state.activeTab === '1'
-            ? <CalendarInstance
-              events={this.props.events}
-              defaultDate={new Date()}
-              selectable={true}
-              popup={true}
-              onSelectEvent={this.onSelectEvent}
-              onSelectSlot={this.handleOk}
-            />
-            : <p style={{ flexBasis: '70%' }}>Second tab Content</p>
-
-          }
-
-          <MainWrapper className="event-form" >
-            <StyledTabs
-              defaultActiveKey={this.state.activeTab}
-              onChange={(activeKey) => {
-                this.setState({
-                  activeTab: activeKey.toString()
-                })
-              }}>
-              <TabPane tab={<span><Icon type="Apple" />Apple</span>} key="1">
-                <EventForm />
-              </TabPane>
-              <TabPane tab={<span><Icon type="Android" />Android</span>} key="2">
-                
-              </TabPane>
-            </StyledTabs>
-          </MainWrapper>
-        </div>
-        <EditEventModal
-          title='Event Detail'
-          visible={this.state.isModalVisible}
-          onClose={this.onModalClose}
-          onOk={this.onModalClose}
-        >
-          <p>Modal</p>
-          <p>{this.state.selectedEvent.description}</p>
-        </EditEventModal>
-      </React.Fragment>
-    )
+  if (props.isLoading) {
+    return (<div className="flex-container" style={{ height: '80vh', justifyContent: 'center', alignItems: 'center' }}>
+      <Icon type="loading" style={{ fontSize: 60, color: 'tomato' }} spin />
+    </div>)
   }
+  // if (props.isError) {
+  //   return (<p>Some Error occoured...</p>)
+  // }
+  return (
+    <React.Fragment>
+      <ActionHeader
+        leftComponent={<LeftActionHeader />}
+        rightComponent={<RightActionHeader />}
+      />
+
+      <div className="flex-container" style={{ height: '520px', margin: '0.5rem' }}>
+        {activeTab === '1'
+          ? <CalendarInstance
+            events={props.events}
+            defaultDate={new Date()}
+            selectable={true}
+            popup={true}
+            onSelectEvent={onSelectEvent}
+            onSelectSlot={handleOk}
+          />
+          : <p style={{ flexBasis: '70%' }}>Second tab Content</p>
+
+        }
+
+        <MainWrapper className="event-form" >
+          <StyledTabs
+            defaultActiveKey={activeTab}
+            onChange={(activeKey) => {
+              setActiveTab(activeKey.toString())
+            }}>
+            <TabPane tab={<span><Icon type="Apple" />Apple</span>} key="1">
+              <EventForm />
+            </TabPane>
+            <TabPane tab={<span><Icon type="Android" />Android</span>} key="2">
+
+            </TabPane>
+          </StyledTabs>
+        </MainWrapper>
+      </div>
+      <EditEventModal
+        title='Event Detail'
+        visible={isModalVisible}
+        onClose={onModalClose}
+        onOk={onModalClose}
+      >
+        <p>Modal</p>
+        <p>{selectedEvent.description}</p>
+      </EditEventModal>
+    </React.Fragment>
+  )
 }
+// }
 
-const mapStateToProps = (state) => {
-  return {
-    isLoading: state.calendarReducer.isLoading,
-    isError: state.calendarReducer.isError,
-    events: state.calendarReducer.events
-  }
-};
+const mapStateToProps = ({ calendarReducer }) => ({
+  isLoading: calendarReducer.isLoading,
+  isError: calendarReducer.isError,
+  events: calendarReducer.events
+})
 
-const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators({
-    getEvents: getEvents,
-    setDate: setDate
-  }, dispatch);
-};
+const mapDispatchToProps = dispatch => bindActionCreators({
+  getEvents,
+  setDate
+}, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(Calendar);
 
