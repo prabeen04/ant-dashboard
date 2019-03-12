8538818481f6df1df99177d1fe2ec01a54dd70c8
Prabeen04 <prabeen.strange@gmail.com>
2019-03-12 20:18:44 +0530

    SettingTab made functional

--- src/components/tabs/settingTab.js
+++ src/components/tabs/settingTab.js
@@ -1,80 +1,75 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Tabs, Icon, Button, List, Checkbox } from 'antd';
 import { StyledTabs } from "../UI/Antd";
 import { TabsWrapper } from "../UI/Layout";
 const TabPane = Tabs.TabPane;
-const data = [
-    <Checkbox>Manchester United</Checkbox>,
-    <Checkbox>Arsenal</Checkbox>,
-    <Checkbox>Chelsea</Checkbox>,
-    <Checkbox>Manchester City</Checkbox>,
-    <Checkbox>Liverpool</Checkbox>,
-    <Checkbox>Tottenham</Checkbox>
-];
-const data2 = [
-    <Checkbox>Premier League</Checkbox>,
-    <Checkbox>Champions League</Checkbox>,
-    <Checkbox>FA Cup</Checkbox>,
-    <Checkbox>Capital One Cup</Checkbox>,
-];
-const data3 = [
-    <Checkbox>Points</Checkbox>,
-    <Checkbox>Most Wins</Checkbox>,
-    <Checkbox>Most Goal</Checkbox>,
-    <Checkbox>Most Chances Created</Checkbox>,
-    <Checkbox>Most fouls</Checkbox>,
-    <Checkbox>Most Tackles</Checkbox>,
-];
-class SettingTab extends Component {
-    constructor(props) {
-        super(props)
-    }
-    render() {
-        return (
-            <div>
-                <TabsWrapper>
-                    <StyledTabs defaultActiveKey="1" style={{ width: 400 }}>
-                        <TabPane tab={<span><Icon type="team" />Team</span>} key="1">
-                            <List
-                                size="small"
-                                bordered
-                                header={<div><h4> Team</h4></div>}
-                                footer={<div style={{ textAlign: 'right' }}> <Button
-                                    type="primary"
-                                    onClick={this.props.hide}>Close</Button></div>}
-                                dataSource={data}
-                                renderItem={item => (<List.Item>{item}</List.Item>)}
-                            />
 
-                        </TabPane>
-                        <TabPane tab={<span><Icon type="trophy" />Trophy</span>} key="2">
-                            <List
-                                size="small"
-                                bordered
-                                header={<div><h4>Filter by Trophies</h4></div>}
-                                footer={<div style={{ textAlign: 'right' }}> <Button
-                                    type="primary"
-                                    onClick={this.props.hide}>Close</Button></div>}
-                                dataSource={data2}
-                                renderItem={item => (<List.Item>{item}</List.Item>)}
-                            />
-                        </TabPane>
-                        <TabPane tab={<span><Icon type="line-chart" />Statistics</span>} key="3">
-                            <List
-                                size="small"
-                                bordered
-                                header={<div><h4>Filter by Stats</h4></div>}
-                                footer={<div style={{ textAlign: 'right' }}> <Button
-                                    type="primary"
-                                    onClick={this.props.hide}>Close</Button></div>}
-                                dataSource={data3}
-                                renderItem={item => (<List.Item>{item}</List.Item>)}
-                            />
-                        </TabPane>
-                    </StyledTabs>
-                </TabsWrapper>
-            </div>
-        )
-    }
+export default function SettingTab(props) {
+    const data = [
+        <Checkbox>Manchester United</Checkbox>,
+        <Checkbox>Arsenal</Checkbox>,
+        <Checkbox>Chelsea</Checkbox>,
+        <Checkbox>Manchester City</Checkbox>,
+        <Checkbox>Liverpool</Checkbox>,
+        <Checkbox>Tottenham</Checkbox>
+    ];
+    const data2 = [
+        <Checkbox>Premier League</Checkbox>,
+        <Checkbox>Champions League</Checkbox>,
+        <Checkbox>FA Cup</Checkbox>,
+        <Checkbox>Capital One Cup</Checkbox>,
+    ];
+    const data3 = [
+        <Checkbox>Points</Checkbox>,
+        <Checkbox>Most Wins</Checkbox>,
+        <Checkbox>Most Goal</Checkbox>,
+        <Checkbox>Most Chances Created</Checkbox>,
+        <Checkbox>Most fouls</Checkbox>,
+        <Checkbox>Most Tackles</Checkbox>,
+    ];
+    return (
+        <div>
+            <TabsWrapper>
+                <StyledTabs defaultActiveKey="1" style={{ width: 400 }}>
+                    <TabPane tab={<span><Icon type="team" />Team</span>} key="1">
+                        <List
+                            size="small"
+                            bordered
+                            header={<div><h4> Team</h4></div>}
+                            footer={<div style={{ textAlign: 'right' }}> <Button
+                                type="primary"
+                                onClick={props.hide}>Close</Button></div>}
+                            dataSource={data}
+                            renderItem={item => (<List.Item>{item}</List.Item>)}
+                        />
+
+                    </TabPane>
+                    <TabPane tab={<span><Icon type="trophy" />Trophy</span>} key="2">
+                        <List
+                            size="small"
+                            bordered
+                            header={<div><h4>Filter by Trophies</h4></div>}
+                            footer={<div style={{ textAlign: 'right' }}> <Button
+                                type="primary"
+                                onClick={props.hide}>Close</Button></div>}
+                            dataSource={data2}
+                            renderItem={item => (<List.Item>{item}</List.Item>)}
+                        />
+                    </TabPane>
+                    <TabPane tab={<span><Icon type="line-chart" />Statistics</span>} key="3">
+                        <List
+                            size="small"
+                            bordered
+                            header={<div><h4>Filter by Stats</h4></div>}
+                            footer={<div style={{ textAlign: 'right' }}> <Button
+                                type="primary"
+                                onClick={props.hide}>Close</Button></div>}
+                            dataSource={data3}
+                            renderItem={item => (<List.Item>{item}</List.Item>)}
+                        />
+                    </TabPane>
+                </StyledTabs>
+            </TabsWrapper>
+        </div>
+    )
 }
-export default SettingTab;
