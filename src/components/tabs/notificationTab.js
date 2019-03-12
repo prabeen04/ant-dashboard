e1f26c0ec7288786b67a93ae6016e14f9ccaa217
Prabeen04 <prabeen.strange@gmail.com>
2019-03-12 20:19:54 +0530

    NotificationTab made functional

--- src/components/tabs/notificationTab.js
+++ src/components/tabs/notificationTab.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Tabs, Icon, Button, List, Checkbox } from 'antd';
 import PastNotification from '../../container/notification/pastNotification';
 import PresentNotification from '../../container/notification/presentNotification';
@@ -6,24 +6,20 @@ import FutureNotification from '../../container/notification/futureNotification'
 
 const TabPane = Tabs.TabPane;
 
-class NotificationTab extends Component {
-    render() {
-        return (
-            <div>
-                <Tabs defaultActiveKey={"2"} scrollToActive={true} style={{ width: 400 }}>
-                    <TabPane key={"1"} tab={<span style={{ color: 'skyblue' }}><Icon type="team" />Yesterday</span>}>
-                        <PastNotification />
-                    </TabPane>
-                    <TabPane key={"2"} tab={<span style={{ color: 'lightgreen' }}><Icon type="trophy" />Today</span>}>
-                        <PresentNotification />
-                    </TabPane>
-                    <TabPane key={"3"} tab={<span style={{ color: 'tomato' }}><Icon type="line-chart" />Tommorow</span>}>
-                        <FutureNotification />
-                    </TabPane>
-                </Tabs>
-            </div>
-        )
-    }
+export default function NotificationTab(props) {
+    return (
+        <div>
+            <Tabs defaultActiveKey={"2"} scrollToActive={true} style={{ width: 400 }}>
+                <TabPane key={"1"} tab={<span style={{ color: 'skyblue' }}><Icon type="team" />Yesterday</span>}>
+                    <PastNotification />
+                </TabPane>
+                <TabPane key={"2"} tab={<span style={{ color: 'lightgreen' }}><Icon type="trophy" />Today</span>}>
+                    <PresentNotification />
+                </TabPane>
+                <TabPane key={"3"} tab={<span style={{ color: 'tomato' }}><Icon type="line-chart" />Tommorow</span>}>
+                    <FutureNotification />
+                </TabPane>
+            </Tabs>
+        </div>
+    )
 }
-
-export default NotificationTab;
