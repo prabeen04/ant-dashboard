99b7ab7c6fbb28cdd8f6f18502fc949e17f549a6
Prabeen04 <prabeen.strange@gmail.com>
2019-03-31 20:42:28 +0530

    post row styles changed to styled component

--- src/container/post/post.js
+++ src/container/post/post.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
 import { bindActionCreators } from 'redux';  
 import AddPost from "./addPost";
 import PostList from './postList';
--- src/container/post/postList.js
+++ src/container/post/postList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import styled from 'styled-components';
 import { bindActionCreators } from 'redux'
 import { withRouter } from 'react-router-dom'
 import { Icon, Card } from 'antd'
@@ -7,6 +8,21 @@ import { getPosts, getSinglePost } from '../../actions/post_actions'
 import './post.css'
 import moment from 'moment'
 
+const PostRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  align-content: center;
+  color: rgb(126, 123, 141);
+  /* background: #ffffff; */
+  font-weight: 500;
+  padding: 0.8rem 1rem;
+  margin: 0.1rem 0.5rem;
+  border-radius: 0.15rem;
+  box-shadow: 0 2px 6px 1px #ccc;
+  transition: 0.2s all ease-in-out;
+  cursor: pointer;
+`
 class PostList extends Component {
   constructor(props) {
     super(props)
@@ -26,7 +42,7 @@ class PostList extends Component {
   }
   render() {
     let renderPost = this.props.posts.map(post => {
-      return <div key={post._id} className="post-row " onClick={()=>this.getSinglePost(post)}>
+      return <PostRow key={post._id}  onClick={() => this.getSinglePost(post)}>
         <div className="post-title">
           <Icon type="idcard" className="post-icon" /> &nbsp;&nbsp;&nbsp;{post.title}
         </div>
@@ -36,11 +52,11 @@ class PostList extends Component {
         <div className="post-title">
           <Icon type="clock-circle-o" className="post-icon" /> &nbsp;&nbsp;&nbsp;{moment(post.date).fromNow()}
         </div>
-      </div>
+      </PostRow>
     })
 
     if (this.props.isLoading) {
-      return <Card loading style={{height: '400px'}}></Card>
+      return <Card loading style={{ height: '400px' }}></Card>
     }
     if (this.props.isError) {
       return <h4>Some Error Occoured...</h4>
