0ac34939e0eb5cf696fc307b3e2f3b1bb6b9ec81
prabeen04 <prabeen.strange@gmail.com>
2019-05-04 13:49:38 +0530

    Test code deleted

--- test/components/common/forms/textInput.test.js
+++ test/components/common/forms/textInput.test.js
@@ -1,10 +1,1 @@
-import React from 'react'
-import { shallow } from "enzyme";
-import TextInput from '../src/components/common/forms/textInput'
 
-describe('It should render a Text Input', () => {
-    const TextInput = shallow(<TextInput />)
-    it('should exist', () => {
-        TextInput.to.exist()
-    })
-})
