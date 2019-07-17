371c75b6ff15909f9f78a18c38f23e1fa1c53182
Prabeen04 <prabeen.strange@gmail.com>
2019-07-17 20:06:51 +0530

    small stuff

--- src/container/post/post.js
+++ src/container/post/post.js
@@ -1,40 +1,38 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
-import { bindActionCreators } from 'redux';  
+import { bindActionCreators } from 'redux';
 import AddPost from "./addPost";
 import PostList from './postList';
 import { Button, Icon, Modal } from 'antd';
-import{ addPost } from '../../actions/post_actions';
+import { addPost } from '../../actions/post_actions';
 import './post.css'
 
 class Post extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
       isModalOpen: false
     }
     this.setIsModalOpen = this.setIsModalOpen.bind(this);
   }
-  setIsModalOpen = (bool) =>{
-    this.setState({
-      isModalOpen: false
-    })
-  }
+  setIsModalOpen = bool => this.setState({ isModalOpen: false })
   render() {
     return (
       <div className="post-wrapper">
         <div className="post-actions">
-        <Button type="primary" icon="filter" >Filter</Button>
-        <Button 
-              type="primary" 
-              icon="plus" 
-              onClick={()=>this.setState({isModalOpen: true})}>
-              Add Post</Button>
+          <Button type="primary" icon="filter" >Filter</Button>
+          <Button
+            type="primary"
+            icon="plus"
+            onClick={() => this.setState({ isModalOpen: true })}>
+            Add Post</Button>
         </div>
-        
-        <AddPost isModalOpen={this.state.isModalOpen} setIsModalOpen={this.setIsModalOpen}/>
-        <PostList/>
+
+        <AddPost
+          isModalOpen={this.state.isModalOpen}
+          setIsModalOpen={this.setIsModalOpen} />
+        <PostList />
       </div>
     )
   }
