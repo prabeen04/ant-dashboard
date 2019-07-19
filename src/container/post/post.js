f8aa50c4fd223f9144629e6ab893990e07521a40
Prabeen04 <prabeen.strange@gmail.com>
2019-07-19 21:29:15 +0530

    redux removed from  post

--- src/container/post/post.js
+++ src/container/post/post.js
@@ -1,11 +1,8 @@
 import React, { Component, useState } from 'react';
-import { connect } from 'react-redux';
 import styled from 'styled-components';
-import { bindActionCreators } from 'redux';
 import AddPost from "./addPost";
 import PostList from './postList';
 import { Button, Icon, Modal } from 'antd';
-import { addPost } from '../../actions/post_actions';
 import './post.css'
 
 function Post() {
@@ -29,10 +26,4 @@ function Post() {
   )
 }
 
-
-const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators({
-    addPost
-  }, dispatch)
-}
-export default connect(null, mapDispatchToProps)(Post);
+export default Post;
