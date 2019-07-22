a42b2c4f6a88436d7c3616abe442f4dcab708e89
Prabeen04 <prabeen.strange@gmail.com>
2019-07-22 22:41:37 +0530

    simple form deleted

--- src/container/forms/forms.js
+++ src/container/forms/forms.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import { Divider, PageHeader, Typography } from 'antd';
-import SimpleForm from "./simpleForm";
 import ComplexForm from "./complexForm";
 import ReduxForm from './reduxForm';
 import FormArray from './formArray';
 import ReduxFormArray from './reduxFormArray';
 import NewFormArray from './newFormArray';
-import ExampleForm from './exampleForm';
 import FieldArraysForm from './fieldArraysForm';
 import SecondArrayForm from './secondArrayForm';
 import ThirdArrayForm from './thirdArrayForm';
@@ -22,8 +20,6 @@ class Forms extends Component {
           title="Forms"
         />
         {/* <Divider />
-          <SimpleForm />
-          <Divider />
           <ComplexForm />
           <Divider />
           <ReduxForm />
