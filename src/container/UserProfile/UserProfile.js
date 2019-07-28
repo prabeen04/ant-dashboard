ee908ca2692e3b8663615ad9f84ca46dd1d6ef0e
Prabeen04 <prabeen.strange@gmail.com>
2019-07-28 10:48:40 +0530

    UserProfile refactor

--- src/container/UserProfile/UserProfile.js
+++ src/container/UserProfile/UserProfile.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react'
 import Axios from 'axios';
 import { PIXABAY_URL } from '../../config/pixabay.config';
-import { Button } from 'antd';
+import { Button, Input } from 'antd';
 
 export default class UserProfile extends Component {
     constructor(props) {
         super(props)
         this.state = {
             query: 'india',
-            list: [
-                { name: 'tom', age: 34 },
-                { name: 'tommy', age: 14 },
-                { name: 'terhe', age: 44 },
-                { name: 'julia', age: 22 },
-            ],
             images: []
         }
     }
@@ -24,18 +18,13 @@ export default class UserProfile extends Component {
         //pixabay api
         console.clear()
         Axios.get(`${PIXABAY_URL}=${this.state.query}`)
-            .then(res => {
-                console.log(res)
-                this.setState({
-                    images: res.data.hits
-                })
-            })
+            .then(res => this.setState({ images: res.data.hits }))
             .catch(err => console.log(err))
     }
     render() {
         return (
             <div>
-                <input
+                <Input
                     value={this.state.query}
                     onChange={(e) => this.setState({ query: e.target.value })}
                 />&nbsp;
