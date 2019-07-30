7056d4e454ad933d0d09f44338898edfbb711bb9
Prabeen04 <prabeen.strange@gmail.com>
2019-07-30 08:25:26 +0530

    mixedbar chart made functional

--- src/container/charts/MixedBarChart.js
+++ src/container/charts/MixedBarChart.js
@@ -1,34 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 import { mixedBarChartSelector } from "../../selectors/chartsSelector";
 
-class MixedBarChart extends Component {
-    render() {
-        return (
-            <div>
-                <BarChart width={500} height={300} data={this.props.mixedBarChartData}
-                    margin={{ top: 20, bottom: 5 }}>
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="days" />
-                    <YAxis />
-                    <Tooltip />
-                    <Legend />
-                    <Bar dataKey="call" stackId="a" fill="#8884d8"barSize={30} />
-                    <Bar dataKey="meeting" stackId="a" fill="#82ca9d"barSize={30} />
-                    <Bar dataKey="email" stackId="a" fill="tomato"barSize={30} />
-                </BarChart>
-            </div>
-        )
-    }
+function MixedBarChart() {
+    return (
+        <div>
+            <BarChart width={500} height={300} data={this.props.mixedBarChartData}
+                margin={{ top: 20, bottom: 5 }}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="days" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="call" stackId="a" fill="#8884d8" barSize={30} />
+                <Bar dataKey="meeting" stackId="a" fill="#82ca9d" barSize={30} />
+                <Bar dataKey="email" stackId="a" fill="tomato" barSize={30} />
+            </BarChart>
+        </div>
+    )
 }
-const mapStateToProps = ({chartsReducer}) => ({
+const mapStateToProps = ({ chartsReducer }) => ({
     mixedBarChartData: mixedBarChartSelector(chartsReducer.data, chartsReducer.mixedBarChartData),
     viewType: chartsReducer.viewType,
     data: chartsReducer.data
 })
-const mapDispatchToProps = dispatch => bindActionCreators({
-    
- }, dispatch)
 export default connect(mapStateToProps, mapDispatchToProps)(MixedBarChart);
