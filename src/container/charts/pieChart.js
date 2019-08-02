3837d3c52ee5e7f17017e41ebea4c8ce6275d806
Prabeen04 <prabeen.strange@gmail.com>
2019-08-02 23:00:26 +0530

    dummy call for piechart deleted

--- src/container/charts/pieChart.js
+++ src/container/charts/pieChart.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react'
-import { PieChart,Area, Pie, Cell, Tooltip } from 'recharts';
+import { PieChart, Area, Pie, Cell, Tooltip } from 'recharts';
 import axios from 'axios';
-const url = 'https://fokuswork.com:8443/salesxl/api/v2.0/opportunity/user/wonLoss';
-const token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJwcmFiZWVuLnN0cmFuZ2VAZ21haWwuY29tIiwic2NvcGVzIjoiUk9MRV9BRE1JTiIsInVzZXJJZCI6IlVTSUY0NjA4NzYyMTIwMTgiLCJvcmdJZCI6Ik9JRjQzMzgxOTIxMjAxOCIsImlhdCI6MTU0MzQ5MzQ0NSwiZXhwIjoxNTQzNTExNDQ1fQ.3wG5f1VUPTzygItFmw9B1yW6_moWS7CPgwYa_NTkOk8'
 const data01 = [
   { name: 'Group A', value: 400, v: 89, color: 'rgba(52, 182, 193, 0.7)' },
   { name: 'Group B', value: 300, v: 100, color: 'rgba(56, 206, 33, 0.7)' },
@@ -20,21 +18,6 @@ class PieChart1 extends Component {
       chartData: data01
     }
   }
-  componentDidMount = () => {
-    axios.get(`${url}`, {
-      headers: {
-        'Authorization': `Bearer ${token}`
-      }
-    })
-      .then(res => {
-        console.log(res)
-        // this.setState({
-        //   chartData: res.data
-        // })
-      })
-      .catch(err => console.log(err))
-  }
-
   render() {
     return (
       <div style={{ margin: '0.2rem' }}>
