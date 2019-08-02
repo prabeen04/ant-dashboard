1c6a45448c7913d8a1ca651a8d4799339fbb5235
Prabeen04 <prabeen.strange@gmail.com>
2019-08-02 23:04:26 +0530

    Piechart component made functional

--- src/container/charts/pieChart.js
+++ src/container/charts/pieChart.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import { PieChart, Area, Pie, Cell, Tooltip } from 'recharts';
 import axios from 'axios';
 const data01 = [
@@ -10,29 +10,22 @@ const data01 = [
   { name: 'Group F', value: 189, v: 60, color: 'rgba(222, 249, 47, 0.7)' },
 ];
 
-class PieChart1 extends Component {
-  constructor(props) {
-    super(props)
 
-    this.state = {
-      chartData: data01
-    }
-  }
-  render() {
-    return (
-      <div style={{ margin: '0.2rem' }}>
-        <PieChart width={this.props.width} height={this.props.height} >
-          <Pie data={this.state.chartData} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={50}>
-            {
-              data01.map((entry, index) => <Cell fill={entry.color} />)
-            }
-          </Pie>
-          <Tooltip />
-          {/* <Legend/> */}
-        </PieChart>
-      </div>
-    )
-  }
+function PieChart1(props) {
+  const [chartData, setChartData] = useState(data01)
+  return (
+    <div style={{ margin: '0.2rem' }}>
+      <PieChart width={props.width} height={props.height} >
+        <Pie data={chartData} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={50}>
+          {
+            chartData.map((entry, index) => <Cell fill={entry.color} />)
+          }
+        </Pie>
+        <Tooltip />
+        {/* <Legend/> */}
+      </PieChart>
+    </div>
+  )
 }
 
 export default PieChart1;
