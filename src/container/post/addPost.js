6d0385de04ef72880a5d6f57740f3fa30f1c4fd6
Prabeen04 <prabeen.strange@gmail.com>
2019-08-04 13:41:09 +0530

    AddPost made functional

--- src/container/post/addPost.js
+++ src/container/post/addPost.js
@@ -6,55 +6,57 @@ import { addPost } from '../../actions/post_actions'
 import { Input, Modal } from 'antd'
 const { TextArea } = Input
 
-class AddPost extends Component {
-  onSubmit = (values) => {
+function AddPost(props) {
+  const { handleSubmit } = props
+  function onSubmit(values) {
     console.log(values)
-    this.props.addPost(values)
-    this.props.setIsModalOpen(false)
+    props.addPost(values)
+    props.setIsModalOpen(false)
   }
-  renderInput = ({ input, label, type, meta: { touched, error }, ...custom }) => (
-    <div>
-      <Input
-        placeholder={label}
-        {...input}
-        {...custom}
-      />
-      {touched && error && <span>{error}</span>}
-    </div>
-  )
-  renderTextarea = ({ input, label, type, meta: { touched, error }, ...custom }) => (
-    <div>
-      <TextArea
-        placeholder={label}
-        rows={5}
-        {...input}
-        {...custom}
-      />
-      {touched && error && <span>{error}</span>}
-    </div>
-  )
-  render() {
-    const { handleSubmit } = this.props
+  function renderInput({ input, label, type, meta: { touched, error }, ...custom }) {
     return (
       <div>
-        <form>
-          <Modal
-            title="Speak your mind"
-            maskClosable={true}
-            wrapClassName="vertical-center-modal"
-            visible={this.props.isModalOpen}
-            okText="AddPost"
-            onOk={handleSubmit(this.onSubmit)}
-            onCancel={() => this.props.setIsModalOpen(false)}
-          >
-            <Field name="title" label="Enter Post Title" component={this.renderInput} />
-            <Field name="author" label="Who are you" component={this.renderInput} />
-            <Field name="body" label="Post your story" component={this.renderTextarea} />
-          </Modal>
-        </form>
+        <Input
+          placeholder={label}
+          {...input}
+          {...custom}
+        />
+        {touched && error && <span>{error}</span>}
       </div>
     )
   }
+  function renderTextarea({ input, label, type, meta: { touched, error }, ...custom }) {
+    return (
+      <div>
+        <TextArea
+          placeholder={label}
+          rows={5}
+          {...input}
+          {...custom}
+        />
+        {touched && error && <span>{error}</span>}
+      </div>
+    )
+  }
+  return (
+    <div>
+      <form>
+        <Modal
+          title="Speak your mind"
+          maskClosable={true}
+          wrapClassName="vertical-center-modal"
+          visible={props.isModalOpen}
+          okText="AddPost"
+          onOk={handleSubmit(onSubmit)}
+          onCancel={() => props.setIsModalOpen(false)}
+        >
+          <Field name="title" label="Enter Post Title" component={renderInput} />
+          <Field name="author" label="Who are you" component={renderInput} />
+          <Field name="body" label="Post your story" component={renderTextarea} />
+        </Modal>
+      </form>
+    </div>
+  )
 }
 
 const mapStateToProps = ({ postReducer }) => ({
