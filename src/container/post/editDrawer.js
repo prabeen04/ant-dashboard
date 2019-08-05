de0c3022cd617d0f9b0011039c953661c4d5c246
Prabeen04 <prabeen.strange@gmail.com>
2019-08-05 20:20:36 +0530

    EditDrawer made functionla

--- src/container/post/editDrawer.js
+++ src/container/post/editDrawer.js
@@ -1,44 +1,29 @@
 import React, { Component } from 'react'
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
+import { useSelector } from "react-redux";
 import { Drawer, Button } from 'antd';
 import DrawerTitle from './drawerTitle';
 import './post.css';
 
-class EditDrawer extends Component {
-    constructor(props) {
-        super(props)
-    }
-    render() {
-        return (
-            <div>
-                <Drawer
-                    title={<DrawerTitle title={this.props.singlePost.title}/>}
-                    placement="right"
-                    width={600}
-                    closable={true}
-                    wrapClassName={'red-background'}
-                    onClose={this.props.handleClose}
-                    visible={this.props.open}
-                >
-                    <h2>{this.props.singlePost.title}</h2>
-                    <h4>{this.props.singlePost.author}</h4>
-                    <p>{this.props.singlePost.date}</p>
-                    <p>{this.props.singlePost.body}</p>
-                </Drawer>
-            </div>
-        )
-    }
-}
-const mapStateToProps = state => {
-    return{
-        singlePost: state.postReducer.singlePost
-    }
+function EditDrawer(props) {
+    const singlePost = useSelector(({ postReducer }) => postReducer.singlePost)
+    return (
+        <div>
+            <Drawer
+                title={<DrawerTitle title={props.singlePost.title} />}
+                placement="right"
+                width={600}
+                closable={true}
+                wrapClassName={'red-background'}
+                onClose={props.handleClose}
+                visible={props.open}
+            >
+                <h2>{props.singlePost.title}</h2>
+                <h4>{props.singlePost.author}</h4>
+                <p>{props.singlePost.date}</p>
+                <p>{props.singlePost.body}</p>
+            </Drawer>
+        </div>
+    )
 }
 
-const mapDispatchToProps = dispatch => {
-    return bindActionCreators({
-
-    }, dispatch)
-}
-export default connect(mapStateToProps, mapDispatchToProps)(EditDrawer);
+export default EditDrawer;
