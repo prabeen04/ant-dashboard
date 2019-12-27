0b2176526d881e99a2c18cfd49a1a9e5e5f9277f
Prabeen04 <prabeen.strange@gmail.com>
2019-12-27 21:31:15 +0530

    ProfileListView made functional

--- src/container/profile/profileListView.js
+++ src/container/profile/profileListView.js
@@ -1,49 +1,49 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import { getSingleProfile } from '../../actions/profile_actions'
-import classNames from 'classnames';
-import '../post/post.css';
-import { Card, Icon } from 'antd';
-const { Meta } = Card;
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { Icon } from "antd";
+import { getSingleProfile } from "../../actions/profile_actions";
+import classNames from "classnames";
+import "../post/post.css";
 
-class ProfileListView extends Component {
-    constructor(props) {
-        super(props)
-    }
-    render() {
-        const listClasses = classNames({
-            'post-row': true,
-            'single-post': true,
-            'activated-profile-listview': this.props.user.name === this.props.activatedProfile.name,
-        });
-        return (
-            <div className={listClasses}
-                style={{ display: 'flex', margin: '0.5rem' }}
-                onClick={() => this.props.getSingleProfile(this.props.user)}>
-                <div className="post-title">
-                    <Icon type="idcard" className="post-icon" /> &nbsp;&nbsp;&nbsp;{this.props.user.name}
-                </div>
-                <div className="post-title">
-                    <Icon type="user" className="post-icon" /> &nbsp;&nbsp;&nbsp;{this.props.user.email}
-                </div>
-                <div className="post-title">
-                    <Icon type="clock-circle-o" className="post-icon" /> &nbsp;&nbsp;&nbsp;{this.props.user.location}
-                </div>
-            </div>
-        )
-    }
+function ProfileListView(props) {
+  const listClasses = classNames({
+    "post-row": true,
+    "single-post": true,
+    "activated-profile-listview":
+      props.user.name === props.activatedProfile.name
+  });
+  return (
+    <div
+      className={listClasses}
+      style={{ display: "flex", margin: "0.5rem" }}
+      onClick={() => props.getSingleProfile(props.user)}
+    >
+      <div className="post-title">
+        <Icon type="idcard" className="post-icon" /> &nbsp;&nbsp;&nbsp;
+        {props.user.name}
+      </div>
+      <div className="post-title">
+        <Icon type="user" className="post-icon" /> &nbsp;&nbsp;&nbsp;
+        {props.user.email}
+      </div>
+      <div className="post-title">
+        <Icon type="clock-circle-o" className="post-icon" /> &nbsp;&nbsp;&nbsp;
+        {props.user.location}
+      </div>
+    </div>
+  );
 }
-const mapStateToProps = (state) => {
-    return {
-        profile: state.profileReducer.profile,
-        activatedProfile: state.profileReducer.singleProfile
-    }
-}
+const mapStateToProps = state => ({
+  profile: state.profileReducer.profile,
+  activatedProfile: state.profileReducer.singleProfile
+});
 
-const mapDispatchToProps = (dispatch) => {
-    return bindActionCreators({
-        getSingleProfile
-    }, dispatch)
-}
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      getSingleProfile
+    },
+    dispatch
+  );
 export default connect(mapStateToProps, mapDispatchToProps)(ProfileListView);
